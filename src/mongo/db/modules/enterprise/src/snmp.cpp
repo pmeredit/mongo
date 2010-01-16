f087fb91350ddc06c60b28dae588b6bd372221b3
Eliot Horowitz <eliot@10gen.com>
2010-01-16 00:50:20 -0500

    some basics working nicely
    GitOrigin-RevId: bf41fe009d96e7a96f4a46f18e9607101cd03bd2

--- src/mongo/db/modules/enterprise/src/snmp.cpp
+++ src/mongo/db/modules/enterprise/src/snmp.cpp
@@ -5,6 +5,7 @@
 #include "stdafx.h"
 #include "util/background.h"
 #include "db/module.h"
+#include "db/dbstats.h"
 
 #include <net-snmp/net-snmp-config.h>
 #include <net-snmp/net-snmp-includes.h>
@@ -16,7 +17,9 @@ namespace mongo {
     static oid rootOID[] =
         { 1, 3, 6, 1, 4, 1, 37601 };
 
-    static u_long myvalue = 18;
+    int my_snmp_callback( netsnmp_mib_handler *handler, netsnmp_handler_registration *reginfo,
+                          netsnmp_agent_request_info *reqinfo, netsnmp_request_info *requests);
+
 
     class SNMPAgent : public BackgroundJob , Module {
     public:
@@ -145,6 +148,14 @@ namespace mongo {
             return x;
         }
 
+        string toString( oid* o ){
+            stringstream ss;
+            int x=0;
+            while ( o[x] )
+                ss << "." << o[x++];
+            return ss.str();
+        }
+
     private:
         
         void _checkRegister( int x ){
@@ -165,12 +176,38 @@ namespace mongo {
             
         }
 
-        void _init(){
-            _checkRegister( netsnmp_register_read_only_counter32_instance( "asdasd" , 
-                                                                           getoid( "1,1,1") , oidlen( "1,1,1" ) ,
-                                                                           &myvalue , NULL ) );
+        void _initCounter( const char * name , const char* oidhelp , int * counter ){
+            log() << "registering: " << name << " " << toString( getoid( oidhelp ) ) << endl;
+
+            netsnmp_handler_registration * reg = netsnmp_create_handler_registration( name , NULL,
+                                                                                      getoid( oidhelp ) , oidlen( oidhelp ) , 
+                                                                                      HANDLER_CAN_RONLY);
+            
+            netsnmp_watcher_info * winfo = netsnmp_create_watcher_info( counter, sizeof(int),
+                                                                        ASN_COUNTER, WATCHER_FIXED_SIZE);
+            
+            _checkRegister( netsnmp_register_watched_scalar( reg, winfo ) );
         }
 
+        void _init(){
+        
+            // uptime
+            _startTime = time(0);
+            _uptime = "1,1,1";
+            netsnmp_handler_registration * upreg = netsnmp_create_handler_registration( "sysUpTime", &my_snmp_callback ,
+                                                                                        getoid( _uptime ),oidlen( _uptime ),
+                                                                                        HANDLER_CAN_RONLY);
+            _checkRegister( netsnmp_register_instance( upreg ) );
+            
+            // globalOpCounters
+            _initCounter( "globalOpInsert","1,2,1,1" , globalOpCounters.getInsert() );
+            _initCounter( "globalOpQuery","1,2,1,2" , globalOpCounters.getQuery() );
+            _initCounter( "globalOpUpdate","1,2,1,3" , globalOpCounters.getUpdate() );
+            _initCounter( "globalOpDelete","1,2,1,4" , globalOpCounters.getDelete() );
+            _initCounter( "globalOpGetMore","1,2,1,5" , globalOpCounters.getGetGore() );
+            
+        }
+        
         string _agentName;
         
         bool _enabled;
@@ -183,8 +220,44 @@ namespace mongo {
         
         map<string,oid*> _oids;
         
+    public:
+        string _uptime;
+        int _startTime;
     } snmpAgent;
 
+    int my_snmp_callback( netsnmp_mib_handler *handler, netsnmp_handler_registration *reginfo,
+                          netsnmp_agent_request_info *reqinfo, netsnmp_request_info *requests){
+        
+        int uptime = 100*(time(0)-snmpAgent._startTime);
+                
+        while (requests) {
+            netsnmp_variable_list *var = requests->requestvb;
+
+            switch (reqinfo->mode) {
+            case MODE_GET:{
+                if (netsnmp_oid_equals(var->name, var->name_length, snmpAgent.getoid( snmpAgent._uptime ) , snmpAgent.oidlen( snmpAgent._uptime ) ) == 0) {
+                    snmp_set_var_typed_value(var, ASN_TIMETICKS, (u_char *) &uptime, sizeof(uptime) );
+                }
+                return SNMP_ERR_NOERROR;
+            }
+                
+            case MODE_GETNEXT: {
+                if (netsnmp_oid_equals(var->name, var->name_length, snmpAgent.getoid( snmpAgent._uptime ) , snmpAgent.oidlen( snmpAgent._uptime ) ) < 0 ){
+                    snmp_set_var_objid(var,snmpAgent.getoid( snmpAgent._uptime ) , snmpAgent.oidlen( snmpAgent._uptime ) );
+                    snmp_set_var_typed_value(var, ASN_TIMETICKS, (u_char *) &uptime, sizeof(uptime) );
+                    return SNMP_ERR_NOERROR;
+                }
+                break;
+            }
+            default:
+                netsnmp_set_request_error(reqinfo, requests, SNMP_ERR_GENERR);
+                break;
+            }
+            
+            requests = requests->next;
+        }
+        return SNMP_ERR_NOERROR;
+    }
 }
 
 #endif
