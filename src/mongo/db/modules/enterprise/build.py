e5b7eebd339f096206264d68d2b6ce11a8dec785
Dan Crosta <dcrosta@10gen.com>
2012-06-01 16:04:04 -0400

    use the detected actual path to find source files
    GitOrigin-RevId: e3a971412e3415ac31df8ad22073fab9b98c147e

--- src/mongo/db/modules/enterprise/build.py
+++ src/mongo/db/modules/enterprise/build.py
@@ -16,7 +16,7 @@ def configure( conf , env , serverOnlyFiles ):
                 gotSNMP = True
 
     if gotSNMP:
-        serverOnlyFiles += env.Glob( "db/modules/enterprise/src/snmp*.cpp" )
+        serverOnlyFiles += env.Glob( root + "/src/snmp*.cpp" )
         env.Append( CPPDEFINES=["NETSNMP_NO_INLINE"] )
     else:
         print( "WARNING: couldn't find snmp pieces, not building snmp support" )
