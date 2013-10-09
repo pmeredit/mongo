53c9020674f090573df6a29ab663549d353524a8
Shaun Verch <shaun.verch@10gen.com>
2013-10-09 12:32:32 -0400

    SERVER-11090 Do not use Module class to register options in auditing module
    GitOrigin-RevId: 9ee305ab10c8ef931d8b16134996718ea1b28f39

--- src/mongo/db/modules/enterprise/src/audit/audit_options.cpp
+++ src/mongo/db/modules/enterprise/src/audit/audit_options.cpp
@@ -1,142 +1,126 @@
 /*
- * Copyright (C) 2013 10gen, Inc.  All Rights Reserved.
+ *    Copyright (C) 2013 10gen Inc.
  */
 
 #include "audit_options.h"
 
+#include "audit_event.h"
 #include "audit_manager.h"
 #include "audit_manager_global.h"
-#include "audit_event.h"
+#include "mongo/base/init.h"
+#include "mongo/db/server_options.h"
 #include "mongo/base/status.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/json.h"
 #include "mongo/db/matcher/expression_parser.h"
-#include "mongo/db/module.h"
 #include "mongo/util/log.h"
+#include "mongo/util/options_parser/environment.h"
 #include "mongo/util/options_parser/option_description.h"
+#include "mongo/util/options_parser/option_description.h"
+#include "mongo/util/options_parser/option_section.h"
+#include "mongo/util/options_parser/options_parser.h"
+#include "mongo/util/options_parser/startup_option_init.h"
 
 namespace mongo {
-
-    namespace moe = mongo::optionenvironment;
-
 namespace audit {
 
-    class AuditOptions : public Module {
+    AuditGlobalParams auditGlobalParams;
 
-    public:
-        AuditOptions() : Module("auditing"), 
-                         _enabled(false),
-                         _auditFormat(AuditFormatText) {}
+    Status addAuditOptions(moe::OptionSection* options) {
 
-        virtual void init() {
-            // Initialize the filter(s).
-            // This should move to the MONGO_INITIALIZER once we get rid of Module
-            getGlobalAuditManager()->enabled = _enabled;
+        typedef moe::OptionDescription OD;
+        typedef moe::PositionalOptionDescription POD;
 
-            if (_enabled) {
-                StatusWithMatchExpression parseResult = MatchExpressionParser::parse(_auditfilter);
-                if (!parseResult.isOK()) {
-                    //return status;
-                    fassertFailed(17185);
-                }
-                getGlobalAuditManager()->auditFilter = parseResult.getValue();
-            }
+        moe::OptionSection auditing_options("Auditing Options");
+
+        Status ret = auditing_options.addOption(OD("auditing", "auditing", moe::Switch,
+                    "activate auditing", true));
+        if (!ret.isOK()) {
+            return ret;
         }
 
-        virtual void shutdown() {}
-
-        void addOptions(moe::OptionSection* options) {
-            typedef moe::OptionDescription OD;
-            
-            moe::OptionSection auditing_options("Auditing Options");
-            
-            Status ret = auditing_options.addOption(OD("auditing", "auditing", moe::Switch,
-                                                       "activate auditing", true));
-            if (!ret.isOK()) {
-                log() << "Failed to register auditing option: " << ret.toString();
-                return;
-            }
-
-            ret = auditing_options.addOption(OD("auditpath", "auditpath", moe::String,
-                                                "full filespec for audit log", true));
-            if (!ret.isOK()) {
-                log() << "Failed to register auditpath option: " << ret.toString();
-                return;
-            }
-
-            ret = auditing_options.addOption(OD("auditformat", "auditformat", moe::String,
-                                                "text or bson", true));
-            if (!ret.isOK()) {
-                log() << "Failed to register auditformat option: " << ret.toString();
-                return;
-            }
-            
-            ret = auditing_options.addOption(OD("auditfilter", "auditfilter", moe::String,
-                                                "filter spec to screen audit records", true));
-            if (!ret.isOK()) {
-                log() << "Failed to register auditfilter option: " << ret.toString();
-                return;
-            }
-
-            ret = options->addSection(auditing_options);
-            if (!ret.isOK()) {
-                log() << "Failed to add auditing option section: " << ret.toString();
-                return;
-            }
-        } 
-
-        virtual void config(moe::Environment& params) {
-            if (params.count("auditing")) {
-                _enabled = true;
-            }
-            if (params.count("auditformat")) {
-                std::string auditFormatStr = params["auditformat"].as<std::string>();
-                if (auditFormatStr == "text") {
-                    _auditFormat = AuditFormatText;
-                }
-                else if (auditFormatStr == "bson") {
-                    _auditFormat = AuditFormatBson;
-                }
-                else {
-                    log() << "invalid auditformat parameter";
-                    ::_exit(EXIT_FAILURE);
-                }
-            }
-            if (params.count("auditfilter")) {
-                try {
-                    _auditfilter = fromjson(params["auditfilter"].as<std::string>());
-                }
-                catch (const MsgAssertionException& e) {
-                    // TODO: This is rather messy.
-                    //      Easier when this is in a MONGO_INTIIALIZER
-                    log() << "problem with auditfilter param: " << e.what();
-                    ::_exit(EXIT_FAILURE);
-                }
-            }
+        ret = auditing_options.addOption(OD("auditpath", "auditpath", moe::String,
+                    "full filespec for audit log", true));
+        if (!ret.isOK()) {
+            return ret;
         }
 
-        bool isEnabled() const {
-            return _enabled;
+        ret = auditing_options.addOption(OD("auditformat", "auditformat", moe::String,
+                    "text or bson", true));
+        if (!ret.isOK()) {
+            return ret;
         }
 
-        AuditFormat getAuditFormat() const {
-            return _auditFormat;
+        ret = auditing_options.addOption(OD("auditfilter", "auditfilter", moe::String,
+                    "filter spec to screen audit records", true));
+        if (!ret.isOK()) {
+            return ret;
         }
 
-    private:
-        bool _enabled;
-        BSONObj _auditfilter;
-        AuditFormat _auditFormat;
+        ret = options->addSection(auditing_options);
+        if (!ret.isOK()) {
+            log() << "Failed to add auditing option section: " << ret.toString();
+            return ret;
+        }
 
-    } auditOptions;
-
-    bool isEnabled() {
-        return auditOptions.isEnabled();
+        return Status::OK();
     }
 
-    AuditFormat getAuditFormat() {
-        return auditOptions.getAuditFormat();
+    Status storeAuditOptions(const moe::Environment& params,
+                             const std::vector<std::string>& args) {
+        if (params.count("auditing")) {
+            auditGlobalParams.enabled = true;
+        }
+        if (params.count("auditformat")) {
+            std::string auditFormatStr = params["auditformat"].as<std::string>();
+            if (auditFormatStr == "text") {
+                auditGlobalParams.auditFormat = AuditGlobalParams::AuditFormatText;
+            }
+            else if (auditFormatStr == "bson") {
+                auditGlobalParams.auditFormat = AuditGlobalParams::AuditFormatBson;
+            }
+            else {
+                log() << "invalid auditformat parameter";
+                ::_exit(EXIT_FAILURE);
+            }
+        }
+        if (params.count("auditfilter")) {
+            try {
+                auditGlobalParams.auditfilter = fromjson(params["auditfilter"].as<std::string>());
+            }
+            catch (const MsgAssertionException& e) {
+                // TODO: This is rather messy.
+                //      Easier when this is in a MONGO_INTIIALIZER
+                log() << "problem with auditfilter param: " << e.what();
+                ::_exit(EXIT_FAILURE);
+            }
+        }
+
+        return Status::OK();
     }
 
+    MONGO_MODULE_STARTUP_OPTIONS_REGISTER(AuditOptions)(InitializerContext* context) {
+        return addAuditOptions(&serverOptions);
+    }
+
+    MONGO_STARTUP_OPTIONS_STORE(AuditOptions)(InitializerContext* context) {
+        return storeAuditOptions(serverParsedOptions, context->args());
+    }
+
+    MONGO_INITIALIZER_WITH_PREREQUISITES(InitializeGlobalAuditManager, ("CreateAuditManager"))
+                                        (InitializerContext* context) {
+        audit::getGlobalAuditManager()->enabled = auditGlobalParams.enabled;
+
+        if (auditGlobalParams.enabled) {
+            StatusWithMatchExpression parseResult =
+                MatchExpressionParser::parse(auditGlobalParams.auditfilter);
+            if (!parseResult.isOK()) {
+                //return status;
+                fassertFailed(17185);
+            }
+            audit::getGlobalAuditManager()->auditFilter = parseResult.getValue();
+        }
+        return Status::OK();
+    }
 } // namespace audit
 } // namespace mongo
--- src/mongo/db/modules/enterprise/src/audit/audit_options.h
+++ src/mongo/db/modules/enterprise/src/audit/audit_options.h
@@ -1,16 +1,45 @@
 /*
- * Copyright (C) 2013 10gen, Inc.  All Rights Reserved.
+ *    Copyright (C) 2013 10gen Inc.
  */
 
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "mongo/base/status.h"
+#include "mongo/db/jsobj.h"
+
 namespace mongo {
+
+    namespace optionenvironment {
+        class OptionSection;
+        class Environment;
+    } // namespace optionenvironment
+
+    namespace moe = optionenvironment;
+
 namespace audit {
 
-    enum AuditFormat {
-        AuditFormatText = 0,
-        AuditFormatBson = 1
+    struct AuditGlobalParams {
+        bool enabled;
+        BSONObj auditfilter;
+
+        enum AuditFormat {
+            AuditFormatText = 0,
+            AuditFormatBson = 1
+        };
+
+        AuditFormat auditFormat;
+
+        AuditGlobalParams() : enabled(false), auditFormat(AuditFormatText) {}
     };
 
-    AuditFormat getAuditFormat();
+    extern AuditGlobalParams auditGlobalParams;
 
+    Status addAuditOptions(moe::OptionSection* options);
+
+    Status storeAuditOptions(const moe::Environment& params,
+                             const std::vector<std::string>& args);
 } // namespace audit
 } // namespace mongo
