9e09bca1b47595f426dbaa0e2fd2f7de86d4f7a4
James Wahlin <james.wahlin@10gen.com>
2014-09-17 11:39:45 -0400

    SERVER-15503 Fix SNMP jstest & add config check
    GitOrigin-RevId: 140927642f65459592207d27c936deede3452bb4

--- src/mongo/db/modules/enterprise/jstests/snmp/simple_snmpwalk.js
+++ src/mongo/db/modules/enterprise/jstests/snmp/simple_snmpwalk.js
@@ -1,19 +1,44 @@
 // This test serves as a basic test of mongodb's snmp support. It launches a mongod as an
 // snmp-master, then it has snmpwalk connect and retrieve all the variables, and it asserts that
 // no assertions occurred
+
+"use strict";
+
 var conn = MongoRunner.runMongod({"snmp-master": ""});
-// run snmp mapper
-run('snmpwalk', '-m', 'src/mongo/db/modules/enterprise/docs/MONGOD-MIB.txt', '-v', '2c', '-c',
-    'mongodb', '127.0.0.1:1161', '1.3.6.1.4.1.34601');
-// ensure mongod did not log any asserts
+
+// Run snmp mapper
+var status;
+
+status = run('snmpwalk', '--version');
+assert.eq(0, status, "snmpwalk not found. status="+ status);
+
+status = run('snmpwalk', '-v', '2c', '-c',
+             'mongodb', '127.0.0.1:1161', '1.3.6.1.4.1.34601');
+assert.eq(0, status, "snmpwalk invocation failed, status="+ status);
+
+print("retrieving mongod log");
 var fullLog = conn.getDB('admin').runCommand({getLog: "global"});
-var asserted = false;
-var regex = RegExp("assert", "i");
-for (i=0; i<fullLog.length; i++) {
-    if (regex.test(fullLog[i])) {
-        asserted = true;
+
+// Confirms net-snmp is configured correctly.
+// Failure implies that the mongod.conf net-snmp configuration file is missing or misconfigured
+print("Check log for missing snmp config");
+var regex = RegExp("no access control information configured");
+for (var i=0; i<fullLog.log.length; i++) {
+    var logLine = fullLog.log[i];
+    if (regex.test(logLine)) {
+        doassert("net-snmp not configured correctly: "+ logLine);
     }
 }
-assert.eq(asserted, false, "mongod asserted while snmpmapper was running");
+
+// Ensures mongod did not log any asserts
+print("check log for assertions");
+var regex = RegExp("assert", "i");
+for (var i=0; i<fullLog.log.length; i++) {
+    var logLine = fullLog.log[i];
+    if (regex.test(logLine)) {
+        doassert("mongod log contains assert during snmpwalk: "+ logLine);
+    }
+}
+
 MongoRunner.stopMongod(conn);
 print("snmp test suite run successfully");
