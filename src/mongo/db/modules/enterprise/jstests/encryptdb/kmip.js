c5036ce9bd73048d4d5cdd9daa3fa000dbdbe8d8
Robert Guo <robert.guo@10gen.com>
2015-08-18 15:04:40 -0400

    SERVER-20045 add KMIP jstests using PyKMIP
    GitOrigin-RevId: 3387cc0532dca0d173bc66503309008c43cf8f26

---
+++ src/mongo/db/modules/enterprise/jstests/encryptdb/kmip.js
@@ -0,0 +1,80 @@
+// This test tests KMIP options and key rotation for encrypted storage engine
+// It assumes that PyKMIP is installed
+
+(function () {
+    "use strict";
+
+    var testDir = "src/mongo/db/modules/enterprise/jstests/encryptdb/";
+
+    function runEncryptedMongod(params) {
+
+        var defaultParams = {
+            enableEncryption: "",
+            kmipServerName: "127.0.0.1",
+            // default port is 5696, we're setting it here to test option parsing
+            kmipPort: "6666",
+            kmipServerCAFile: "jstests/libs/ca.pem",
+            kmipClientCertificateFile: testDir + "libs/client_password_protected.pem",
+            kmipClientCertificatePassword: "qwerty",
+        };
+
+        var md = MongoRunner.runMongod(Object.merge(params, defaultParams));
+
+        return md;
+    }
+
+    function assertFind(md) {
+        var testDB = md.getDB("test");
+        var doc = testDB.test.findOne({}, {_id: 0});
+
+        assert.eq({"a": 1}, doc, "Document did not have expected value");
+    }
+
+    function assertKeyId(md, keyId) {
+
+        // restart with no keyID should work
+        md = runEncryptedMongod({
+            restart: md,
+        });
+        assert.neq(null, md, "Wasn't able to restart mongod without a keyID");
+        assertFind(md);
+        MongoRunner.stopMongod(md);
+
+        // restart explicitly with the keyID should also work
+        md = runEncryptedMongod({
+            restart: md,
+            kmipKeyIdentifier: keyId,
+        });
+        assert.neq(null, md, "Wasn't able to restart mongod with the correct keyID of " + keyId);
+        assertFind(md);
+        MongoRunner.stopMongod(md);
+    }
+
+    var pid = _startMongoProgram("python", testDir + "kmip_server.py");
+    // Assert here that PyKMIP is compatible with the default Python version
+    assert(checkProgram(pid));
+    // wait for PyKMIP, a KMIP server framework, to start
+    assert.soon(function () {return rawMongoProgramOutput().search("KMIP server") !== -1; });
+
+    // start mongod with default keyID of "1"
+    var md = runEncryptedMongod();
+    var testDB = md.getDB("test");
+    testDB.test.insert({a: 1});
+    MongoRunner.stopMongod(md);
+    assertKeyId(md, 1);
+
+    // do a key rotation, keyID is now "2"
+    runEncryptedMongod({
+        restart: md,
+        kmipRotateMasterKey: "",
+    });
+    assertKeyId(md, 2);
+
+    // do a key rotation and explicitly specify the keyID "1"
+    runEncryptedMongod({
+        restart: md,
+        kmipRotateMasterKey: "",
+        kmipKeyIdentifier: "1"
+    });
+    assertKeyId(md, 1);
+})()
---
+++ src/mongo/db/modules/enterprise/jstests/encryptdb/kmip_server.py
@@ -0,0 +1,36 @@
+# this file is a thin wrapper around the PyKMIP server
+# which is required for some encrypted storage engine tests
+
+import logging
+
+from kmip.services.kmip_server import KMIPServer
+
+def main():
+
+    logger = logging.getLogger(__name__)
+
+    server = KMIPServer(
+        host="127.0.0.1",
+        port=6666,
+        keyfile="jstests/libs/server.pem",
+        certfile="jstests/libs/server.pem",
+        cert_reqs="CERT_REQUIRED",
+        ssl_version="PROTOCOL_TLSv1",
+        ca_certs="jstests/libs/ca.pem",
+        do_handshake_on_connect=True,
+        suppress_ragged_eofs=True)
+
+    logger.info("Starting KMIP server")
+
+    try:
+        server.serve()
+    except Exception as e:
+        logger.info('Exception received while serving: {0}'.format(e))
+    finally:
+        server.close()
+
+    logger.info("Stopping KMIP server")
+
+
+if __name__ == '__main__':
+    main()
