6c476fa005641638845a6e0efb13b7fae78ab24c
Daniel Gottlieb <daniel.gottlieb@10gen.com>
2017-01-09 16:00:41 -0500

    SERVER-25712: Address queryable_mmapv1 cases with large block sizes
    GitOrigin-RevId: 503ae84731efa7bd0ee2f24bf6721acba27b2ac3

--- src/mongo/db/modules/enterprise/src/queryable/queryable_mmapv1/queryable_datafile.cpp
+++ src/mongo/db/modules/enterprise/src/queryable/queryable_mmapv1/queryable_datafile.cpp
@@ -70,6 +70,16 @@ DataFile::~DataFile() {
 #endif
 }
 
+std::size_t DataFile::getPageSizeForIdx(std::size_t pageIdx) const {
+    const std::size_t lastPageIdx = _mappedBlocks.size() - 1;
+    if (pageIdx < lastPageIdx) {
+        return _pageSize;
+    }
+
+    std::size_t lastPageOffset = pageIdx * _pageSize;
+    return _reader->getFileSize() - lastPageOffset;
+}
+
 Status DataFile::ensureRange(const std::size_t offset, const std::size_t count) {
     stdx::lock_guard<stdx::mutex> lock(_mappingLock);
 
@@ -85,13 +95,17 @@ Status DataFile::ensureRange(const std::size_t offset, const std::size_t count) 
 
         auto startPos = static_cast<char*>(_basePtr) + (pageIdx * _pageSize);
 #ifdef _WIN32
-        auto vpRet = VirtualAlloc(startPos, _pageSize, MEM_COMMIT, PAGE_READWRITE);
+        auto vpRet = VirtualAlloc(startPos, getPageSizeForIdx(pageIdx), MEM_COMMIT, PAGE_READWRITE);
         uassert(ErrorCodes::OperationFailed,
                 str::stream() << "Failed to make a page read/write. Code: " << GetLastError(),
                 vpRet);
 #else
-        auto mmapRet = mmap(
-            startPos, _pageSize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
+        auto mmapRet = mmap(startPos,
+                            getPageSizeForIdx(pageIdx),
+                            PROT_READ | PROT_WRITE,
+                            MAP_PRIVATE | MAP_ANON | MAP_FIXED,
+                            -1,
+                            0);
         uassert(ErrorCodes::ExceededMemoryLimit, "Failed to mmap a page", mmapRet != MAP_FAILED);
         uassert(ErrorCodes::BadValue, "Mmap returned an unexpected address", mmapRet == startPos);
 #endif
@@ -99,22 +113,23 @@ Status DataFile::ensureRange(const std::size_t offset, const std::size_t count) 
         _allocState->allocPage(this, pageIdx);
     }
 
-    auto blockSize = _reader->getBlockSize();
-    auto blockIdxRange = getRange(offset, count, blockSize);
+    auto fileBlockSize = _reader->getBlockSize();
+    auto blockIdxRange = getRange(offset, count, fileBlockSize);
     for (std::size_t num = 0; num < std::get<1>(blockIdxRange); ++num) {
         auto blockIdx = std::get<0>(blockIdxRange) + num;
         if (_mappedBlocks[blockIdx]) {
             continue;
         }
 
-        if (!_allocState->allocBlock(blockSize).isOK()) {
+        auto thisBlockSize = _reader->getBlockSizeForIdx(blockIdx);
+        if (!_allocState->allocBlock(thisBlockSize).isOK()) {
             throw WriteConflictException();
         }
 
-        auto startPos = static_cast<char*>(_basePtr) + (blockIdx * blockSize);
-        auto status = _reader->readBlockInto(DataRange(startPos, blockSize), blockIdx);
+        auto startPos = static_cast<char*>(_basePtr) + (blockIdx * fileBlockSize);
+        auto status = _reader->readBlockInto(DataRange(startPos, thisBlockSize), blockIdx);
         if (!status.isOK()) {
-            return status;
+            return status.getStatus();
         }
 
         _mappedBlocks[blockIdx] = true;
@@ -124,17 +139,20 @@ Status DataFile::ensureRange(const std::size_t offset, const std::size_t count) 
 }
 
 Status DataFile::releasePage(const std::size_t pageIdx) {
-    stdx::lock_guard<stdx::mutex> lock(_mappingLock);
     auto startPos = static_cast<char*>(_basePtr) + (pageIdx * _pageSize);
+
+    stdx::lock_guard<stdx::mutex> lock(_mappingLock);
 #ifdef _WIN32
-    auto vpRet = VirtualFree(startPos, _pageSize, MEM_DECOMMIT);
-    uassert(ErrorCodes::OperationFailed,
-            str::stream() << "Failed to decommit a page. Code: " << GetLastError(),
-            vpRet);
+    auto vpRet = VirtualFree(startPos, getPageSizeForIdx(pageIdx), MEM_DECOMMIT);
+    if (!vpRet) {
+        severe() << "Failed to decommit a page. Code: " << GetLastError();
+        fassertFailedNoTrace(40369);
+    }
 #else
-    auto mmapRet = mmap(startPos, _pageSize, PROT_NONE, MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
-    uassert(ErrorCodes::ExceededMemoryLimit, "Failed to mmap a page", mmapRet != MAP_FAILED);
-    uassert(ErrorCodes::BadValue, "Mmap returned an unexpected address", mmapRet == startPos);
+    auto mmapRet = mmap(
+        startPos, getPageSizeForIdx(pageIdx), PROT_NONE, MAP_PRIVATE | MAP_ANON | MAP_FIXED, -1, 0);
+    invariant(mmapRet != MAP_FAILED);
+    invariant(mmapRet == startPos);
 #endif
 
     _mappedPages[pageIdx] = false;
@@ -142,12 +160,15 @@ Status DataFile::releasePage(const std::size_t pageIdx) {
 
     auto offset = pageIdx * _pageSize;
     // Convert [offset -> offset + _pageSize] into block indexes.
-    auto blockSize = _reader->getBlockSize();
-    auto blockIdxRange = getRange(offset, _pageSize, blockSize);
+    auto blockIdxRange = getRange(offset, _pageSize, _reader->getBlockSize());
     for (std::size_t num = 0; num < std::get<1>(blockIdxRange); ++num) {
         auto blockIdx = std::get<0>(blockIdxRange) + num;
+        if (blockIdx >= _mappedBlocks.size()) {
+            break;
+        }
+
         if (_mappedBlocks[blockIdx]) {
-            _allocState->freeBlock(blockSize);
+            _allocState->freeBlock(_reader->getBlockSizeForIdx(blockIdx));
             _mappedBlocks[blockIdx] = false;
         }
     }
