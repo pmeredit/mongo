1c5e98899f988d712195ffdfd629064938a9aa8b
Mark Benvenuto <mark.benvenuto@mongodb.com>
2017-07-27 11:02:38 -0400

    SERVER-29948 Add tests for all watchdog monitored paths
    GitOrigin-RevId: 47013666d65b49da5a5f98fc7ccf4bf7fb679c5e

--- src/mongo/db/modules/enterprise/jstests/watchdog/wd_dbpath_hang.js
+++ src/mongo/db/modules/enterprise/jstests/watchdog/wd_dbpath_hang.js
@@ -1,52 +1,14 @@
-// Storage Node Watchdog test cases
+// Storage Node Watchdog - validate --dbpath
 //
-load("src/mongo/db/modules/enterprise/jstests/watchdog/lib/charybdefs_lib.js");
+load("src/mongo/db/modules/enterprise/jstests/watchdog/lib/wd_test_common.js");
 
 (function() {
     'use strict';
 
-    let control = new CharybdefsControl();
+    let control = new CharybdefsControl("dbpath_hang");
 
-    const db_path = control.getMountPath() + "/db";
+    const dbPath = control.getMountPath() + "/db";
 
-    function testMongoD() {
-        // Now start MongoD with it enabled at startup
-        //
-        const conn = MongoRunner.runMongod({
-            dbpath: db_path,
-            setParameter: "watchdogPeriodSeconds=" + control.getWatchdogPeriodSeconds()
-        });
-        assert.neq(null, conn, 'mongod was unable to start up');
-
-        // Wait for watchdog to get running
-        const admin = conn.getDB("admin");
-
-        // Wait for the watchdog to run some checks first
-        control.waitForWatchdogToStart(admin);
-
-        // Hang the file system
-        control.addWriteDelayFaultAndWait("watchdog_probe.txt");
-
-        // Check MongoD is dead by sending SIGTERM
-        // This will trigger our "nice" shutdown, but since mongod is stuck in the kernel doing I/O,
-        // the process will not terminate until charybdefs is done sleeping.
-        print("Stopping MongoDB now, it will terminate once charybdefs is done sleeping.");
-        MongoRunner.stopMongod(conn, undefined, {allowedExitCode: 61});
-    }
-
-    // Cleanup previous runs
-    control.cleanup();
-
-    try {
-        // Start the file system
-        control.start();
-
-        // Create the data directory on fuse mount point.
-        mkdir(db_path);
-
-        testMongoD();
-    } finally {
-        control.cleanup();
-    }
+    testFuseAndMongoD(control, {dbpath: dbPath});
 
 })();
