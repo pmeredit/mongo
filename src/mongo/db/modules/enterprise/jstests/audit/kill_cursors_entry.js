afecbe5930cf2536c3b62e515675685cbddb1d46
David Storch <david.storch@10gen.com>
2017-10-11 13:04:50 -0400

    SERVER-31516 Add test for auditing killCursors command.
    The test exercises a code path which could crash due to the
    NamespaceString bug described in this ticket.
    
    GitOrigin-RevId: 541071b307c4af3b207bec076bbf07bdc3b44c9b

---
+++ src/mongo/db/modules/enterprise/jstests/audit/kill_cursors_entry.js
@@ -0,0 +1,36 @@
+// Test auditing of the killCursors command.
+(function() {
+    "use strict";
+
+    load('src/mongo/db/modules/enterprise/jstests/audit/audit.js');
+
+    const standalone = MongoRunner.runMongodAuditLogger({});
+    const audit = standalone.auditSpooler();
+
+    const testDB = standalone.getDB("test");
+
+    // Issue a killCursors that the server expects to be owned by a collection, and verify the audit
+    // entry. Cursors owned by a collection have 00 as their most significant bits.
+    const collectionOwnedCursorId = NumberLong("1");
+    let cmdRes = assert.commandWorked(
+        testDB.runCommand({killCursors: "kill_cursors_entry", cursors: [collectionOwnedCursorId]}));
+    assert.eq(1, cmdRes.cursorsNotFound.length);
+    audit.assertEntry("authCheck", {
+        command: "killCursors",
+        ns: "test.kill_cursors_entry",
+        args: {killCursors: "kill_cursors_entry", cursorId: collectionOwnedCursorId}
+    });
+
+    // Issue a killCursors that the server expects to be owned by the global cursor manager, and
+    // verify the audit entry. Cursors owned by the global cursor manager have 01 as their most
+    // significant bits. We use 0x7000000000000000.
+    const globallyOwnedCursorId = NumberLong("8070450532247928832");
+    cmdRes = assert.commandWorked(
+        testDB.runCommand({killCursors: "$cmd.aggregate", cursors: [globallyOwnedCursorId]}));
+    assert.eq(1, cmdRes.cursorsNotFound.length);
+    audit.assertEntry("authCheck", {
+        command: "killCursors",
+        ns: "test.$cmd.aggregate",
+        args: {killCursors: "$cmd.aggregate", cursorId: globallyOwnedCursorId}
+    });
+}());
