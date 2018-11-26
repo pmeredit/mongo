0e86344ac19830263035bfc83e6ddccd7b271ef2
Patrick Meredith <pmeredit@gmail.com>
2018-11-26 12:21:35 -0500

    SERVER-32930: All comments save refactor bounded trig + decimal128 unittests

--- src/mongo/db/pipeline/expression_trigonometric.h
+++ src/mongo/db/pipeline/expression_trigonometric.h
@@ -1,29 +1,30 @@
 /**
- * Copyright (c) 2018 MongoDB Inc.
+ *    Copyright (C) 2018-present MongoDB, Inc.
  *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
  *
- * As a special exception, the copyright holders give permission to link the
- * code of portions of this program with the OpenSSL library under certain
- * conditions as described in each individual source file and distribute
- * linked combinations including the program with the OpenSSL library. You
- * must comply with the GNU Affero General Public License in all respects for
- * all of the code used other than as permitted herein. If you modify file(s)
- * with this exception, you may extend this exception to your version of the
- * file(s), but you are not obligated to do so. If you do not wish to do so,
- * delete this exception statement from your version. If you delete this
- * exception statement from all source files in the program, then also delete
- * it in the license file.
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
  */
 
 #pragma once
@@ -45,16 +46,14 @@ public:
         } else if (type == NumberDecimal) {
             return Value(decimalFunc(numericArg.getDecimal()));
         } else {
-            long long num = numericArg.getLong();
-            uassert(50968,
-                    str::stream() << "cannot apply trigonometric function to minimum long long",
-                    num != std::numeric_limits<long long>::min());
+            auto num = static_cast<double>(numericArg.getLong());
             return Value(doubleFunc(num));
         }
     }
 
     virtual double doubleFunc(double x) const = 0;
     virtual Decimal128 decimalFunc(Decimal128 x) const = 0;
+    virtual const char* getOpName() const = 0;
 };
 
 template <typename BoundedTrigType>
@@ -68,7 +67,7 @@ public:
         if (type == NumberDouble) {
             auto input = numericArg.getDouble();
             uassert(50989,
-                    str::stream() << "cannot apply inverse trigonometric function to " << input
+                    str::stream() << "cannot apply " << getOpName() << " to " << input
                                   << ", value must in ["
                                   << getLowerBound()
                                   << ","
@@ -80,7 +79,7 @@ public:
         } else if (type == NumberDecimal) {
             auto input = numericArg.getDecimal();
             uassert(50990,
-                    str::stream() << "cannot apply inverse trigonometric function to "
+                    str::stream() << "cannot apply " << getOpName() << " to "
                                   << input.toDouble()
                                   << ", value must in ["
                                   << getLowerBound()
@@ -93,7 +92,7 @@ public:
         } else {
             auto input = numericArg.getLong();
             uassert(50991,
-                    str::stream() << "cannot apply inverse trigonometric function to " << input
+                    str::stream() << "cannot apply " << getOpName() << " to " << input
                                   << ", value must in ["
                                   << getLowerBound()
                                   << ","
@@ -160,6 +159,7 @@ public:
     virtual boost::optional<double> getUpperBound() const = 0;
     virtual double doubleFunc(double x) const = 0;
     virtual Decimal128 decimalFunc(Decimal128 x) const = 0;
+    virtual const char* getOpName() const = 0;
 };
 
 class ExpressionArcCosine final : public ExpressionBoundedTrigonometric<ExpressionArcCosine> {
