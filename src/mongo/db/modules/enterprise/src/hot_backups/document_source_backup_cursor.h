a4f632b2298861f8d7a26f954ca0226a342750f9
Charlie Swanson <charlie.swanson@mongodb.com>
2019-01-09 12:54:25 -0500

    SERVER-38311 Conform to new DocumentSource interface
    This was shifted around a bit to help change the merging strategy
    required to run $out in a sharded cluster
    
    GitOrigin-RevId: 02d19b792c3a48a45b4a2b1ce29998c6a6b04f9e

--- src/mongo/db/modules/enterprise/src/hot_backups/document_source_backup_cursor.h
+++ src/mongo/db/modules/enterprise/src/hot_backups/document_source_backup_cursor.h
@@ -88,6 +88,10 @@ public:
         return kStageName;
     }
 
+    boost::optional<MergingLogic> mergingLogic() {
+        return boost::none;
+    }
+
     Value serialize(boost::optional<ExplainOptions::Verbosity> explain = boost::none) const final;
 
     StageConstraints constraints(Pipeline::SplitState pipeState) const final {
