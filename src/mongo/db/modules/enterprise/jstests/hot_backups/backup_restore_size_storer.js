6542d9dd0842140616b121d2f47ba0d18ded84da
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-03-29 12:29:39 -0400

    SERVER-39593 Persist sizeStorer information before opening the backup cursor
    GitOrigin-RevId: 3d52a91b5692954999bf2138677535a57d0da3f2

---
+++ src/mongo/db/modules/enterprise/jstests/hot_backups/backup_restore_size_storer.js
@@ -0,0 +1,62 @@
+/**
+ * Test that the 'sizeStorer.wt' file gets flushed the most up-to-date information before beginning
+ * a nonblocking backup.
+ *
+ * @tags: [requires_fsync]
+ */
+(function() {
+    'use strict';
+
+    load('jstests/libs/backup_utils.js');
+
+    function addDocuments(db, starting) {
+        let size = 100;
+        jsTest.log("Creating " + size + " test documents.");
+        let bulk = db.getCollection(collName).initializeUnorderedBulkOp();
+        for (let i = starting; i < starting + size; ++i) {
+            bulk.insert({i: i});
+        }
+        assert.writeOK(bulk.execute());
+    }
+
+    const dbName = "test";
+    const collName = "coll";
+
+    const rst = new ReplSetTest({nodes: 2});
+    rst.startSet();
+    rst.initiate();
+
+    const primary = rst.getPrimary();
+    const primaryDB = primary.getDB(dbName);
+
+    // Add some documents and force a checkpoint to have an initial sizeStorer file to backup.
+    addDocuments(primaryDB, 0);
+    rst.awaitReplication();
+    assert.commandWorked(primaryDB.adminCommand({fsync: 1}));
+
+    const backupPath = MongoRunner.dataPath + 'backup_restore';
+
+    // Add more documents and begin a backup immediately after, before another checkpoint can run.
+    addDocuments(primaryDB, 100);
+    rst.awaitReplication();
+
+    let numRecords = primaryDB.getCollection(collName).find({}).count();
+    assert.eq(200, numRecords);
+
+    backupData(primary, backupPath);
+
+    let copiedFiles = ls(backupPath);
+    assert.gt(copiedFiles.length, 0);
+
+    rst.stopSet();
+
+    const conn = MongoRunner.runMongod({dbpath: backupPath, noCleanData: true});
+    assert.neq(conn, null);
+
+    // Check that the size information is correct for the collection.
+    const db = conn.getDB(dbName);
+    numRecords = db.getCollection(collName).find({}).count();
+    assert.eq(200, numRecords);
+
+    MongoRunner.stopMongod(conn);
+}());
