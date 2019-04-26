547c0d4b6ea4fbd5144d4313b0daa063450a5fce
Mark Benvenuto <mark.benvenuto@mongodb.com>
2019-04-26 13:38:16 -0400

    SERVER-40768 Upgrade Watchdog test from Python 2 to Python 3
    GitOrigin-RevId: ae28e172a5d079855b273aca6392f97405f0b9aa

--- src/mongo/db/modules/enterprise/jstests/watchdog/charybdefs_setup.sh
+++ src/mongo/db/modules/enterprise/jstests/watchdog/charybdefs_setup.sh
@@ -16,12 +16,12 @@ rm -rf /data/charybdefs
 rm -rf /data/thrift
 
 # Use the mongo branch and fork from here
-git clone -b mongo https://github.com/markbenvenuto/charybdefs.git
+git clone -b mongo_42 https://github.com/markbenvenuto/charybdefs.git
 
 # Run the build script in the mongo branch
 cd charybdefs/mongo
 
 # Build and setup thrift and charybdefs
-bash ./build.sh
+PATH=/opt/mongodbtoolchain/v3/bin:$PATH bash ./build.sh
 
 echo Done - charybdefs_setup.sh
