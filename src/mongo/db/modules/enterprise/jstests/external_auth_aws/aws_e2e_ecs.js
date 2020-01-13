9174e82b249e3098be159ed42975e20dad99a617
Mark Benvenuto <mark.benvenuto@mongodb.com>
2020-01-13 15:55:41 +0000

    SERVER-45072 Add E2E testing on ECS task to evergreen
    GitOrigin-RevId: a03a52b044b1dd743af2c939ae51b938cd4a7656

---
+++ src/mongo/db/modules/enterprise/jstests/external_auth_aws/aws_e2e_ecs.js
@@ -0,0 +1,63 @@
+/**
+ * Validate that MONGODB-IAM auth works from ECS temporary credentials.
+ */
+load("src/mongo/db/modules/enterprise/jstests/external_auth_aws/lib/aws_e2e_lib.js");
+
+(function() {
+"use strict";
+
+if (_isWindows()) {
+    return;
+}
+
+function isUbuntu1804() {
+    if (_isWindows()) {
+        return false;
+    }
+
+    const grep_result = runProgram('grep', 'UBUNTU_CODENAME=bionic', '/etc/os-release');
+    if (grep_result === 0) {
+        return true;
+    }
+
+    return false;
+}
+
+// Since the container is Ubuntu 18.04, it does to make sense to run binaries from other distros on
+// it.
+if (!isUbuntu1804()) {
+    return;
+}
+
+const config = readSetupJson();
+
+const lib_dir = "src/mongo/db/modules/enterprise/jstests/external_auth_aws/lib";
+const container_tester = `${lib_dir}/container_tester.py`;
+const base_command = getPython3Binary() + ` -u  ${container_tester}`;
+
+const run_prune_command = base_command + " -v remote_gc_services " +
+    " --cluster " + config["iam_auth_ecs_cluster"];
+
+const run_test_command = base_command + " -d -v run_e2e_test" +
+    " --cluster " + config["iam_auth_ecs_cluster"] + " --task_definition " +
+    config["iam_auth_ecs_task_definition"] + " --subnets " + config["iam_auth_ecs_subnet_a"] +
+    " --subnets " + config["iam_auth_ecs_subnet_b"] + " --security_group " +
+    config["iam_auth_ecs_security_group"] + " --files mongod:/root/mongod mongo:/root/mongo " +
+    ` ${lib_dir}/ecs_hosted_test.js:/root/ecs_hosted_test.js ` +
+    ` --script ${lib_dir}/ecs_hosted_test.sh`;
+
+// Pass in the AWS credentials as environment variables
+// AWS_SHARED_CREDENTIALS_FILE does not work in evergreen for an unknown reason
+const env = {
+    AWS_ACCESS_KEY_ID: config["iam_auth_ecs_account"],
+    AWS_SECRET_ACCESS_KEY: config["iam_auth_ecs_secret_access_key"],
+}
+
+// Prune other containers
+let ret = runWithEnv(['/bin/sh', '-c', run_prune_command], env);
+assert.eq(ret, 0, "Prune Container failed");
+
+// Run the test in a container
+ret = runWithEnv(['/bin/sh', '-c', run_test_command], env);
+assert.eq(ret, 0, "Container Test failed");
+}());
---
+++ src/mongo/db/modules/enterprise/jstests/external_auth_aws/lib/ecs_hosted_test.js
@@ -0,0 +1,44 @@
+/**
+ * Verify the AWS IAM ECS hosted auth works
+ */
+
+(function() {
+"use strict";
+
+// This varies based on hosting ECS task as the account id and role name can vary
+const AWS_ACCOUNT_ARN = "arn:aws:sts::557821124784:assumed-role/ecsTaskExecutionRole/*"
+
+const conn = MongoRunner.runMongod({
+    setParameter: {
+        "authenticationMechanisms": "MONGODB-IAM,SCRAM-SHA-256",
+    },
+    auth: "",
+});
+
+const external = conn.getDB("$external");
+const admin = conn.getDB("admin");
+
+assert.commandWorked(admin.runCommand({createUser: "admin", pwd: "pwd", roles: ['root']}));
+assert(admin.auth("admin", "pwd"));
+
+assert.commandWorked(external.runCommand({createUser: AWS_ACCOUNT_ARN, roles: []}));
+
+// Try the command line
+const smoke = runMongoProgram("mongo",
+                              "--host",
+                              "localhost",
+                              "--port",
+                              conn.port,
+                              '--authenticationMechanism',
+                              'MONGODB-IAM',
+                              '--authenticationDatabase',
+                              '$external',
+                              "--eval",
+                              "1");
+assert.eq(smoke, 0, "Could not auth with smoke user");
+
+// Try the auth function
+assert(external.auth({mechanism: 'MONGODB-IAM'}));
+
+MongoRunner.stopMongod(conn);
+}());
