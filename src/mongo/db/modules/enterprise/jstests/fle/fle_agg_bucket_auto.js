1a69cef5868d02499b7fa0f96e499772599b71a1
David Percy <david.percy@mongodb.com>
2020-02-05 18:41:48 +0000

    SERVER-45447 $accumulator - FLE analysis
    GitOrigin-RevId: aea8f5bda95b8291e2034af295881abeb0cd1196

--- src/mongo/db/modules/enterprise/jstests/fle/fle_agg_bucket_auto.js
+++ src/mongo/db/modules/enterprise/jstests/fle/fle_agg_bucket_auto.js
@@ -228,5 +228,62 @@ command = {
 };
 assertCommandUnchanged(command, false, true);
 
+// Test that $accumulator is allowed as long as it doesn't touch any encrypted fields.
+command = {
+    aggregate: coll.getName(),
+    pipeline: [{
+        $bucketAuto: {
+            groupBy: "$price",
+            buckets: 3,
+            output: {
+                abc: {
+                    $accumulator: {
+                        init: 'function() {}',
+                        initArgs: {$const: []},
+                        accumulate: 'function() {}',
+                        accumulateArgs: "$nonSecretString",
+                        merge: 'function() {}',
+                        finalize: 'function() {}',
+                        lang: 'js',
+                    }
+                },
+            }
+        }
+    }],
+    cursor: {},
+    jsonSchema: {type: "object", properties: {secretString: encryptedStringSpec}},
+    isRemoteSchema: false,
+};
+assertCommandUnchanged(command, false, true);
+
+// Test that $accumulator is not allowed to reference encrypted or possibly-encrypted fields.
+command = {
+    aggregate: coll.getName(),
+    pipeline: [{
+        $bucketAuto: {
+            groupBy: "$price",
+            buckets: 3,
+            output: {
+                abc: {
+                    $accumulator: {
+                        init: 'function() {}',
+                        initArgs: {$const: []},
+                        accumulate: 'function() {}',
+                        accumulateArgs:
+                            {$cond: ["$unknownBool", "a public string", "$secretString"]},
+                        merge: 'function() {}',
+                        finalize: 'function() {}',
+                        lang: 'js',
+                    }
+                },
+            }
+        }
+    }],
+    cursor: {},
+    jsonSchema: {type: "object", properties: {secretString: encryptedStringSpec}},
+    isRemoteSchema: false,
+};
+assert.commandFailedWithCode(testDB.runCommand(command), 51221);
+
 mongocryptd.stop();
 })();
