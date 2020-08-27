26a34c9c033811209defa1a4e87f39f7ce0d6edf
Sara Golemon <sara.golemon@mongodb.com>
2020-08-27 22:03:59 +0000

    SERVER-50605 Validate escaping of audit applicationMessage
    GitOrigin-RevId: db499d40b1e8ca8260176a867346f015c6bec9a5

--- src/mongo/db/modules/enterprise/jstests/audit/application_message.js
+++ src/mongo/db/modules/enterprise/jstests/audit/application_message.js
@@ -4,16 +4,37 @@
 'use strict';
 
 load('src/mongo/db/modules/enterprise/jstests/audit/lib/audit.js');
-print("START audit-log-application-message.js");
 
-// Test with BSON files to ensure some coverage for BSON
-var m = MongoRunner.runMongodAuditLogger({}, true);
-var audit = m.auditSpooler();
-var db = m.getDB("test");
+function runTest(asBSON) {
+    jsTest.log("START audit-log-application-message.js " + tojson(asBSON));
 
-assert.commandWorked(db.runCommand({logApplicationMessage: "Hello World"}));
-audit.assertEntry("applicationMessage", {msg: "Hello World"});
+    const m = MongoRunner.runMongodAuditLogger({}, asBSON);
+    const audit = m.auditSpooler();
+    const db = m.getDB("test");
 
-MongoRunner.stopMongod(m);
-print("SUCCESS audit-log-application-message.js");
+    // Test null byte separately.
+    // We expect this to fail during command parsing when the message
+    // is treated like a namespace (due to BasicCommand handling).
+    assert.commandFailedWithCode(db.runCommand({logApplicationMessage: "Hello\u0000World"}),
+                                 [ErrorCodes.InvalidNamespace]);
+
+    // Write and check for an audit message containing any character except null byte.
+    for (let i = 1; i < 256; ++i) {
+        try {
+            const msg = "Hello" + String.fromCharCode(i) + "World";
+            assert.commandWorked(db.runCommand({logApplicationMessage: msg}));
+            audit.assertEntry("applicationMessage", {msg: msg});
+        } catch (e) {
+            jsTest.log("Failed at i=" + i + ", asBSON=" + tojson(asBSON));
+            throw e;
+        }
+    }
+
+    MongoRunner.stopMongod(m);
+    jsTest.log("SUCCESS audit-log-application-message.js " + tojson(asBSON));
+}
+
+// Test with both JSON and BSON files to ensure some coverage for each.
+runTest(true);
+runTest(false);
 })();
