84caf8a9120e12e0a03e847c68c26c03d9a4c501
Lingzhi Deng <lingzhi.deng@mongodb.com>
2020-09-11 11:34:39 -0400

    SERVER-50855: Create voteCommitImportCollection command
    GitOrigin-RevId: a9ce36e46836acf1b7071b1e578da6bec651db93

---
+++ src/mongo/db/modules/enterprise/jstests/live_import/vote_commit_import_collection_command_sanity.js
@@ -0,0 +1,115 @@
+/**
+ * Test sanity of the voteCommitImportCollection command.
+ *
+ * @tags: [requires_persistence, requires_replication, requires_wiredtiger]
+ */
+
+(function() {
+"use strict";
+
+function testInvalidUsages(db) {
+    // Missing fields.
+    assert.commandFailedWithCode(db.runCommand({voteCommitImportCollection: "test.foo"}), 40414);
+
+    // Invalid hostAndPort.
+    assert.commandFailedWithCode(db.runCommand({
+        voteCommitImportCollection: "test.foo",
+        from: "localhost:27017:1",
+        dryRunSuccess: true
+    }),
+                                 ErrorCodes.FailedToParse);
+
+    // Invalid vote.
+    assert.commandFailedWithCode(db.runCommand({
+        voteCommitImportCollection: "test.foo",
+        from: "localhost:27017",
+        dryRunSuccess: "yes"
+    }),
+                                 ErrorCodes.TypeMismatch);
+
+    // Invalid reason.
+    assert.commandFailedWithCode(db.runCommand({
+        voteCommitImportCollection: "test.foo",
+        from: "localhost:27017",
+        dryRunSuccess: false,
+        reason: 12345
+    }),
+                                 ErrorCodes.TypeMismatch);
+
+    // Unknown field.
+    assert.commandFailedWithCode(db.runCommand({
+        voteCommitImportCollection: "test.foo",
+        from: "localhost:27017",
+        dryRunSuccess: false,
+        foo: "foo"
+    }),
+                                 40415);
+}
+
+// Test standalone.
+jsTestLog("Testing standalone");
+const standalone = MongoRunner.runMongod();
+const adminDB = standalone.getDB("admin");
+testInvalidUsages(adminDB);
+
+// voteCommitImportCollection is not allowed on standalone.
+assert.commandFailedWithCode(
+    adminDB.runCommand(
+        {voteCommitImportCollection: "test.foo", from: "localhost:27017", dryRunSuccess: true}),
+    ErrorCodes.NoReplicationEnabled);
+MongoRunner.stopMongod(standalone);
+
+// Test replica set.
+jsTestLog("Testing replica set");
+const kKeyFile = "jstests/libs/key1";
+const rst = new ReplSetTest({nodes: 2, nodeOptions: {auth: ""}, keyFile: kKeyFile});
+rst.startSet();
+rst.initiateWithHighElectionTimeout();
+const primary = rst.getPrimary();
+const secondary = rst.getSecondary();
+const primaryAdmin = primary.getDB('admin');
+const secondaryAdmin = secondary.getDB('admin');
+
+// Setup initial user.
+primaryAdmin.createUser({user: 'admin', pwd: 'pass', roles: jsTest.adminUserRoles}, {w: 2});
+assert(primaryAdmin.auth('admin', 'pass'));
+assert(secondaryAdmin.auth('admin', 'pass'));
+
+testInvalidUsages(primaryAdmin);
+testInvalidUsages(secondaryAdmin);
+
+// voteCommitImportCollection is not allowed even with admin role.
+assert.commandFailedWithCode(
+    primaryAdmin.runCommand(
+        {voteCommitImportCollection: "test.foo", from: "localhost:27017", dryRunSuccess: true}),
+    ErrorCodes.Unauthorized);
+assert.commandFailedWithCode(
+    secondaryAdmin.runCommand(
+        {voteCommitImportCollection: "test.foo", from: "localhost:27017", dryRunSuccess: true}),
+    ErrorCodes.Unauthorized);
+
+assert(primaryAdmin.logout());
+assert(secondaryAdmin.logout());
+
+// Test with internal privilege.
+authutil.asCluster(primary, kKeyFile, () => {
+    // voteCommitImportCollection is admin-only.
+    assert.commandFailedWithCode(
+        primary.getDB("test").runCommand(
+            {voteCommitImportCollection: "test.foo", from: "localhost:27017", dryRunSuccess: true}),
+        ErrorCodes.Unauthorized);
+    // voteCommitImportCollection is only allowed against admin database with internal privilege.
+    assert.commandWorked(primary.getDB("admin").runCommand(
+        {voteCommitImportCollection: "test.foo", from: "localhost:27017", dryRunSuccess: true}));
+});
+
+authutil.asCluster(secondary, kKeyFile, () => {
+    // voteCommitImportCollection is not allowed on secondary.
+    assert.commandFailedWithCode(
+        secondary.getDB("admin").runCommand(
+            {voteCommitImportCollection: "test.foo", from: "localhost:27017", dryRunSuccess: true}),
+        ErrorCodes.NotWritablePrimary);
+});
+
+rst.stopSet();
+}());
---
+++ src/mongo/db/modules/enterprise/src/live_import/commands/vote_commit_import_collection_command.cpp
@@ -0,0 +1,85 @@
+/**
+ * Copyright (C) 2020-present MongoDB, Inc.  All Rights Reserved.
+ */
+
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
+
+#include "mongo/platform/basic.h"
+
+#include "live_import/commands/vote_commit_import_collection_gen.h"
+#include "mongo/db/auth/authorization_session.h"
+#include "mongo/db/commands.h"
+#include "mongo/db/repl/replication_coordinator.h"
+#include "mongo/logv2/log.h"
+
+namespace mongo {
+namespace {
+
+/**
+ * Vote commit of an ongoing live import.
+ *
+ * {
+ *     voteCommitImportCollection: <NamespaceString>,
+ *     from: <HostAndPort>,
+ *     dryRunSuccess: <bool>,
+ *     reason: <string>,
+ * }
+ */
+class VoteCommitImportCollectionCommand final
+    : public TypedCommand<VoteCommitImportCollectionCommand> {
+public:
+    using Request = VoteCommitImportCollection;
+
+    std::string help() const override {
+        return "An internal mongod command to coordinate live import of collection files in a "
+               "replica set";
+    }
+
+    bool adminOnly() const override {
+        return true;
+    }
+
+    AllowedOnSecondary secondaryAllowed(ServiceContext*) const override {
+        return AllowedOnSecondary::kNever;
+    }
+
+    class Invocation final : public InvocationBase {
+    public:
+        using InvocationBase::InvocationBase;
+
+        void typedRun(OperationContext* opCtx) {
+            BSONObjBuilder result;
+            uassertStatusOK(
+                repl::ReplicationCoordinator::get(opCtx)->checkReplEnabledForCommand(&result));
+
+            const auto& cmd = request();
+            LOGV2(5085500,
+                  "Received voteCommitImportCollection request",
+                  "namespace"_attr = cmd.getCommandParameter().ns(),
+                  "from"_attr = cmd.getFrom(),
+                  "dryRunSuccess"_attr = cmd.getDryRunSuccess(),
+                  "reason"_attr = cmd.getReason());
+        }
+
+    private:
+        NamespaceString ns() const override {
+            return request().getCommandParameter();
+        }
+
+        bool supportsWriteConcern() const override {
+            return false;
+        }
+
+        void doCheckAuthorization(OperationContext* opCtx) const override {
+            uassert(ErrorCodes::Unauthorized,
+                    "Unauthorized",
+                    AuthorizationSession::get(opCtx->getClient())
+                        ->isAuthorizedForActionsOnResource(ResourcePattern::forClusterResource(),
+                                                           ActionType::internal));
+        }
+    };
+
+} voteCommitImportCollectionCommand;
+
+}  // namespace
+}  // namespace mongo
