360bc742ea0871c387307164d8008101a75e996d
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2020-11-11 20:56:24 +0000

    SERVER-48221 Shut down ftdc after storage engine
    GitOrigin-RevId: f7274c2082f729c1715b2dfd7fd233016a2a58a6

--- src/mongo/db/modules/enterprise/src/encryptdb/encryption_server_status.cpp
+++ src/mongo/db/modules/enterprise/src/encryptdb/encryption_server_status.cpp
@@ -2,12 +2,16 @@
  * Copyright (C) 2016 MongoDB, Inc.  All Rights Reserved.
  */
 
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kFTDC
+
 #include "mongo/platform/basic.h"
 
 #include "encryption_key_manager.h"
 
 #include "mongo/db/commands/server_status.h"
+#include "mongo/db/concurrency/d_concurrency.h"
 #include "mongo/db/operation_context.h"
+#include "mongo/logv2/log.h"
 
 namespace mongo {
 
@@ -30,6 +34,16 @@ public:
     }
 
     BSONObj generateSection(OperationContext* opCtx, const BSONElement& configElement) const {
+        // This is unsafe to access when the storage engine is shutting down. As the global
+        // exclusive lock is held during shutdown, we protect this with a global intent lock of an
+        // immediate timeout to not block FTDC.
+        Lock::GlobalLock lk(
+            opCtx, LockMode::MODE_IS, Date_t::now(), Lock::InterruptBehavior::kLeaveUnlocked);
+        if (!lk.isLocked()) {
+            LOGV2_DEBUG(4822101, 2, "Failed to retrieve encryptionAtRest statistics");
+            return BSONObj();
+        }
+
         BSONObjBuilder result;
         EncryptionHooks* hooks =
             dynamic_cast<EncryptionKeyManager*>(EncryptionHooks::get(opCtx->getServiceContext()));
