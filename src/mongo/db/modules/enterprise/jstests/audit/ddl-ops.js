c801860eb65ccfc8130ef8f5679b8148f29415de
Varun Ravichandran <varun.ravichandran@mongodb.com>
2021-01-30 02:02:42 +0000

    SERVER-50994: Audit dropCollection, dropIndex, and dropView during dropDatabase on standalones
    GitOrigin-RevId: 38de413900f7f0ee4a43423dea46cd8d2dfa8d2c

--- src/mongo/db/modules/enterprise/jstests/audit/ddl-ops.js
+++ src/mongo/db/modules/enterprise/jstests/audit/ddl-ops.js
@@ -53,167 +53,241 @@ function runTests(mode, mongo, audit, improvedAuditingEnabled) {
     /**
      * Returns a RegExp instance for a drop* namespace.
      * Will match test.collname or test.system.drop.1234i56t78.collname
-     * This covers both single-phase and two-phase drop strageties.
+     * This covers both single-phase and two-phase drop strategies.
      */
-    function dropTestCollNSS(collname) {
-        return new RegExp('test\\.(system\\.drop\\.\\d+i\\d+t\\d+\\.)?' + collname);
+    function dropTestCollNSS(dbname, collname) {
+        return new RegExp(dbname + '\\.(system\\.(drop\\.\\d+i\\d+t\\d+\\.)?)?' + collname);
     }
 
     jsTest.log('START audit/ddl-ops.js ' + mode);
 
     const admin = mongo.getDB("admin");
-    const test = mongo.getDB('test');
+    const testOne = mongo.getDB('testOne');
+    const testTwo = mongo.getDB('testTwo');
     assert.commandWorked(admin.runCommand({createUser: "admin", pwd: "admin", roles: ["root"]}));
     assert(admin.auth('admin', 'admin'));
     audit.fastForward();
 
     //// Create Collection (and implicitly Database)
-    assert.writeOK(test.implicitCollection.insert({x: 1}));
-    audit.assertEntryForAdmin('createDatabase', {ns: 'test'});
-    audit.assertEntryForAdmin('createCollection', {ns: 'test.implicitCollection'});
+
+    assert.writeOK(testOne.implicitCollection.insert({x: 1}));
+    audit.assertEntryForAdmin('createDatabase', {ns: 'testOne'});
+    audit.assertEntryForAdmin('createCollection', {ns: 'testOne.implicitCollection'});
     audit.assertEntryForAdmin('createIndex', {
-        ns: 'test.implicitCollection',
+        ns: 'testOne.implicitCollection',
         indexName: '_id_',
         indexSpec: {v: 2, key: {_id: 1}, name: '_id_'}
     });
 
-    assert.commandWorked(test.createCollection('explicitCollection'));
-    audit.assertEntryForAdmin('createCollection', {ns: 'test.explicitCollection'});
+    assert.commandWorked(testOne.createCollection('explicitCollection'));
+    audit.assertEntryForAdmin('createCollection', {ns: 'testOne.explicitCollection'});
     audit.assertEntryForAdmin('createIndex', {
-        ns: 'test.explicitCollection',
+        ns: 'testOne.explicitCollection',
         indexName: '_id_',
         indexSpec: {v: 2, key: {_id: 1}, name: '_id_'}
     });
 
     //// Create Index
-    assert.commandWorked(test.implicitCollection.createIndex({x: 1}));
+    assert.commandWorked(testOne.implicitCollection.createIndex({x: 1}));
     audit.assertEntryForAdmin('createIndex', {
-        ns: 'test.implicitCollection',
+        ns: 'testOne.implicitCollection',
         indexName: 'x_1',
         indexSpec: {v: 2, key: {x: 1}, name: 'x_1'}
     });
 
-    assert.commandWorked(test.explicitCollection.createIndex({y: 1}));
+    assert.commandWorked(testOne.explicitCollection.createIndex({y: 1}));
     audit.assertEntryForAdmin('createIndex', {
-        ns: 'test.explicitCollection',
+        ns: 'testOne.explicitCollection',
         indexName: 'y_1',
         indexSpec: {v: 2, key: {y: 1}, name: 'y_1'}
     });
 
-    assert.commandWorked(test.explicitCollection.dropIndex({y: 1}));
-    audit.assertEntryForAdmin('dropIndex', {ns: 'test.explicitCollection', indexName: 'y_1'});
+    assert.commandWorked(testOne.explicitCollection.dropIndex({y: 1}));
+    audit.assertEntryForAdmin('dropIndex', {ns: 'testOne.explicitCollection', indexName: 'y_1'});
 
     //// Create View
-    assert.commandWorked(test.createView('implicitView', 'implicitCollection', []));
-    const expectImplicitView = {ns: 'test.implicitView'};
+    assert.commandWorked(testOne.createView('implicitView', 'implicitCollection', []));
+    const expectImplicitViewTestOne = {ns: 'testOne.implicitView'};
     if (improvedAuditingEnabled) {
-        expectImplicitView.viewOn = 'test.implicitCollection';
-        expectImplicitView.pipeline = [];
+        expectImplicitViewTestOne.viewOn = 'testOne.implicitCollection';
+        expectImplicitViewTestOne.pipeline = [];
     }
-    audit.assertEntryForAdmin('createCollection', expectImplicitView);
+    audit.assertEntryForAdmin('createCollection', expectImplicitViewTestOne);
+
     assert.commandWorked(
-        test.createView('addZedView', 'implicitCollection', [{'$addFields': {z: 1}}]));
-    const expectZedView = {ns: 'test.addZedView'};
+        testOne.createView('addZedView', 'implicitCollection', [{'$addFields': {z: 1}}]));
+    const expectZedView = {ns: 'testOne.addZedView'};
     if (improvedAuditingEnabled) {
-        expectZedView.viewOn = 'test.implicitCollection';
+        expectZedView.viewOn = 'testOne.implicitCollection';
         expectZedView.pipeline = [{'$addFields': {z: 1}}];
     }
     audit.assertEntryForAdmin('createCollection', expectZedView);
 
-    assert.commandWorked(test.createView('explicitView', 'explicitCollection', []));
-    const expectExplicitView = {ns: 'test.explicitView'};
+    assert.commandWorked(testOne.createView('explicitView', 'explicitCollection', []));
+    const expectExplicitView = {ns: 'testOne.explicitView'};
     if (improvedAuditingEnabled) {
-        expectExplicitView.viewOn = 'test.explicitCollection';
+        expectExplicitView.viewOn = 'testOne.explicitCollection';
         expectExplicitView.pipeline = [];
     }
     audit.assertEntryForAdmin('createCollection', expectExplicitView);
 
+    assert.commandWorked(testTwo.createView('implicitView', 'implicitCollection', []));
+    const expectImplicitViewTestTwo = {ns: 'testTwo.implicitView'};
+    if (improvedAuditingEnabled) {
+        expectImplicitViewTestTwo.viewOn = 'testTwo.implicitCollection';
+        expectImplicitViewTestTwo.pipeline = [];
+    }
+    audit.assertEntryForAdmin('createCollection', expectImplicitViewTestTwo);
+
     // Drop views
-    test.explicitView.drop();
-    test.addZedView.drop();
-    test.implicitView.drop();
+    testOne.explicitView.drop();
+    testOne.addZedView.drop();
 
     if (improvedAuditingEnabled) {
         audit.assertEntryForAdmin('dropCollection', expectExplicitView);
         audit.assertEntryForAdmin('dropCollection', expectZedView);
-        audit.assertEntryForAdmin('dropCollection', expectImplicitView);
         // In sharded environments, dropping a collection or view that doesn't exist does not return
         // an error by design, but standalones return NamespaceNotFound. Both scenarios are audited
         // with the NamespaceNotFound error code.
         if (mode == 'Sharded') {
-            assert.commandWorked(test.runCommand({drop: "nonexistentView"}));
+            assert.commandWorked(testOne.runCommand({drop: "nonexistentView"}));
         } else {
-            assert.commandFailedWithCode(test.runCommand({drop: "nonexistentView"}),
+            assert.commandFailedWithCode(testOne.runCommand({drop: "nonexistentView"}),
                                          [ErrorCodes.NamespaceNotFound]);
         }
-        const expectNamespaceErrorView = {ns: 'test.nonexistentView'};
+        const expectNamespaceErrorView = {ns: 'testOne.nonexistentView'};
         expectNamespaceErrorView.viewOn = '';
         expectNamespaceErrorView.pipeline = [];
         audit.assertEntryForAdmin(
-            'dropCollection', expectNamespaceErrorView, ErrorCodes.NamespaceNotFound);
+            'dropCollection', expectNamespaceErrorView, {}, ErrorCodes.NamespaceNotFound);
     }
 
     //// Drop Collections
-    test.implicitCollection.drop();
-    audit.assertEntryForAdmin('dropCollection', {ns: 'test.implicitCollection'});
-    audit.assertEntryForAdmin('dropIndex',
-                              {ns: dropTestCollNSS('implicitCollection'), indexName: '_id_'});
-    audit.assertEntryForAdmin('dropIndex',
-                              {ns: dropTestCollNSS('implicitCollection'), indexName: 'x_1'});
-
-    test.explicitCollection.drop();
-    audit.assertEntryForAdmin('dropCollection', {ns: 'test.explicitCollection'});
-    audit.assertEntryForAdmin('dropIndex',
-                              {ns: dropTestCollNSS('explicitCollection'), indexName: '_id_'});
+    testOne.explicitCollection.drop();
+    audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.explicitCollection'});
+    audit.assertEntryForAdmin(
+        'dropIndex', {ns: dropTestCollNSS('testOne', 'explicitCollection'), indexName: '_id_'});
 
     //// Rename
-    assert.writeOK(test.origCollection.insert({x: 1}));
-    audit.assertEntryForAdmin('createCollection', {ns: 'test.origCollection'});
+    assert.writeOK(testOne.origCollection.insert({x: 1}));
+    audit.assertEntryForAdmin('createCollection', {ns: 'testOne.origCollection'});
     audit.assertEntryForAdmin('createIndex', {
-        ns: 'test.origCollection',
+        ns: 'testOne.origCollection',
         indexName: '_id_',
         indexSpec: {v: 2, key: {_id: 1}, name: '_id_'}
     });
 
-    assert.commandWorked(test.origCollection.renameCollection('newCollection', false));
+    assert.commandWorked(testOne.origCollection.renameCollection('newCollection', false));
     audit.assertEntryForAdmin('renameCollection',
-                              {old: 'test.origCollection', new: 'test.newCollection'});
+                              {old: 'testOne.origCollection', new: 'testOne.newCollection'});
 
     //// Rename with overwrite
-    assert.writeOK(test.origCollection.insert({x: 2}));
-    audit.assertEntryForAdmin('createCollection', {ns: 'test.origCollection'});
+    assert.writeOK(testOne.origCollection.insert({x: 2}));
+    audit.assertEntryForAdmin('createCollection', {ns: 'testOne.origCollection'});
     audit.assertEntryForAdmin('createIndex', {
-        ns: 'test.origCollection',
+        ns: 'testOne.origCollection',
         indexName: '_id_',
         indexSpec: {v: 2, key: {_id: 1}, name: '_id_'}
     });
 
-    assert.commandWorked(test.origCollection.renameCollection('newCollection', true));
-    audit.assertEntryForAdmin('dropCollection', {ns: 'test.newCollection'});
+    assert.commandWorked(testOne.origCollection.renameCollection('newCollection', true));
+    audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.newCollection'});
+
     {
         // Due to deferred drop via system.drop rename,
         // the order of these two entries is not guaranteed.
         // Rewind partially to allow arbitrary ordering.
         const dropCollPos = audit._auditLine;
-        audit.assertEntryForAdmin('dropIndex',
-                                  {ns: dropTestCollNSS('newCollection'), indexName: '_id_'});
+        audit.assertEntryForAdmin(
+            'dropIndex', {ns: dropTestCollNSS('testOne', 'newCollection'), indexName: '_id_'});
         audit._auditLine = dropCollPos;
         audit.assertEntryForAdmin('renameCollection',
-                                  {old: 'test.origCollection', new: 'test.newCollection'});
+                                  {old: 'testOne.origCollection', new: 'testOne.newCollection'});
     }
-    assert.eq(test.newCollection.count({}), 1);
-    assert.eq(test.newCollection.count({x: 2}), 1);
+    assert.eq(testOne.newCollection.count({}), 1);
+    assert.eq(testOne.newCollection.count({x: 2}), 1);
 
     //// Drop Database
-    assert.commandWorked(test.dropDatabase());
-    if (mode !== 'Standalone') {
-        // TODO: SERVER-50994 Collections dropped during dropDatabase only audit in sharded,
-        // not standlone.
-        audit.assertEntryForAdmin('dropCollection', {ns: 'test.newCollection'});
-        audit.assertEntryForAdmin('dropIndex',
-                                  {ns: dropTestCollNSS('newCollection'), indexName: '_id_'});
+    assert.commandWorked(testOne.dropDatabase());
+    if (mode === 'Standalone') {
+        // In standalones, dropDatabase is audited first. All views are dropped and audited before
+        // the collection drops, and the indexes in the collections being dropped are audited before
+        // the collections themselves. However, the order of the collections and their indexes
+        // relative to each other is arbitrary, so partially rewind to account for that.
+        audit.assertEntryForAdmin('dropDatabase', {ns: 'testOne'});
+        if (improvedAuditingEnabled) {
+            audit.assertEntryForAdmin('dropCollection', expectImplicitViewTestOne);
+        }
+
+        {
+            const startLine = audit._auditLine;
+            audit.assertEntryForAdmin(
+                'dropIndex', {ns: dropTestCollNSS('testOne', 'newCollection'), indexName: '_id_'});
+            audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.newCollection'});
+            audit._auditLine = startLine;
+
+            audit.assertEntryForAdmin(
+                'dropIndex',
+                {ns: dropTestCollNSS('testOne', 'implicitCollection'), indexName: '_id_'});
+            audit.assertEntryForAdmin(
+                'dropIndex',
+                {ns: dropTestCollNSS('testOne', 'implicitCollection'), indexName: 'x_1'});
+            audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.implicitCollection'});
+            audit._auditLine = startLine;
+
+            audit.assertEntryForAdmin('dropIndex',
+                                      {ns: dropTestCollNSS('testOne', 'views'), indexName: '_id_'});
+            audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.system.views'});
+        }
+    } else {
+        // In sharded environments, the collections are explicitly dropped first (again in arbitrary
+        // order). Each collection drop audit precedes the audits for the indexes in that collection
+        // being dropped. Views are audited after the `system.views` collection, and dropDatabase is
+        // audited at the end.
+        {
+            const startLine = audit._auditLine;
+            audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.newCollection'});
+            audit.assertEntryForAdmin(
+                'dropIndex', {ns: dropTestCollNSS('testOne', 'newCollection'), indexName: '_id_'});
+            audit._auditLine = startLine;
+
+            audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.implicitCollection'});
+            audit.assertEntryForAdmin(
+                'dropIndex',
+                {ns: dropTestCollNSS('testOne', 'implicitCollection'), indexName: '_id_'});
+            audit.assertEntryForAdmin(
+                'dropIndex',
+                {ns: dropTestCollNSS('testOne', 'implicitCollection'), indexName: 'x_1'});
+            audit._auditLine = startLine;
+
+            audit.assertEntryForAdmin('dropCollection', {ns: 'testOne.system.views'});
+            audit.assertEntryForAdmin('dropIndex',
+                                      {ns: dropTestCollNSS('testOne', 'views'), indexName: '_id_'});
+            if (improvedAuditingEnabled) {
+                audit.assertEntryForAdmin('dropCollection', expectImplicitViewTestOne);
+            }
+        }
+
+        audit.assertEntryForAdmin('dropDatabase', {ns: 'testOne'});
     }
-    audit.assertEntryForAdmin('dropDatabase', {ns: 'test'});
+
+    // Since views dropped during dropDatabase are audited by iterating through the ViewCatalog,
+    // this simply serves as a sanity check to ensure that the view created on database testTwo
+    // isn't audited until testTwo is actually dropped.
+    audit.assertNoEntry('dropCollection', expectImplicitViewTestTwo);
+
+    assert.commandWorked(testTwo.dropDatabase());
+    if (mode === 'Standalone') {
+        audit.assertEntryForAdmin('dropDatabase', {ns: 'testTwo'});
+        if (improvedAuditingEnabled) {
+            audit.assertEntryForAdmin('dropCollection', expectImplicitViewTestTwo);
+        }
+    } else {
+        if (improvedAuditingEnabled) {
+            audit.assertEntryForAdmin('dropCollection', expectImplicitViewTestTwo);
+        }
+        audit.assertEntryForAdmin('dropDatabase', {ns: 'testTwo'});
+    }
 
     jsTest.log('SUCCESS audit/ddl-ops.js ' + mode);
 }
