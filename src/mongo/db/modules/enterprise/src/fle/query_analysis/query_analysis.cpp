f6109612a29cdc30f8215af2d48ac17bb211853e
samontea <merciers.merciers@gmail.com>
2021-02-05 23:06:41 +0000

    SERVER-53127 Let AggregationRequestHelper::parseFromBSON() return an AggregateCommand
    GitOrigin-RevId: 71373ac59beb87526210f8be12e4cd6c14e45865

--- src/mongo/db/modules/enterprise/src/fle/query_analysis/query_analysis.cpp
+++ src/mongo/db/modules/enterprise/src/fle/query_analysis/query_analysis.cpp
@@ -358,11 +358,11 @@ PlaceHolderResult addPlaceHoldersForAggregate(
     const BSONObj& cmdObj,
     std::unique_ptr<EncryptionSchemaTreeNode> schemaTree) {
     // Parse the command to an AggregateCommand to verify that there no unknown fields.
-    auto request = uassertStatusOK(aggregation_request_helper::parseFromBSON(
+    auto request = aggregation_request_helper::parseFromBSON(
         dbName,
         cmdObj,
         boost::none,
-        APIParameters::get(expCtx->opCtx).getAPIStrict().value_or(false)));
+        APIParameters::get(expCtx->opCtx).getAPIStrict().value_or(false));
 
     // Add the populated list of involved namespaces to the expression context, needed at parse time
     // by stages such as $lookup and $out.
