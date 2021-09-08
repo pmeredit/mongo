a8e240220b33a34334689541f25cb4893e830cd7
Sara Golemon <sara.golemon@mongodb.com>
2021-09-08 16:52:10 +0000

    SERVER-59528 Disable SCRAM-SHA-1 by default in FIPS mode
    GitOrigin-RevId: 606008c47a6efa4592707418c86e76226d61a996

---
+++ src/mongo/db/modules/enterprise/jstests/fips/fips_scram_sha1.js
@@ -0,0 +1,159 @@
+// Test that FIPS mode implicitly disables SCRAM-SHA-1 authentication mechanism
+// and that it can still be explicitly enabled.
+
+(function() {
+'use strict';
+
+const SERVER_CERT = "jstests/libs/server.pem";
+const CLIENT_CERT = "jstests/libs/client.pem";
+const CA_FILE = "jstests/libs/ca.pem";
+
+function shellConnect(port, mech) {
+    let uri = 'mongodb://root:root@localhost:' + port + '/admin';
+    if (mech !== undefined) {
+        uri += '?authMechanism=' + mech;
+    }
+    return runMongoProgram('mongo',
+                           uri,
+                           '--tls',
+                           '--tlsCertificateKeyFile',
+                           CLIENT_CERT,
+                           '--tlsCAFile',
+                           CA_FILE,
+                           '--eval',
+                           ';');
+}
+
+function runTest(conn, opts) {
+    const admin = conn.getDB('admin');
+    conn.getDB('admin').createUser({user: 'root', pwd: 'root', roles: ['root']});
+    assert(conn.getDB('admin').auth('root', 'root'));
+
+    // Tri-state value.
+    // true/false: SCRAM-SHA-1 explicitly (not)set in setParameter.
+    // undefined: setParameter not present.
+    const setparam = (function(sp) {
+        if (sp.authenticationMechanisms === undefined) {
+            return undefined;
+        }
+
+        return sp.authenticationMechanisms.includes('SCRAM-SHA-1');
+    })(opts.setParameter || {});
+
+    // Directly ask the server if it supports SCRAM-SHA-1.
+    const sha1Enabled = (function() {
+        const mechs =
+            assert.commandWorked(admin.runCommand({getParameter: 1, authenticationMechanisms: 1}))
+                .authenticationMechanisms;
+        jsTest.log('authenticationMechanisms: ' + tojson(mechs));
+        return mechs.includes('SCRAM-SHA-1');
+    })();
+
+    // If there's no explicit authMechs list, and fips mode is enabled,
+    // then we should get a D1 log message about implicit disable.
+    const implicitlyDisabled = (setparam === undefined) && (opts.tlsFIPSMode !== undefined);
+    const kSHA1DisabledMsg = 5952800;
+    assert.eq(checkLog.checkContainsOnceJson(conn, kSHA1DisabledMsg, {}), implicitlyDisabled);
+
+    // If there is an explicit authMechs list without SHA1,
+    // or SHA1 got implicitly disabled,
+    // then we should not expect SHA1 support.
+    assert.eq(sha1Enabled, !((setparam === false) || implicitlyDisabled));
+
+    // Validate connecting fresh clients and using negotiation vs explicit auth methods.
+    assert.eq(shellConnect(conn.port), 0);
+    assert.eq(shellConnect(conn.port, 'SCRAM-SHA-1'), sha1Enabled ? 0 : 1);
+    assert.eq(shellConnect(conn.port, 'SCRAM-SHA-256'), 0);
+}
+
+const baseOpts = {
+    tlsMode: "requireTLS",
+    tlsCertificateKeyFile: SERVER_CERT,
+    tlsCAFile: CA_FILE,
+};
+
+const fipsOpts = Object.extend({
+    tlsFIPSMode: '',
+},
+                               baseOpts);
+
+const overrideNonFipsOpts = Object.extend({
+    setParameter: {
+        authenticationMechanisms: 'SCRAM-SHA-1,SCRAM-SHA-256',
+    },
+},
+                                          baseOpts);
+
+const overrideFipsOpts = Object.extend({
+    setParameter: {
+        authenticationMechanisms: 'SCRAM-SHA-1,SCRAM-SHA-256',
+    },
+},
+                                       fipsOpts);
+
+function testWithOpts(test) {
+    // Test non-fips mode.
+    test(baseOpts);
+
+    // Test fips mode with default mechs.
+    test(fipsOpts);
+
+    // Override non-fips.
+    test(overrideNonFipsOpts);
+
+    // Test fips mode with override mechs.
+    test(overrideFipsOpts);
+}
+
+try {
+    // Initial startup to probe OS FIPS capablity.
+    jsTest.log('Checking for OS level FIPS support');
+    clearRawMongoProgramOutput();
+    MongoRunner.stopMongod(MongoRunner.runMongod(fipsOpts));
+} catch (e) {
+    const mongoOutput = rawMongoProgramOutput();
+    jsTest.log('Probe server failed to start');
+    assert(mongoOutput.match(/this version of mongodb was not compiled with FIPS support/) ||
+           mongoOutput.match(/FIPS modes is not enabled on the operating system/) ||
+           mongoOutput.match(/FIPS_mode_set:fips mode not supported/));
+    // Acceptable failure if we don't even have FIPS mode support.
+    jsTest.log('FIPS support is not available at this time');
+    return;
+}
+
+testWithOpts(function(opts) {
+    // We don't include auth in baseOpts because mongos enabled auth via keyFile, not auth.
+    const myopts = Object.extend({auth: ''}, opts);
+    jsTest.log('Standlone: ' + tojson(myopts));
+    const conn = MongoRunner.runMongod(myopts);
+    runTest(conn, myopts);
+    MongoRunner.stopMongod(conn);
+    jsTest.log('SUCCESS - standalone mongod');
+});
+
+testWithOpts(function(opts) {
+    function clone(opts) {
+        // Clone parts of the options object because Javascript
+        // ShardingTest mutates opts.setParameters without first
+        // cloning it. TODO(SERVER-59909)
+        const ret = Object.assign({}, opts);
+        if (ret.setParameter) {
+            ret.setParameter = Object.assign({}, ret.setParameter);
+        }
+        return ret;
+    }
+
+    jsTest.log('Sharding: ' + tojson(opts));
+    const st = new ShardingTest({
+        mongos: [clone(opts)],
+        config: [clone(opts)],
+        rs: {nodes: [clone(opts)]},
+        shards: 1,
+        useHostname: false,
+        other: {keyFile: 'jstests/libs/key1'},
+    });
+    runTest(st.s0, opts);
+    st.stop();
+    jsTest.log('SUCCESS - sharding');
+});
+})();
---
+++ src/mongo/db/modules/enterprise/src/fips/fips_flag.cpp
@@ -0,0 +1,81 @@
+/**
+ *    Copyright (C) 2021 MongoDB Inc.
+ */
+
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kNetwork
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/base/init.h"
+#include "mongo/db/auth/sasl_options.h"
+#include "mongo/logv2/log.h"
+#include "mongo/util/net/ssl_options.h"
+#include "mongo/util/options_parser/startup_option_init.h"
+#include "mongo/util/options_parser/startup_options.h"
+
+namespace moe = mongo::optionenvironment;
+
+namespace mongo {
+namespace {
+
+/**
+ * Helper for activating on either net.tls.FIPSMode or tls.FIPSMode.
+ * The former is used in server mode, the latter in client mode.
+ */
+bool isEnabled(const std::string& key) {
+    const auto& params = mongo::optionenvironment::startupOptionsParsed;
+    return params.count(key) && params[key].as<bool>();
+}
+
+/**
+ * Scans current saslGlobalOptions for usage of SCRAM-SHA-1.
+ */
+bool hasSCRAMSHA1() {
+    return std::any_of(saslGlobalParams.authenticationMechanisms.cbegin(),
+                       saslGlobalParams.authenticationMechanisms.cend(),
+                       [](const auto& v) { return v == "SCRAM-SHA-1"_sd; });
+}
+
+/**
+ * When in FIPS mode, we want to disable SCRAM-SHA-1 as an authentication mechanism
+ * unless it has been explicit enabled via setParameter.
+ */
+MONGO_STARTUP_OPTIONS_POST(FipsOptionInit)(InitializerContext*) {
+    const auto& params = mongo::optionenvironment::startupOptionsParsed;
+
+    if (!isEnabled("net.tls.FIPSMode") && !isEnabled("tls.FIPSMode")) {
+        // FIPS mode not enabled.
+        return;
+    }
+
+    if (params.count("setParameter")) {
+        auto sp = params["setParameter"].as<std::map<std::string, std::string>>();
+        if (sp.find("authenticationMechanisms") != sp.end()) {
+            // Explicit override via configuration, accept it.
+            return;
+        }
+    }
+
+    if (!hasSCRAMSHA1()) {
+        // SCRAM-SHA-1 isn't enabled to begin with.
+        // As of this writing, this will never pass,
+        // but this protects future-us from wasting too much time.
+        return;
+    }
+
+    // FIPS mode is enabled, and the config does not explicitly ask for SCRAM-SHA-1.
+    // Prefer std::erase() in C++20, but for now do a simple copy and replace.
+    LOGV2_DEBUG(5952800, 1, "Implicitly disabling SCRAM-SHA-1 due to FIPS mode");
+    auto& authMechs = saslGlobalParams.authenticationMechanisms;
+    typename std::remove_reference_t<decltype(authMechs)> mechs;
+    std::copy_if(authMechs.cbegin(),
+                 authMechs.cend(),
+                 std::back_inserter(mechs),
+                 [](const auto& v) { return v != "SCRAM-SHA-1"_sd; });
+    authMechs = std::move(mechs);
+
+    invariant(!hasSCRAMSHA1());
+}
+
+}  // namespace
+}  // namespace mongo
