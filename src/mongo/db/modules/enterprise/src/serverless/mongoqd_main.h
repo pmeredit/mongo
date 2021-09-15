3d26ba3a8ea67c66c2c43a85079752976a079d94
jannaerin <golden.janna@gmail.com>
2021-09-15 19:56:42 +0000

    SERVER-59918 Create mongoqd binary
    GitOrigin-RevId: 40f1f254337c99be25ddb4e715d56c152e187218

---
+++ src/mongo/db/modules/enterprise/src/serverless/mongoqd_main.h
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2021 MongoDB, Inc.  All Rights Reserved.
+ */
+
+#include "mongo/util/exit_code.h"
+
+namespace mongo {
+
+ExitCode mongoqd_main(int argc, char* argv[]);
+
+}  // namespace mongo
