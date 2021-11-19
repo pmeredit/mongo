af1c5bc088f4a8eb862deaf7b3e179743f931ec2
mathisbessamdb <mathis.bessa@mongodb.com>
2021-11-19 16:20:55 +0000

    SERVER-61424 adding TenantToShardCache and link it to mongoq
    SERVER-61424 adding tenant_to_shard_cache to be built with mongos, initialized and called in sharding_initialization
    
    SERVER-61424 - some sconscript cleanup
    
    SERVER-61424 running clang format
    
    SERVER-61424 running clang format
    
    SERVER-61424 some more cleanup
    
    removing the file from branch
    
    SERVER-61424 nit rework
    
    SERVER-61424 changing copyright for enterprise since it doesnt have SSPL
    
    SERVER-61424 adding TenantToShardCache and link it to mongoq
    
    SERVER-61424 adding tenant_to_shard_cache to be built with mongos, initialized and called in sharding_initialization
    
    SERVER-61424 some sconscript cleanup
    
    SERVER-61424 running clang format
    
    SERVER-61424 running clang format
    
    SERVER-61424 some more cleanup
    
    SERVER-61424 removing the file from branch
    
    SERVER-61424 changing copyright for enterprise since it doesnt have SSPL
    
    SERVER-61424 some sconscript cleanup
    
    SERVER-61424 running clang format
    
    SERVER-61424 removing the file from branch
    
    SERVER-61424 no longer use initialize_tenant_to_shard_cache since it was moved to sharding_initialization llibrary
    
    SERVER-61424 no longer use initialize_tenant_to_shard_cache since it was moved to sharding_initialization llibrary
    
    GitOrigin-RevId: 9b6b5a25a4d882eb1095883869ffaa9ece85da82

---
+++ src/mongo/db/modules/enterprise/src/serverless/tenant_to_shard_cache.cpp
@@ -0,0 +1,43 @@
+/**
+ *    Copyright (C) 2021-present MongoDB, Inc.
+ */
+
+
+#include "tenant_to_shard_cache.h"
+#include "mongo/base/init.h"
+#include "mongo/db/service_context.h"
+#include "mongo/platform/basic.h"
+#include "mongo/s/initialize_tenant_to_shard_cache.h"
+
+namespace mongo {
+
+namespace {
+Mutex tenantToShardCacheMutex;  // protects access to decoration instance of TenantToShardCache.
+}
+
+const auto getTenantToShardCache =
+    ServiceContext::declareDecoration<std::unique_ptr<TenantToShardCache>>();
+
+TenantToShardCache* TenantToShardCache::get(ServiceContext* service) {
+    stdx::lock_guard<Latch> lk(tenantToShardCacheMutex);
+    return getTenantToShardCache(service).get();
+}
+
+void TenantToShardCache::set(ServiceContext* service,
+                             std::unique_ptr<TenantToShardCache> newTenantToShardCache) {
+    stdx::lock_guard<Latch> lk(tenantToShardCacheMutex);
+    auto& tenantToShardCache = getTenantToShardCache(service);
+    tenantToShardCache = std::move(newTenantToShardCache);
+}
+
+TenantToShardCache::TenantToShardCache() {}
+
+void TenantToShardCache::init(ServiceContext* service) {
+    TenantToShardCache::set(service, std::make_unique<TenantToShardCache>());
+}
+
+MONGO_INITIALIZER(InitializeTenantToShardCache)(InitializerContext*) {
+    registerTenantToShardCacheInitializer(&TenantToShardCache::init);
+}
+
+}  // namespace mongo
---
+++ src/mongo/db/modules/enterprise/src/serverless/tenant_to_shard_cache.h
@@ -0,0 +1,26 @@
+/**
+ *    Copyright (C) 2021-present MongoDB, Inc.
+ */
+
+#pragma once
+
+#include "mongo/db/keys_collection_document_gen.h"
+#include <memory>
+
+namespace mongo {
+
+class ServiceContext;
+
+class TenantToShardCache {
+public:
+    // Decorate ServiceContext with TenantToShardCache instance.
+    static TenantToShardCache* get(ServiceContext* service);
+    static void set(ServiceContext* service,
+                    std::unique_ptr<TenantToShardCache> tenantToShardCache);
+
+    static void init(ServiceContext* service);
+
+    explicit TenantToShardCache();
+};
+
+}  // namespace mongo
