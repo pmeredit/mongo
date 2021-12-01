881eaf1c411dca473253de28b2408b2134b95979
sergey.galtsev <sergey.galtsev@mongodb.com>
2021-12-01 16:20:43 +0000

    SERVER-61571 build csfle library and unit test
    GitOrigin-RevId: 0e647a07f3aaafd42180fd7081d279a4aefb89ba

---
+++ src/mongo/db/modules/enterprise/src/fle/lib/mongo_csfle.cpp
@@ -0,0 +1,352 @@
+/**
+ *    Copyright (C) 2021-present MongoDB, Inc.
+ */
+
+
+#include "mongo/platform/basic.h"
+
+#include "mongo_csfle.h"
+
+#include "fle/query_analysis/query_analysis.h"
+#include "mongo/base/initializer.h"
+#include "mongo/bson/bsonobj.h"
+#include "mongo/db/concurrency/locker_noop_client_observer.h"
+#include "mongo/db/exec/projection_executor.h"
+#include "mongo/db/exec/projection_executor_builder.h"
+#include "mongo/db/matcher/matcher.h"
+#include "mongo/db/namespace_string.h"
+#include "mongo/db/ops/parsed_update_array_filters.h"
+#include "mongo/db/query/collation/collator_factory_interface.h"
+#include "mongo/db/query/projection_parser.h"
+#include "mongo/db/service_context.h"
+#include "mongo/db/update/update_driver.h"
+#include "mongo/embedded/api_common.h"
+#include "mongo/rpc/op_msg_rpc_impls.h"
+#include "mongo/util/assert_util.h"
+#include "mongo/util/version.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+
+#if defined(_WIN32)
+#define MONGO_API_CALL __cdecl
+#else
+#define MONGO_API_CALL
+#endif
+
+namespace mongo {
+
+using CsfleSupportStatusImpl = StatusForAPI<mongo_csfle_v1_error>;
+
+const NamespaceString kDummyNamespaceStr = NamespaceString("");
+
+/**
+ * C interfaces that use enterCXX() must provide a translateException() function that converts any
+ * possible exception into a StatusForAPI<> object.
+ */
+static CsfleSupportStatusImpl translateException(stdx::type_identity<CsfleSupportStatusImpl>) try {
+    throw;
+} catch (const ExceptionFor<ErrorCodes::ReentrancyNotAllowed>& ex) {
+    return {MONGO_CSFLE_V1_ERROR_REENTRANCY_NOT_ALLOWED, ex.code(), ex.what()};
+} catch (const DBException& ex) {
+    return {MONGO_CSFLE_V1_ERROR_EXCEPTION, ex.code(), ex.what()};
+} catch (const ExceptionForAPI<mongo_csfle_v1_error>& ex) {
+    return {ex.statusCode(), mongo::ErrorCodes::InternalError, ex.what()};
+} catch (const std::bad_alloc& ex) {
+    return {MONGO_CSFLE_V1_ERROR_ENOMEM, mongo::ErrorCodes::InternalError, ex.what()};
+} catch (const std::exception& ex) {
+    return {MONGO_CSFLE_V1_ERROR_UNKNOWN, mongo::ErrorCodes::InternalError, ex.what()};
+} catch (...) {
+    return {MONGO_CSFLE_V1_ERROR_UNKNOWN,
+            mongo::ErrorCodes::InternalError,
+            "Unknown error encountered in performing requested mongo_csfle_v1 operation"};
+}
+
+/**
+ * C interfaces that use enterCXX() must provide a tranlsateExceptionFallback() function that
+ * populates a StatusForAPI<> object to indicate a double-fault error during error reporting. The
+ * translateExceptionFallback() function gets called when translateException() throws, and it should
+ * not include any code that may itself throw.
+ *
+ * We use an out param instead of returning the StatusForAPI<> object so as to avoid a std::string
+ * copy that may allocate memory.
+ */
+static void translateExceptionFallback(CsfleSupportStatusImpl& status) noexcept {
+    status.error = MONGO_CSFLE_V1_ERROR_IN_REPORTING_ERROR;
+    status.exception_code = -1;
+    setErrorMessageNoAlloc(status.what);
+}
+
+BSONObj analyzeQuery(const BSONObj document, OperationContext* opCtx, const NamespaceString ns) {
+
+    mongo::OpMsgRequest opmsg;
+    opmsg.body = document;
+    const StringData commandName = document.firstElementFieldName();
+
+    BSONObjBuilder schemaInfoBuilder;
+    if (commandName == "find"_sd) {
+        cryptd_query_analysis::processFindCommand(
+            opCtx, ns.db().toString(), document, &schemaInfoBuilder);
+    } else if (commandName == "aggregate"_sd) {
+        cryptd_query_analysis::processAggregateCommand(
+            opCtx, ns.db().toString(), document, &schemaInfoBuilder);
+    } else if (commandName == "findandmodify"_sd || commandName == "findAndModify"_sd) {
+        cryptd_query_analysis::processFindAndModifyCommand(
+            opCtx, ns.db().toString(), document, &schemaInfoBuilder);
+    } else if (commandName == "count"_sd) {
+        cryptd_query_analysis::processCountCommand(
+            opCtx, ns.db().toString(), document, &schemaInfoBuilder);
+    } else if (commandName == "distinct"_sd) {
+        cryptd_query_analysis::processDistinctCommand(
+            opCtx, ns.db().toString(), document, &schemaInfoBuilder);
+    } else if (commandName == "update"_sd) {
+        cryptd_query_analysis::processUpdateCommand(opCtx, opmsg, &schemaInfoBuilder);
+    } else if (commandName == "insert"_sd) {
+        cryptd_query_analysis::processInsertCommand(opCtx, opmsg, &schemaInfoBuilder);
+    } else if (commandName == "delete"_sd) {
+        cryptd_query_analysis::processDeleteCommand(opCtx, opmsg, &schemaInfoBuilder);
+    } else {
+        // TODO
+        invariant(false);
+    }
+
+    return schemaInfoBuilder.obj();
+}
+
+}  // namespace mongo
+
+struct mongo_csfle_v1_status {
+    mongo::CsfleSupportStatusImpl statusImpl;
+};
+
+namespace mongo {
+
+namespace {
+
+CsfleSupportStatusImpl* getStatusImpl(mongo_csfle_v1_status* status) {
+    return status ? &status->statusImpl : nullptr;
+}
+
+using CsfleSupportException = ExceptionForAPI<mongo_csfle_v1_error>;
+
+ServiceContext* initialize() {
+    srand(static_cast<unsigned>(curTimeMicros64()));
+
+    // The global initializers can take arguments, which would normally be supplied on the command
+    // line, but we assume that clients of this library will never want anything other than the
+    // defaults for all configuration that would be controlled by these parameters.
+    Status status = mongo::runGlobalInitializers(std::vector<std::string>{});
+    uassertStatusOKWithContext(status, "Global initialization failed");
+    setGlobalServiceContext(ServiceContext::make());
+    auto serviceContext = getGlobalServiceContext();
+
+    serviceContext->registerClientObserver(std::make_unique<LockerNoopClientObserver>());
+
+    return serviceContext;
+}
+
+struct ServiceContextDestructor {
+    /**
+     * This destructor gets called when the CSFLE Library gets torn down, either by a call to
+     * mongo_csfle_v1_destroy() or when the process exits.
+     */
+    void operator()(mongo::ServiceContext* const serviceContext) const noexcept {
+        Status status = mongo::runGlobalDeinitializers();
+        uassertStatusOKWithContext(status, "Global deinitilization failed");
+
+        setGlobalServiceContext(nullptr);
+    }
+};
+
+using EmbeddedServiceContextPtr = std::unique_ptr<mongo::ServiceContext, ServiceContextDestructor>;
+}  // namespace
+}  // namespace mongo
+
+struct mongo_csfle_v1_lib {
+    mongo_csfle_v1_lib() : serviceContext(mongo::initialize()) {}
+
+    mongo_csfle_v1_lib(const mongo_csfle_v1_lib&) = delete;
+    void operator=(const mongo_csfle_v1_lib&) = delete;
+
+    mongo::EmbeddedServiceContextPtr serviceContext;
+};
+
+struct mongo_csfle_v1_query_analyzer {
+    mongo_csfle_v1_query_analyzer(mongo::ServiceContext::UniqueClient client)
+        : client(std::move(client)), opCtx(this->client->makeOperationContext()) {}
+
+
+    mongo::ServiceContext::UniqueClient client;
+    mongo::ServiceContext::UniqueOperationContext opCtx;
+};
+
+
+namespace mongo {
+namespace {
+
+std::unique_ptr<mongo_csfle_v1_lib> library;
+
+mongo_csfle_v1_lib* csfle_lib_init() {
+    if (library) {
+        throw CsfleSupportException{
+            MONGO_CSFLE_V1_ERROR_LIBRARY_ALREADY_INITIALIZED,
+            "Cannot initialize the CSFLE Support Library when it is already initialized."};
+    }
+
+    library = std::make_unique<mongo_csfle_v1_lib>();
+
+    return library.get();
+}
+
+void csfle_lib_fini(mongo_csfle_v1_lib* const lib) {
+    if (!lib) {
+        throw CsfleSupportException{
+            MONGO_CSFLE_V1_ERROR_INVALID_LIB_HANDLE,
+            "Cannot close a `NULL` pointer referencing a CSFLE Support Library Instance"};
+    }
+
+    if (!library) {
+        throw CsfleSupportException{
+            MONGO_CSFLE_V1_ERROR_LIBRARY_NOT_INITIALIZED,
+            "Cannot close the CSFLE Support Library when it is not initialized"};
+    }
+
+    if (library.get() != lib) {
+        throw CsfleSupportException{MONGO_CSFLE_V1_ERROR_INVALID_LIB_HANDLE,
+                                    "Invalid CSFLE Support Library handle."};
+    }
+
+    library.reset();
+}
+
+mongo_csfle_v1_query_analyzer* query_analyzer_create(mongo_csfle_v1_lib* const lib) {
+    if (!library) {
+        throw CsfleSupportException{MONGO_CSFLE_V1_ERROR_LIBRARY_NOT_INITIALIZED,
+                                    "Cannot create a new collator when the CSFLE Support Library "
+                                    "is not yet initialized."};
+    }
+
+    if (library.get() != lib) {
+        throw CsfleSupportException{MONGO_CSFLE_V1_ERROR_INVALID_LIB_HANDLE,
+                                    "Cannot create a new collator when the CSFLE Support Library "
+                                    "is not yet initialized."};
+    }
+
+    return new mongo_csfle_v1_query_analyzer(lib->serviceContext->makeClient("csfle_support"));
+}
+
+int capi_status_get_error(const mongo_csfle_v1_status* const status) noexcept {
+    invariant(status);
+    return status->statusImpl.error;
+}
+
+const char* capi_status_get_what(const mongo_csfle_v1_status* const status) noexcept {
+    invariant(status);
+    return status->statusImpl.what.c_str();
+}
+
+int capi_status_get_code(const mongo_csfle_v1_status* const status) noexcept {
+    invariant(status);
+    return status->statusImpl.exception_code;
+}
+
+/**
+ * toInterfaceType changes the compiler's interpretation from our internal BSON type 'char*' to
+ * 'uint8_t*' which is the interface type of the CSFLE library.
+ */
+auto toInterfaceType(char* bson) noexcept {
+    return static_cast<uint8_t*>(static_cast<void*>(bson));
+}
+
+/**
+ * fromInterfaceType changes the compiler's interpretation from 'uint8_t*' which is the BSON
+ * interface type of the CSFLE library to our internal type 'char*'.
+ */
+auto fromInterfaceType(const uint8_t* bson) noexcept {
+    return static_cast<const char*>(static_cast<const void*>(bson));
+}
+
+}  // namespace
+}  // namespace mongo
+
+extern "C" {
+
+mongo_csfle_v1_lib* MONGO_API_CALL mongo_csfle_v1_create(mongo_csfle_v1_status* status) {
+    return enterCXX(mongo::getStatusImpl(status), [&]() { return mongo::csfle_lib_init(); });
+}
+
+int MONGO_API_CALL mongo_csfle_v1_destroy(mongo_csfle_v1_lib* const lib,
+                                          mongo_csfle_v1_status* const status) {
+    return enterCXX(mongo::getStatusImpl(status), [&]() { return mongo::csfle_lib_fini(lib); });
+}
+
+int MONGO_API_CALL mongo_csfle_v1_status_get_error(const mongo_csfle_v1_status* const status) {
+    return mongo::capi_status_get_error(status);
+}
+
+const char* MONGO_API_CALL
+mongo_csfle_v1_status_get_explanation(const mongo_csfle_v1_status* const status) {
+    return mongo::capi_status_get_what(status);
+}
+
+int MONGO_API_CALL mongo_csfle_v1_status_get_code(const mongo_csfle_v1_status* const status) {
+    return mongo::capi_status_get_code(status);
+}
+
+mongo_csfle_v1_status* MONGO_API_CALL mongo_csfle_v1_status_create(void) {
+    return new mongo_csfle_v1_status;
+}
+
+void MONGO_API_CALL mongo_csfle_v1_status_destroy(mongo_csfle_v1_status* const status) {
+    delete status;
+}
+
+mongo_csfle_v1_query_analyzer* MONGO_API_CALL
+mongo_csfle_v1_query_analyzer_create(mongo_csfle_v1_lib* lib, mongo_csfle_v1_status* const status) {
+    return enterCXX(mongo::getStatusImpl(status),
+                    [&]() { return mongo::query_analyzer_create(lib); });
+}
+
+void MONGO_API_CALL
+mongo_csfle_v1_query_analyzer_destroy(mongo_csfle_v1_query_analyzer* const collator) {
+    mongo::CsfleSupportStatusImpl* nullStatus = nullptr;
+    static_cast<void>(enterCXX(nullStatus, [=]() { delete collator; }));
+}
+
+uint8_t* MONGO_API_CALL mongo_csfle_v1_analyze_query(mongo_csfle_v1_query_analyzer* matcher,
+                                                     const uint8_t* documentBSON,
+                                                     const char* ns_str,
+                                                     uint32_t ns_len,
+                                                     uint32_t* bson_len,
+                                                     mongo_csfle_v1_status* status) {
+    return enterCXX(mongo::getStatusImpl(status), [&]() {
+        mongo::BSONObj document(mongo::fromInterfaceType(documentBSON));
+
+        mongo::StringData ns_sd(ns_str, ns_len);
+        mongo::NamespaceString ns(ns_sd);
+        mongo::OperationContext* opCtx = matcher->opCtx.get();
+
+        auto result = mongo::analyzeQuery(document, opCtx, ns);
+
+        // TODO -cleanup
+        auto outputSize = static_cast<size_t>(result.objsize());
+        auto output = new (std::nothrow) char[outputSize];
+
+        uassert(mongo::ErrorCodes::ExceededMemoryLimit,
+                "Failed to allocate memory for projection",
+                output);
+
+        static_cast<void>(std::copy_n(result.objdata(), outputSize, output));
+        *bson_len = result.objsize();
+        return mongo::toInterfaceType(output);
+    });
+}
+
+
+void MONGO_API_CALL mongo_csfle_v1_bson_free(uint8_t* bson) {
+    mongo::CsfleSupportStatusImpl* nullStatus = nullptr;
+    static_cast<void>(enterCXX(nullStatus, [=]() { delete[](bson); }));
+}
+
+}  // extern "C"
---
+++ src/mongo/db/modules/enterprise/src/fle/lib/mongo_csfle_test.cpp
@@ -0,0 +1,176 @@
+/**
+ *    Copyright (C) 2021-present MongoDB, Inc.
+ */
+
+#include <algorithm>
+#include <string>
+#include <utility>
+
+#include "fle/lib/mongo_csfle.h"
+
+#include "mongo/base/initializer.h"
+#include "mongo/bson/json.h"
+#include "mongo/db/service_context_test_fixture.h"
+#include "mongo/unittest/unittest.h"
+#include "mongo/util/quick_exit.h"
+#include "mongo/util/scopeguard.h"
+
+namespace {
+
+using mongo::ScopeGuard;
+
+class CsfleTest : public mongo::unittest::Test {
+protected:
+    void setUp() override {
+        status = mongo_csfle_v1_status_create();
+        ASSERT(status);
+
+        lib = mongo_csfle_v1_create(status);
+        ASSERT(lib);
+    }
+
+    void tearDown() override {
+        int code = mongo_csfle_v1_destroy(lib, status);
+        ASSERT_EQ(code, MONGO_CSFLE_V1_SUCCESS);
+        lib = nullptr;
+
+        mongo_csfle_v1_status_destroy(status);
+        status = nullptr;
+    }
+
+    /**
+     * toBSONForAPI is a custom converter from json which satisfies the unusual uint8_t type that
+     * mongo_csfle uses for BSON. The intermediate BSONObj is also returned since its lifetime
+     * governs the lifetime of the uint8_t*.
+     */
+    auto toBSONForAPI(const char* json) {
+        auto bson = mongo::fromjson(json);
+        return std::make_pair(static_cast<const uint8_t*>(static_cast<const void*>(bson.objdata())),
+                              bson);
+    }
+
+    /**
+     * fromBSONForAPI is a custom converter to json which satisfies the unusual uint8_t type that
+     * mongo_csfle uses for BSON.
+     */
+    auto fromBSONForAPI(const uint8_t* bson) {
+        return mongo::tojson(
+            mongo::BSONObj(static_cast<const char*>(static_cast<const void*>(bson))),
+            mongo::JsonStringFormat::LegacyStrict);
+    }
+
+
+    auto checkAnalysis(const char* inputJSON, const char* outputJSON) {
+        auto matcher = mongo_csfle_v1_query_analyzer_create(lib, nullptr);
+        ASSERT(matcher);
+        ON_BLOCK_EXIT([matcher] { mongo_csfle_v1_query_analyzer_destroy(matcher); });
+
+        uint32_t bson_len = 0;
+        auto ret = mongo_csfle_v1_analyze_query(
+            matcher, toBSONForAPI(inputJSON).first, "db.test", 7, &bson_len, nullptr);
+        ON_BLOCK_EXIT([ret] { mongo_csfle_v1_bson_free(ret); });
+        auto sret = fromBSONForAPI(ret);
+
+        ASSERT_EQ(sret, outputJSON);
+
+        return true;
+    }
+
+    mongo_csfle_v1_status* status = nullptr;
+    mongo_csfle_v1_lib* lib = nullptr;
+};
+
+TEST_F(CsfleTest, InitializationIsSuccessful) {
+    ASSERT_EQ(MONGO_CSFLE_V1_SUCCESS, mongo_csfle_v1_status_get_error(status));
+    ASSERT(lib);
+}
+
+TEST_F(CsfleTest, DoubleInitializationFails) {
+    auto lib2 = mongo_csfle_v1_create(status);
+
+    ASSERT(!lib2);
+    ASSERT_EQ(MONGO_CSFLE_V1_ERROR_LIBRARY_ALREADY_INITIALIZED,
+              mongo_csfle_v1_status_get_error(status));
+}
+
+TEST_F(CsfleTest, CheckMatchWorksWithDefaults) {
+    ASSERT_TRUE(checkAnalysis(
+        R"({
+        "find": "test",
+        "filter": {
+            "user.account": "secret"
+        },
+        "jsonSchema": {
+            "type": "object",
+            "properties": {
+                "ssn": {
+                    "encrypt": {
+                        "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic",
+                        "keyId": [
+                            {
+                                "$uuid": "1362d0ed-6182-478e-bb8a-ebcc53b91aa1"
+                            }
+                        ],
+                        "bsonType": "long"
+                    }
+                },
+                "user": {
+                    "type": "object",
+                    "properties": {
+                        "account": {
+                            "encrypt": {
+                                "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic",
+                                "keyId": [
+                                    {
+                                        "$uuid": "93282c77-9a6b-47cf-9c4c-beda02730881"
+                                    }
+                                ],
+                                "bsonType": "string"
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        "isRemoteSchema": false,
+        "lsid": {
+            "id": {
+                "$uuid": "32de9140-7ade-46bf-a72a-49442e4e93d7"
+            }
+        },
+        "$db": "test"
+    })",
+        R"({ "hasEncryptionPlaceholders" : true, "schemaRequiresEncryption" : true, "result" : )"
+        R"({ "find" : "test", "filter" : { "user.account" : { "$eq" : { "$binary" : )"
+        R"("ADMAAAAQYQABAAAABWtpABAAAAAEkygsd5prR8+cTL7aAnMIgQJ2AAcAAABzZWNyZXQAAA==", "$type" : )"
+        R"("06" } } }, "lsid" : { "id" : { "$binary" : "Mt6RQHreRr+nKklELk6T1w==", )"
+        R"("$type" : "04" } } } })"));
+}
+
+}  // namespace
+
+// Define main function as an entry to these tests.
+//
+// Note that we don't use the main() defined for most other unit tests so that we can avoid double
+// calling runGlobalInitializers(), which is called both from the regular unit test main() and from
+// the CSFLE Library intializer function that gets tested here.
+int main(const int argc, const char* const* const argv) {
+    // See comment by the same code block in mongo_embedded_test.cpp
+    auto ret = mongo::runGlobalInitializers(std::vector<std::string>{argv, argv + argc});
+    if (!ret.isOK()) {
+        std::cerr << "Global initilization failed";
+        return EXIT_FAILURE;
+    }
+
+    ret = mongo::runGlobalDeinitializers();
+    if (!ret.isOK()) {
+        std::cerr << "Global deinitilization failed";
+        return EXIT_FAILURE;
+    }
+
+    const auto result = ::mongo::unittest::Suite::run(std::vector<std::string>(), "", "", 1);
+
+    // This is the standard exit path for Mongo processes. See the mongo::quickExit() declaration
+    // for more information.
+    mongo::quickExit(result);
+}
