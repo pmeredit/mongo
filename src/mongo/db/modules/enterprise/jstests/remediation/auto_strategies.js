3be1b195d3627b4cbd69e9e2c809c39e54cc013c
Matthew Russotto <matthew.russotto@mongodb.com>
2022-01-14 13:57:27 -0500

    SERVER-62464 Move remediation script to permanent location
    GitOrigin-RevId: 499eb57d5d86d59dc69c9e9f4be2fcd73954684e

---
+++ src/mongo/db/modules/enterprise/jstests/remediation/auto_strategies.js
@@ -0,0 +1,290 @@
+/**
+ * auto_strategies.js
+ *
+ * This script creates a replica set with the collections expected to be found after runnning
+ * scan_checked_replset.js over a corrupted 5-node replica set.  It then runs
+ * repair_checked_documents.py with each of the remediation options, and checks that remediation
+ * occurred as expected.
+ *
+ * Note this test does not actually create a corrupt replset, nor does it check actual remediation.
+ *
+ */
+(function() {
+"use strict";
+
+const strategyNames = [
+    "majority",
+    "majorityDeletePluralityKeep",
+    "majorityKeepPluralityDelete",
+    "plurality",
+    "majorityKeepNeverDelete",
+    "pluralityKeepNeverDelete",
+    "majorityDeleteNeverKeep",
+    "pluralityDeleteNeverKeep",
+];
+
+const missingDocMarker = "dbcheck_docWasMissing";
+const NC = "no change";
+const rangeCollName = "unhealthyRanges";
+const metadataDbName = "config";
+const saveCollPrefix = "dbcheck.";
+const testCollName = "test";
+const scriptUnderTest = "src/mongo/db/modules/enterprise/src/scripts/repair_checked_documents.py";
+
+let python_binary = '/opt/mongodbtoolchain/v3/bin/python3';
+if (runProgram('/bin/sh', '-c', 'ls ' + python_binary) !== 0) {
+    python_binary = 'python3';
+}
+
+const testDocs = [
+    // Consistent documents shouldn't appear in the scan, but if they do, they should behave
+    // properly.
+    {
+        versions: [1, 1, 1, 1, 1],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Consistent document"
+    },
+
+    // Majority documents
+    {
+        versions: [1, 1, 1, 1, 2],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Two versions 4:1"
+    },
+    {
+        versions: [1, 1, 1, 1, null],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Four node same, one missing"
+    },
+    {
+        versions: [1, 1, 1, 2, 2],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Two versions 3:2"
+    },
+    {
+        versions: [1, 1, 1, 2, 3],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Three versions 3:1:1"
+    },
+    {
+        versions: [1, 1, 1, 2, null],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Two versions 3:1:1 missing"
+    },
+    {
+        versions: [1, 1, 1, null, null],
+        correctVersion: [1, 1, 1, 1, 1, 1, NC, NC],
+        msg: "Three same, two missing"
+    },
+
+    // Consistent missing
+    {
+        versions: [null, null, null, null, null],
+        correctVersion: [null, null, null, null, NC, NC, null, null],
+        msg: "Consistent missing document"
+    },
+
+    // Majority missing
+    {
+        versions: [null, null, null, null, 1],
+        correctVersion: [null, null, null, null, NC, NC, null, null],
+        msg: "4 missing:1"
+    },
+    {
+        versions: [null, null, null, 1, 1],
+        correctVersion: [null, null, null, null, NC, NC, null, null],
+        msg: "3 missing:2"
+    },
+    {
+        versions: [null, null, null, 1, 2],
+        correctVersion: [null, null, null, null, NC, NC, null, null],
+        msg: "3 missing:1:1"
+    },
+
+    // Two present, no majority.
+    {versions: [1, 1, 2, 2, 3], correctVersion: [NC, NC, NC, NC, NC, NC, NC, NC], msg: "2:2:1"},
+    {
+        versions: [1, 1, 2, 2, null],
+        correctVersion: [NC, NC, NC, NC, NC, NC, NC, NC],
+        msg: "2:2:1 missing"
+    },
+    {
+        versions: [1, 1, null, null, 2],
+        correctVersion: [NC, 1, null, NC, NC, 1, NC, null],
+        msg: "2:2 missing:1"
+    },
+    {versions: [1, 1, 2, 3, 4], correctVersion: [NC, 1, NC, 1, NC, 1, NC, NC], msg: "2:1:1:1"},
+
+    // Two missing, no majority (other cases are covered above)
+    {
+        versions: [1, 2, 3, null, null],
+        correctVersion: [NC, NC, null, null, NC, NC, NC, null],
+        msg: "1:1:1:2 missing"
+    },
+
+    // All different
+    {
+        versions: [1, 2, 3, 4, 5],
+        correctVersion: [NC, NC, NC, NC, NC, NC, NC, NC],
+        msg: "All different"
+    },
+    {
+        versions: [1, 2, 3, 4, null],
+        correctVersion: [NC, NC, null, NC, NC, NC, NC, null],
+        msg: "All different one missing"
+    },
+];
+
+function createTestData(db) {
+    let startId = 1;
+    let endId = startId;
+    let saveCollNames = [];
+    db[testCollName].drop();
+    for (let i = 1; i <= 5; i++) {
+        let name = saveCollPrefix + testCollName + "." + i;
+        db[name].drop();
+        saveCollNames.push(name);
+    }
+
+    for (let testDoc of testDocs) {
+        let unchangedDoc = {_id: endId, msg: testDoc.msg, version: NC};
+        assert.commandWorked(db[testCollName].insert(unchangedDoc));
+        for (let i = 0; i < 5; i++) {
+            let saveCollName = saveCollNames[i];
+            let doc;
+            if (testDoc.versions[i] === null) {
+                doc = {_id: endId};
+                doc[missingDocMarker] = 1;
+            } else
+                doc = {_id: endId, msg: testDoc.msg, version: testDoc.versions[i]};
+            assert.commandWorked(db[saveCollName].insert(doc));
+        }
+        endId++;
+    }
+    let metadataDb = db.getSiblingDB(metadataDbName);
+    metadataDb[rangeCollName].remove({"_id.db": db.getName(), "_id.collection": testCollName});
+    assert.commandWorked(metadataDb[rangeCollName].insert({
+        _id: {db: db.getName(), collection: testCollName, minKey: startId, maxKey: endId},
+        scanned: true,
+        scanCollections: saveCollNames
+    }));
+}
+
+function checkTestDataResults(db, strategy, dryrun = false) {
+    let currentId = 1;
+    let wrongDocs = 0;
+    const strategyName = strategyNames[strategy];
+    for (let testDoc of testDocs) {
+        let correctDoc;
+        let resultDoc = db[testCollName].findOne({_id: currentId});
+        let correctVersion = NC;
+        if (!dryrun)
+            correctVersion = testDoc.correctVersion[strategy];
+        if (correctVersion !== null) {
+            correctDoc = {_id: currentId, msg: testDoc.msg, version: correctVersion};
+        }
+        if (resultDoc === null) {
+            if (correctVersion !== null) {
+                wrongDocs++;
+                printjson({
+                    error: "Document is missing and should be present",
+                    strategy: strategyName,
+                    correctDoc: correctDoc
+                });
+            }
+        } else if (correctVersion === null) {
+            wrongDocs++;
+            printjson({
+                error: "Document is present and should be deleted",
+                strategy: strategyName,
+                resultDoc: resultDoc
+            });
+        } else if (bsonWoCompare(resultDoc, correctDoc) != 0) {
+            wrongDocs++;
+            printjson({
+                error: "Document is present but has incorrect contents",
+                strategy: strategyName,
+                resultDoc: resultDoc,
+                correctDoc: correctDoc
+            });
+        }
+        currentId++;
+    }
+    assert.eq(0, wrongDocs, "There were " + wrongDocs + " wrong docs for strategy " + strategyName);
+}
+
+let rst = new ReplSetTest({name: TestData.testName, nodes: 1});
+rst.startSet();
+rst.initiateWithHighElectionTimeout();
+let dbName = TestData.testName;
+const primary = rst.getPrimary();
+const primaryDb = primary.getDB(dbName);
+let mongoUri = "mongodb://" + primary.host;
+if (TestData.auth) {
+    // ClusterMonitor for replSetGetStatus, applyOps for _internalReadAtClusterTime,
+    // readWriteAnyDatabase for repairs.
+    primaryDb.getSiblingDB("admin").createRole({
+        role: "corruptAdmin",
+        roles: ["clusterMonitor", "readWriteAnyDatabase"],
+        privileges: [
+            {resource: {cluster: true}, actions: ["applyOps"]},
+            {resource: {db: "local", collection: "system.healthlog"}, actions: ["find"]},
+            {
+                resource: {db: metadataDbName, collection: rangeCollName},
+                actions: [
+                    "find",
+                    "insert",
+                    "update",
+                    "remove",
+                    "createCollection",
+                    "dropCollection",
+                    "createIndex",
+                    "dropIndex"
+                ]
+            }
+        ]
+    });
+    primaryDb.getSiblingDB("admin").createUser(
+        {user: "admin", pwd: "password", roles: ["corruptAdmin"]});
+    mongoUri = "mongodb://admin:password@" + primary.host;
+}
+
+for (let strategy = 0; strategy < strategyNames.length; strategy++) {
+    createTestData(primaryDb);
+    assert.eq(0,
+              runProgram(python_binary,
+                         scriptUnderTest,
+                         "--no-dryrun",
+                         "--strategy",
+                         strategyNames[strategy],
+                         mongoUri));
+    checkTestDataResults(primaryDb, strategy);
+}
+
+// The verbose output is not tested; this merely tests that the repair still works with the
+// verbose option enabled.
+for (let strategy = 0; strategy < strategyNames.length; strategy++) {
+    createTestData(primaryDb);
+    assert.eq(0,
+              runProgram(python_binary,
+                         scriptUnderTest,
+                         "--no-dryrun",
+                         "--verbose",
+                         "--strategy",
+                         strategyNames[strategy],
+                         mongoUri));
+    checkTestDataResults(primaryDb, strategy);
+}
+
+// The dryrun output is not tested; this merely tests that the repair is not done with dry run
+// enabled.
+for (let strategy = 0; strategy < strategyNames.length; strategy++) {
+    createTestData(primaryDb);
+    assert.eq(0,
+              runProgram(
+                  python_binary, scriptUnderTest, "--strategy", strategyNames[strategy], mongoUri));
+    checkTestDataResults(primaryDb, strategy, true /* dryrun*/);
+}
+
+rst.stopSet();
+})();
---
+++ src/mongo/db/modules/enterprise/jstests/remediation/badreplset.js
@@ -0,0 +1,291 @@
+/**
+ * badreplset.js
+ * This script creates a replica set with documents that are different on the various nodes.
+ * It then pauses and allows repairs to be made, while the set is still being written to.
+ * When the user hits a key it continues, removes documents that were intentionally unresolvable,
+ * and checks the replica set for consistency.
+ *
+ * The intended use is testing the repair_checked_replset.js script. It is currently not intended to
+ * be run from evergreen.
+ */
+(function() {
+"use strict";
+
+load("jstests/libs/parallelTester.js");
+
+function writeLoadFunc(host, dbName, collName, shutdownLatch) {
+    const nodeDB = new Mongo(host).getDB(dbName);
+    const coll = nodeDB[collName];
+    while (shutdownLatch.getCount() > 0) {
+        assert.commandWorked(coll.insert({desc: "Document is OK", status: "good"}));
+        sleep(100);
+    }
+}
+
+function dbCheckCompleted(db) {
+    return db.currentOp().inprog.filter(x => x["desc"] == "dbCheck")[0] === undefined;
+}
+
+// Wait for dbCheck to complete (on both primaries and secondaries).  Fails an assertion if
+// dbCheck takes longer than maxMs.
+function awaitDbCheckCompletion(rst, db) {
+    let start = Date.now();
+
+    assert.soon(() => dbCheckCompleted(db), "dbCheck timed out");
+    rst.awaitSecondaryNodes();
+    rst.awaitReplication();
+
+    // Give the health log buffers some time to flush.
+    sleep(100);
+}
+
+const nodeCount = 3;
+// TestData.enableTestCommands = false
+let rst = new ReplSetTest({
+    name: TestData.testName,
+    nodes: [{}, {}, {}, {rsConfig: {arbiterOnly: true}}],
+    nodeOptions: {setParameter: {oplogApplicationEnforcesSteadyStateConstraints: false}}
+});
+rst.startSet();
+rst.initiateWithHighElectionTimeout();
+let dbName = TestData.testName;
+let collName = "test";
+const primary = rst.getPrimary();
+const primaryDb = primary.getDB(dbName);
+const primaryColl = primaryDb[collName];
+let secondary = rst.getSecondaries()[0];
+let tiebreaker = rst.getSecondaries()[1];
+
+const docOk = 0;
+const docMissingPrimaryShouldExist = 1;
+const docMissingPrimaryShouldNotExist = 2;
+const docMissingPrimaryDifferentOnTiebreaker = 3;
+const docMissingSecondaryShouldExist = 4;
+const docMissingSecondaryShouldNotExist = 5;
+const docMissingSecondaryDifferentOnTiebreaker = 6;
+const docDifferentCorrectOnPrimary = 7;
+const docDifferentCorrectOnSecondary = 8;
+const docDifferentAllNodes = 9;
+
+if (TestData.auth) {
+    // ClusterMonitor for replSetGetStatus, applyOps for _internalReadAtClusterTime,
+    // readWriteAnyDatabase for repairs.
+    primaryDb.getSiblingDB("admin").createRole({
+        role: "corruptAdmin",
+        roles: ["clusterMonitor", "readWriteAnyDatabase"],
+        privileges: [
+            {resource: {cluster: true}, actions: ["applyOps"]},
+            {resource: {db: "local", collection: "system.healthlog"}, actions: ["find"]},
+            {
+                resource: {db: "config", collection: "unhealthyRanges"},
+                actions: [
+                    "find",
+                    "insert",
+                    "update",
+                    "remove",
+                    "createCollection",
+                    "dropCollection",
+                    "createIndex",
+                    "dropIndex"
+                ]
+            }
+        ]
+    });
+    primaryDb.getSiblingDB("admin").createUser(
+        {user: "admin", pwd: "password", roles: ["corruptAdmin"]});
+}
+
+assert.commandWorked(primaryColl.insert({_id: docOk, desc: "Document is OK", status: "good"}));
+assert.commandWorked(primaryColl.insert({
+    _id: docMissingSecondaryShouldExist,
+    desc: "Document is missing on secondary, should exist",
+    status: "good"
+}));
+assert.commandWorked(primaryColl.insert({
+    _id: docMissingSecondaryShouldNotExist,
+    desc: "Document is missing on secondary, should not exist",
+    status: "bad"
+}));
+assert.commandWorked(primaryColl.insert({
+    _id: docMissingSecondaryDifferentOnTiebreaker,
+    desc: "Document is missing on secondary, different on tiebreaker",
+    status: "indeterminate",
+    node: "primary"
+}));
+assert.commandWorked(primaryColl.insert({
+    _id: docDifferentCorrectOnPrimary,
+    desc: "Document is different and correct on primary",
+    status: "good"
+}));
+assert.commandWorked(primaryColl.insert({
+    _id: docDifferentCorrectOnSecondary,
+    desc: "Document is different and correct on secondary",
+    status: "bad"
+}));
+assert.commandWorked(primaryColl.insert({
+    _id: docDifferentAllNodes,
+    desc: "Document is different on all nodes",
+    status: "indeterminate",
+    node: "primary"
+}));
+rst.awaitReplication();
+
+{
+    jsTestLog("Stopping secondary to corrupt the set");
+    rst.stop(secondary, {forRestart: true});
+    const standaloneSecondary =
+        MongoRunner.runMongod({dbpath: secondary.dbpath, noCleanData: true});
+    const standaloneDb = standaloneSecondary.getDB(dbName);
+    const standaloneColl = standaloneDb[collName];
+    assert.commandWorked(standaloneColl.insert({
+        _id: docMissingPrimaryShouldExist,
+        desc: "Document is missing on primary, should exist",
+        status: "good"
+    }));
+    assert.commandWorked(standaloneColl.insert({
+        _id: docMissingPrimaryShouldNotExist,
+        desc: "Document is missing on primary, should not exist",
+        status: "bad"
+    }));
+    assert.commandWorked(standaloneColl.insert({
+        _id: docMissingPrimaryDifferentOnTiebreaker,
+        desc: "Document is missing on primary, different on secondary and tiebreaker",
+        status: "indeterminate",
+        node: "secondary"
+    }));
+    assert.commandWorked(standaloneColl.remove({_id: docMissingSecondaryShouldExist}));
+    assert.commandWorked(standaloneColl.remove({_id: docMissingSecondaryShouldNotExist}));
+    assert.commandWorked(standaloneColl.remove({_id: docMissingSecondaryDifferentOnTiebreaker}));
+    assert.commandWorked(standaloneColl.replaceOne(
+        {_id: docDifferentCorrectOnPrimary},
+        {desc: "Document is different and correct on primary", status: "bad"}));
+    assert.commandWorked(standaloneColl.replaceOne(
+        {_id: docDifferentCorrectOnSecondary},
+        {desc: "Document is different and correct on secondary", status: "good"}));
+    assert.commandWorked(standaloneColl.replaceOne(
+        {_id: docDifferentAllNodes},
+        {desc: "Document is different on all nodes", status: "indeterminate", node: "secondary"}));
+    MongoRunner.stopMongod(standaloneSecondary);
+}
+jsTestLog("Restarting secondary");
+secondary = rst.start(secondary, {}, true /* restart */);
+rst.awaitSecondaryNodes();
+
+jsTestLog("Stopping tiebreaker to corrupt the set");
+{
+    rst.stop(tiebreaker, {forRestart: true});
+    const standaloneTiebreaker =
+        MongoRunner.runMongod({dbpath: tiebreaker.dbpath, noCleanData: true});
+    const standaloneDb = standaloneTiebreaker.getDB(dbName);
+    const standaloneColl = standaloneDb[collName];
+    assert.commandWorked(standaloneColl.insert({
+        _id: docMissingPrimaryShouldExist,
+        desc: "Document is missing on primary, should exist",
+        status: "good"
+    }));
+    assert.commandWorked(standaloneColl.insert({
+        _id: docMissingPrimaryDifferentOnTiebreaker,
+        desc: "Document is missing on primary, different on secondary and tiebreaker",
+        status: "indeterminate",
+        node: "tiebreaker"
+    }));
+    assert.commandWorked(standaloneColl.remove({_id: docMissingSecondaryShouldNotExist}));
+    assert.commandWorked(
+        standaloneColl.replaceOne({_id: docMissingSecondaryDifferentOnTiebreaker}, {
+            _id: docMissingSecondaryDifferentOnTiebreaker,
+            desc: "Document is missing on secondary, different on tiebreaker",
+            status: "indeterminate",
+            node: "tiebreaker"
+        }));
+    assert.commandWorked(standaloneColl.replaceOne(
+        {_id: docDifferentCorrectOnSecondary},
+        {desc: "Document is different and correct on secondary", status: "good"}));
+    assert.commandWorked(standaloneColl.replaceOne(
+        {_id: docDifferentAllNodes},
+        {desc: "Document is different on all nodes", status: "indeterminate", node: "tiebreaker"}));
+    MongoRunner.stopMongod(standaloneTiebreaker);
+}
+
+jsTestLog("Restarting tiebreaker");
+tiebreaker = rst.start(tiebreaker, {}, true /* restart */);
+rst.awaitSecondaryNodes();
+
+jsTestLog("Secondary restarted, verifying corruption");
+const secondaryDb = secondary.getDB(dbName);
+const secondaryColl = secondaryDb[collName];
+
+const statusProj = {
+    _id: 0,
+    status: 1
+};
+assert.eq(primaryColl.find({_id: docMissingPrimaryShouldExist}).toArray(), []);
+assert.eq(primaryColl.find({_id: docMissingPrimaryShouldNotExist}).toArray(), []);
+assert.eq(primaryColl.find({_id: docMissingSecondaryShouldExist}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(primaryColl.find({_id: docMissingSecondaryShouldNotExist}, statusProj).toArray(),
+          [{status: "bad"}]);
+assert.eq(primaryColl.find({_id: docDifferentCorrectOnPrimary}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(primaryColl.find({_id: docDifferentCorrectOnSecondary}, statusProj).toArray(),
+          [{status: "bad"}]);
+
+assert.eq(secondaryColl.find({_id: docMissingPrimaryShouldExist}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(secondaryColl.find({_id: docMissingPrimaryShouldNotExist}, statusProj).toArray(),
+          [{status: "bad"}]);
+assert.eq(secondaryColl.find({_id: docMissingSecondaryShouldExist}).toArray(), []);
+assert.eq(secondaryColl.find({_id: docMissingSecondaryShouldNotExist}).toArray(), []);
+assert.eq(secondaryColl.find({_id: docDifferentCorrectOnPrimary}, statusProj).toArray(),
+          [{status: "bad"}]);
+assert.eq(secondaryColl.find({_id: docDifferentCorrectOnSecondary}, statusProj).toArray(),
+          [{status: "good"}]);
+
+assert.commandWorked(primaryDb.runCommand({dbCheck: collName, minKey: 0, maxKey: 5}));
+awaitDbCheckCompletion(rst, primaryDb);
+assert.commandWorked(primaryDb.runCommand({dbCheck: collName, minKey: 5, maxCount: 10}));
+awaitDbCheckCompletion(rst, primaryDb);
+
+jsTestLog("Corruption is verified, attempting to repair");
+
+function repairDoc(coll, docOrId, deleteDoc) {
+    if (deleteDoc) {
+        assert.commandWorked(coll.replaceOne(docOrId, {special: "deleted"}, {upsert: true}));
+        assert.commandWorked(coll.remove(docOrId));
+    } else {
+        assert.commandWorked(coll.remove(docOrId));
+        assert.commandWorked(coll.replaceOne({_id: docOrId._id}, docOrId, {upsert: true}));
+    }
+}
+
+let shutdownLatch = new CountDownLatch(1);
+let writeLoadThread = new Thread(writeLoadFunc, primary.host, dbName, collName, shutdownLatch);
+writeLoadThread.start();
+jsTestLog("Time to repair the replica set");
+print("Start your repair on " + primary.host);
+print("Then hit enter to continue");
+passwordPrompt();
+shutdownLatch.countDown();
+writeLoadThread.join();
+
+jsTestLog("Verifing repairs: secondary");
+rst.awaitReplication();
+
+assert.eq(secondaryColl.find({_id: docMissingPrimaryShouldExist}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(secondaryColl.find({_id: docMissingPrimaryShouldNotExist}).toArray(), []);
+assert.eq(secondaryColl.find({_id: docMissingSecondaryShouldExist}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(secondaryColl.find({_id: docMissingSecondaryShouldNotExist}).toArray(), []);
+assert.eq(secondaryColl.find({_id: docDifferentCorrectOnPrimary}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(secondaryColl.find({_id: docDifferentCorrectOnSecondary}, statusProj).toArray(),
+          [{status: "good"}]);
+assert.eq(secondaryColl.find({status: "bad"}).itcount(), 0);
+
+jsTestLog("Removing indeterminate documents so dbhash passes");
+repairDoc(primaryColl, {_id: docMissingPrimaryDifferentOnTiebreaker}, true);
+repairDoc(primaryColl, {_id: docMissingSecondaryDifferentOnTiebreaker}, true);
+repairDoc(primaryColl, {_id: docDifferentAllNodes}, true);
+
+rst.stopSet();
+})();
