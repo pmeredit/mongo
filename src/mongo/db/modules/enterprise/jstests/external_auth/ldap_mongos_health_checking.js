90dbb346d56b5db430bd5a15729e4c6501e64b09
Andrew Shuvalov <andrew.shuvalov@mongodb.com>
2022-01-22 00:54:24 +0000

    SERVER-62678 Migrate LDAP health checker to use permanent OpenLDAP & AD servers
    GitOrigin-RevId: 01a082325c04b5f752239e70704605f5c2cb2a3d

--- src/mongo/db/modules/enterprise/jstests/external_auth/ldap_mongos_health_checking.js
+++ src/mongo/db/modules/enterprise/jstests/external_auth/ldap_mongos_health_checking.js
@@ -13,195 +13,222 @@ const kIterations = 2;
 const kWaitForCompletedChecksCount = 100;
 const kWaitForPassedChecksCount = 10;
 
-const ldapServers =
-    "ec2-3-142-199-96.us-east-2.compute.amazonaws.com,ec2-3-141-40-15.us-east-2.compute.amazonaws.com";
-// const ldapServers = "ldaptest.10gen.cc";
-const ldapServersArray = ldapServers.split(',');
-
-var st = new ShardingTest({
-    shards: 1,
-    config: 1,
-    mongos: [{
-        setParameter: {
-            healthMonitoringIntensities: tojson({
-                values: [
-                    {type: "ldap", intensity: "critical"},
-                ]
-            }),
-            progressMonitor: tojson({interval: 100, deadline: 60}),
-            healthMonitoringIntervals: tojson({values: [{type: "ldap", interval: 30}]}),
+const ldapTestServers = function() {
+    return {
+        "OpenLDAP": {
+            "ldapServers": baseLDAPUrls[0],
+            "ldapTransportSecurity": "none",
+            "ldapBindMethod": "simple",
+            "ldapQueryUser": "cn=ldapz_admin,ou=Users,dc=10gen,dc=cc",
+            "ldapQueryPassword": "Secret123",
+            "ldapAuthzQueryTemplate": "{USER}?memberOf",
+            "ldapTimeoutMS": 10000,
         },
-        ldapServers: ldapServers,
-        ldapTransportSecurity: "none",
-        ldapBindMethod: "simple",
-        //"ldapQueryUser": "cn=ldapz_admin,ou=Users,dc=10gen,dc=cc",
-        ldapQueryUser: "cn=admin,dc=10gen,dc=cc",
-        ldapQueryPassword: "Secret123",
-        ldapTimeoutMS: 8000,
-    }],
-});
-
-assert.commandWorked(st.s0.adminCommand(
-    {"setParameter": 1, logComponentVerbosity: {processHealth: {verbosity: 2}}}));
-
-const mongosProcessId = (() => {
-    clearRawMongoProgramOutput();
-    const shellArgs = ['ps', '-e'];
-    const rc = _runMongoProgram.apply(null, shellArgs);
-    assert.eq(rc, 0);
-    var lines = rawMongoProgramOutput();
-    var found;
-    lines.split('\n').forEach((line) => {
-        const match = line.match(/[' ']+([0-9]+).*mongos.*/i);
-        if (match) {
-            found = match[1];
+        "ActiveDirectory": {
+            // Elastic IP for the ec2-18-216-194-49.us-east-2.compute.amazonaws.com host.
+            "ldapServers": "3.129.54.246",
+            "ldapTransportSecurity": "none",
+            "ldapBindMethod": "simple",
+            "ldapQueryUser": "cn=ldapz_admin,cn=Users,dc=mongotest,dc=com",
+            "ldapQueryPassword": "Secret123",
+            "ldapAuthzQueryTemplate": "{USER}?memberOf",
+            "ldapTimeoutMS": 10000,
         }
-    });
-    return found;
-})();
+    };
+}();
 
-// If there is more than one server, disable all but one server with firewall.
-// If there is only one server skip this test.
-const testWithPartiallyDisabledFirewall = function() {
-    if (ldapServersArray.length <= 1 || !isAnyUbuntu) {
-        return;
+const runTestSuite = function(ldapTestServer) {
+    const ldapServersArray = ldapTestServer["ldapServers"].split(',');
+
+    var st = new ShardingTest({
+        shards: 1,
+        config: 1,
+        mongos: [{
+            setParameter: {
+                healthMonitoringIntensities: tojson({
+                    values: [
+                        {type: "ldap", intensity: "critical"},
+                    ]
+                }),
+                progressMonitor: tojson({interval: 100, deadline: 60}),
+                healthMonitoringIntervals: tojson({values: [{type: "ldap", interval: 30}]}),
+            },
+            ldapServers: ldapTestServer["ldapServers"],
+            ldapTransportSecurity: "none",
+            ldapBindMethod: "simple",
+            ldapQueryUser: ldapTestServer["ldapQueryUser"],
+            ldapQueryPassword: ldapTestServer["ldapQueryPassword"],
+            ldapTimeoutMS: 8000,
+        }],
+    });
+
+    assert.commandWorked(st.s0.adminCommand(
+        {"setParameter": 1, logComponentVerbosity: {processHealth: {verbosity: 2}}}));
+
+    const mongosProcessId = (() => {
+        clearRawMongoProgramOutput();
+        const shellArgs = ['ps', '-e'];
+        const rc = _runMongoProgram.apply(null, shellArgs);
+        assert.eq(rc, 0);
+        var lines = rawMongoProgramOutput();
+        var found;
+        lines.split('\n').forEach((line) => {
+            const match = line.match(/[' ']+([0-9]+).*mongos.*/i);
+            if (match) {
+                found = match[1];
+            }
+        });
+        return found;
+    })();
+
+    // If there is more than one server, disable all but one server with firewall.
+    // If there is only one server skip this test.
+    const testWithPartiallyDisabledFirewall = function() {
+        if (ldapServersArray.length <= 1 || !isAnyUbuntu) {
+            return;
+        }
+        const indexToNotBlock = Math.floor(Math.random() * ldapServersArray.length);
+        const serversToBlock = ldapServersArray.slice(0, indexToNotBlock)
+                                   .concat(ldapServersArray.slice(indexToNotBlock + 1));
+
+        serversToBlock.forEach((serverName) => {
+            changeFirewallForServer(serverName);
+        });
+        sleep(10000);  // Let mongos to run with firewall.
+        // The timeout for each request is 8 sec. In each health check, one thread
+        // will succeed and one timeout before the firewall is enabled.
+
+        // Mongos should be functional.
+        assert.commandWorked(st.s0.adminCommand({"ping": 1}));
+
+        serversToBlock.forEach((serverName) => {
+            changeFirewallForServer(serverName, true /* remove rule */);
+        });
+    };
+
+    const testWithFullyDisabledFirewall = function() {
+        ldapServersArray.forEach((serverName) => {
+            changeFirewallForServer(serverName);
+        });
+        sleep(Math.random() * 10000);  // Let mongos to run with firewall.
+
+        ldapServersArray.forEach((serverName) => {
+            changeFirewallForServer(serverName, true /* remove rule */);
+        });
+
+        // Mongos should be functional.
+        assert.commandWorked(st.s0.adminCommand({"ping": 1}));
+    };
+
+    const printMemoryForProcess = function(processId) {
+        clearRawMongoProgramOutput();
+        // /smaps could be unavailable, first print the /status
+        const statusShellArgs = ['ls', '/proc/' + processId + '/status'];
+        var rc = _runMongoProgram.apply(null, statusShellArgs);
+        if (rc != 0) {
+            jsTestLog(`Process status unavailable for pid ${processId}`);
+            // List /proc for debug.
+            const shellArgs = ['ls', '/proc'];
+            _runMongoProgram.apply(null, shellArgs);
+            return;
+        }
+        clearRawMongoProgramOutput();
+        const shellArgs = ['cat', '/proc/' + processId + '/smaps'];
+        rc = _runMongoProgram.apply(null, shellArgs);
+        if (rc != 0) {
+            jsTestLog(`/proc/${processId}/smaps not available`);
+            return;
+        }
+        var lines = rawMongoProgramOutput();
+        var totalKb = 0;
+        lines.split('\n').forEach((line) => {
+            const match = line.match(/[' ']+Pss:[' ']+([0-9]+).*kB.*/i);
+            if (match) {
+                totalKb += parseInt(match[1]);
+            }
+        });
+        jsTestLog(`Total memory usage ${totalKb} kB`);
+    };
+
+    const printFdCount = function(processId) {
+        clearRawMongoProgramOutput();
+        const shellArgs = ['ls', '/proc/' + processId + '/fd/'];
+        const rc = _runMongoProgram.apply(null, shellArgs);
+        if (rc != 0) {
+            jsTestLog(`/proc/${processId}/fd/ not available`);
+            return;
+        }
+        var lines = rawMongoProgramOutput();
+        var totalFds = lines.split('\n').length;
+        jsTestLog(`Total files open ${totalFds}`);
+    };
+
+    // Expects some minimal check count to pass. This expects the test to be enabled
+    // on Enterprise builds only.
+    const checkServerStats = function() {
+        while (true) {
+            let result =
+                assert.commandWorked(st.s0.adminCommand({serverStatus: 1, health: {details: true}}))
+                    .health;
+            print(`Server status: ${tojson(result)}`);
+            // Wait for: at least 100 checks completed.
+            // At least some checks passed (more than 1).
+            if (result.LDAP.totalChecks >= kWaitForCompletedChecksCount &&
+                result.LDAP.totalChecks - result.LDAP.totalChecksWithFailure >=
+                    kWaitForPassedChecksCount) {
+                break;
+            }
+            sleep(1000);
+        }
+    };
+
+    var firewallIsEnabledAtStart = false;
+    try {
+        if (isAnyUbuntu) {
+            firewallIsEnabledAtStart = checkFirewallIsEnabled();
+            if (!firewallIsEnabledAtStart) {
+                enableFirewall();
+            }
+            for (var i = 0; i < kIterations; ++i) {
+                testWithPartiallyDisabledFirewall();
+                if (i % 10 == 0) {
+                    printMemoryForProcess(mongosProcessId);
+                    printFdCount(mongosProcessId);
+                }
+            }
+            for (var i = 0; i < kIterations; ++i) {
+                testWithFullyDisabledFirewall();
+                if (i % 10 == 0) {
+                    printMemoryForProcess(mongosProcessId);
+                    printFdCount(mongosProcessId);
+                }
+            }
+        }
+
+    } finally {
+        if (isAnyUbuntu && !firewallIsEnabledAtStart) {
+            disableFirewall();
+        }
     }
-    const indexToNotBlock = Math.floor(Math.random() * ldapServersArray.length);
-    const serversToBlock = ldapServersArray.slice(0, indexToNotBlock)
-                               .concat(ldapServersArray.slice(indexToNotBlock + 1));
 
-    serversToBlock.forEach((serverName) => {
-        changeFirewallForServer(serverName);
-    });
-    sleep(10000);  // Let mongos to run with firewall.
-    // The timeout for each request is 8 sec. In each health check, one thread
-    // will succeed and one timeout before the firewall is enabled.
-
-    // Mongos should be functional.
+    sleep(8000);  // Let all health checker threads stuck because of firewall to terminate.
     assert.commandWorked(st.s0.adminCommand({"ping": 1}));
-
-    serversToBlock.forEach((serverName) => {
-        changeFirewallForServer(serverName, true /* remove rule */);
-    });
-};
-
-const testWithFullyDisabledFirewall = function() {
-    ldapServersArray.forEach((serverName) => {
-        changeFirewallForServer(serverName);
-    });
-    sleep(Math.random() * 10000);  // Let mongos to run with firewall.
-
-    ldapServersArray.forEach((serverName) => {
-        changeFirewallForServer(serverName, true /* remove rule */);
-    });
-
-    // Mongos should be functional.
-    assert.commandWorked(st.s0.adminCommand({"ping": 1}));
-};
-
-const printMemoryForProcess = function(processId) {
-    clearRawMongoProgramOutput();
-    // /smaps could be unavailable, first print the /status
-    const statusShellArgs = ['ls', '/proc/' + processId + '/status'];
-    var rc = _runMongoProgram.apply(null, statusShellArgs);
-    if (rc != 0) {
-        jsTestLog(`Process status unavailable for pid ${processId}`);
-        // List /proc for debug.
-        const shellArgs = ['ls', '/proc'];
-        _runMongoProgram.apply(null, shellArgs);
-        return;
-    }
-    clearRawMongoProgramOutput();
-    const shellArgs = ['cat', '/proc/' + processId + '/smaps'];
-    rc = _runMongoProgram.apply(null, shellArgs);
-    if (rc != 0) {
-        jsTestLog(`/proc/${processId}/smaps not available`);
-        return;
-    }
-    var lines = rawMongoProgramOutput();
-    var totalKb = 0;
-    lines.split('\n').forEach((line) => {
-        const match = line.match(/[' ']+Pss:[' ']+([0-9]+).*kB.*/i);
-        if (match) {
-            totalKb += parseInt(match[1]);
-        }
-    });
-    jsTestLog(`Total memory usage ${totalKb} kB`);
-};
-
-const printFdCount = function(processId) {
-    clearRawMongoProgramOutput();
-    const shellArgs = ['ls', '/proc/' + processId + '/fd/'];
-    const rc = _runMongoProgram.apply(null, shellArgs);
-    if (rc != 0) {
-        jsTestLog(`/proc/${processId}/fd/ not available`);
-        return;
-    }
-    var lines = rawMongoProgramOutput();
-    var totalFds = lines.split('\n').length;
-    jsTestLog(`Total files open ${totalFds}`);
-};
-
-// Expects some minimal check count to pass. This expects the test to be enabled
-// on Enterprise builds only.
-const checkServerStats = function() {
-    while (true) {
-        let result =
-            assert.commandWorked(st.s0.adminCommand({serverStatus: 1, health: {details: true}}))
-                .health;
-        print(`Server status: ${tojson(result)}`);
-        // Wait for: at least 100 checks completed.
-        // At least some checks passed (more than 1).
-        if (result.LDAP.totalChecks >= kWaitForCompletedChecksCount &&
-            result.LDAP.totalChecks - result.LDAP.totalChecksWithFailure >=
-                kWaitForPassedChecksCount) {
-            break;
-        }
-        sleep(1000);
-    }
-};
-
-var firewallIsEnabledAtStart = false;
-try {
     if (isAnyUbuntu) {
-        firewallIsEnabledAtStart = checkFirewallIsEnabled();
-        if (!firewallIsEnabledAtStart) {
-            enableFirewall();
-        }
-        for (var i = 0; i < kIterations; ++i) {
-            testWithPartiallyDisabledFirewall();
-            if (i % 10 == 0) {
-                printMemoryForProcess(mongosProcessId);
-                printFdCount(mongosProcessId);
-            }
-        }
-        for (var i = 0; i < kIterations; ++i) {
-            testWithFullyDisabledFirewall();
-            if (i % 10 == 0) {
-                printMemoryForProcess(mongosProcessId);
-                printFdCount(mongosProcessId);
-            }
-        }
+        printMemoryForProcess(mongosProcessId);
+        printFdCount(mongosProcessId);
     }
+    checkServerStats();
 
-} finally {
-    if (isAnyUbuntu && !firewallIsEnabledAtStart) {
-        disableFirewall();
+    try {
+        jsTestLog('Shutting down the sharded cluster');
+        st.stop();
+    } catch (err) {
+        jsTestLog(`Error during shutdown ${err}`);
     }
-}
+};
 
-sleep(8000);  // Let all health checker threads stuck because of firewall to terminate.
-assert.commandWorked(st.s0.adminCommand({"ping": 1}));
-if (isAnyUbuntu) {
-    printMemoryForProcess(mongosProcessId);
-    printFdCount(mongosProcessId);
-}
-checkServerStats();
-
-try {
-    jsTestLog('Shutting down the sharded cluster');
-    st.stop();
-} catch (err) {
-    jsTestLog(`Error during shutdown ${err}`);
-}
+Object.keys(ldapTestServers).forEach((serverName) => {
+    jsTestLog(`Running tests for server ${serverName}`);
+    runTestSuite(ldapTestServers[serverName]);
+});
 })();
