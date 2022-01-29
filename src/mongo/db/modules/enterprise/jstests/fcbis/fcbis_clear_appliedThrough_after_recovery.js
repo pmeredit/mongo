e57ae8a30a2038ef10ec1700af1cc538be348931
Lingzhi Deng <lingzhi.deng@mongodb.com>
2022-01-28 22:55:51 -0500

    SERVER-62745: Test that appliedThrough is cleared after FCBIS
    GitOrigin-RevId: 194c735049f843e4adfa15535f48a0de7e72df16

---
+++ src/mongo/db/modules/enterprise/jstests/fcbis/fcbis_clear_appliedThrough_after_recovery.js
@@ -0,0 +1,78 @@
+/**
+ * Tests that appliedThrough is cleared after FCBIS.
+ * @tags: [requires_fcv_52, requires_persistence, requires_wiredtiger]
+ */
+(function() {
+"use strict";
+
+load("jstests/replsets/rslib.js");
+load("jstests/libs/fail_point_util.js");
+
+const testName = TestData.testName;
+const rst = new ReplSetTest({
+    name: testName,
+    nodes: [{
+        setParameter: {
+            'initialSyncMethod': "fileCopyBased",
+        }
+    }]
+});
+rst.startSet();
+rst.initiateWithHighElectionTimeout();
+const primary = rst.getPrimary();
+const primaryDb = primary.getDB("test");
+
+const featureEnabled = assert
+                           .commandWorked(primaryDb.adminCommand(
+                               {getParameter: 1, featureFlagFileCopyBasedInitialSync: 1}))
+                           .featureFlagFileCopyBasedInitialSync.value;
+if (!featureEnabled) {
+    jsTestLog("Skipping test because the file copy based initial sync feature flag is disabled");
+    rst.stopSet();
+    return;
+}
+
+// Add some data to be cloned.
+assert.commandWorked(primaryDb.test.insert([{a: 1}, {b: 2}, {c: 3}]));
+rst.awaitReplication();
+
+// Ensure there's an up-to-date stable checkpoint.
+rst.awaitLastStableRecoveryTimestamp();
+assert.commandWorked(primary.adminCommand({fsync: 1}));
+
+assert.commandWorked(
+    primary.adminCommand({configureFailPoint: "disableSnapshotting", mode: "alwaysOn"}));
+jsTestLog("Doing more writes after stopping stable checkpoints");
+assert.commandWorked(primaryDb.runCommand(
+    {insert: "test", documents: [{a: 4}, {a: 5}, {a: 6}], writeConcern: {w: 1}}));
+
+jsTestLog("Adding the initial sync destination node to the replica set");
+const initialSyncNode = rst.add({
+    rsConfig: {priority: 0, votes: 0},
+    setParameter: {
+        'initialSyncMethod': 'fileCopyBased',
+        'failpoint.fCBISForceExtendBackupCursor': tojson({mode: {times: 1}}),
+        'numInitialSyncAttempts': 1,
+        'logComponentVerbosity': tojson({replication: {verbosity: 1, initialSync: 2}})
+    }
+});
+rst.reInitiate();
+
+assert.commandWorked(initialSyncNode.adminCommand({
+    waitForFailPoint: 'fCBISForceExtendBackupCursor',
+    timesEntered: 1,
+    maxTimeMS: kDefaultWaitForFailPointTimeout
+}));
+assert.commandWorked(
+    primary.adminCommand({configureFailPoint: "disableSnapshotting", mode: "off"}));
+// Do more writes to trigger a new stable checkpoint on the primary.
+assert.commandWorked(primaryDb.test.insert([{a: 7}, {b: 8}, {c: 9}]));
+
+rst.waitForState(initialSyncNode, ReplSetTest.State.SECONDARY);
+
+const minValid = initialSyncNode.getCollection('local.replset.minvalid').findOne();
+assert(!minValid.hasOwnProperty('begin'), tojson(minValid));
+
+// TODO (SERVER-63233): Remove skipValidation after the fastcount issue is fixed.
+rst.stopSet(null, null, {skipValidation: true});
+})();
