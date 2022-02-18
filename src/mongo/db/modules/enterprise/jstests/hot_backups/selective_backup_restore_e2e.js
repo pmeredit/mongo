1012d580362f442295b77032dfcb087f5b9b4ccf
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2022-02-18 03:40:03 +0000

    SERVER-63230 Write end-to-end tests for replica set selective backups/restores
    GitOrigin-RevId: 1c591faa6173e4cbb01bed5507a73d307e17ce8a

---
+++ src/mongo/db/modules/enterprise/jstests/hot_backups/selective_backup_restore_e2e.js
@@ -0,0 +1,293 @@
+/**
+ * Tests end-to-end WiredTiger selective backups and restores.
+ *
+ * While concurrent FSM workloads are running in the background on the primary node, several
+ * full and selective backups are taken. The backups are restored and verified by running dbHash on
+ * the collections restored in the selective backup against the collections restored in the full
+ * backup.
+ *
+ * @tags: [
+ *     requires_persistence,
+ *     requires_replication,
+ *     requires_wiredtiger
+ * ]
+ */
+(function() {
+"use strict";
+
+load("jstests/disk/libs/wt_file_helper.js");
+load("jstests/libs/backup_utils.js");
+load("jstests/libs/feature_flag_util.js");
+load("jstests/libs/uuid_util.js");
+load("src/mongo/db/modules/enterprise/jstests/hot_backups/libs/incremental_backup_helpers.js");
+
+// Windows doesn't guarantee synchronous file operations.
+if (_isWindows()) {
+    print("Skipping test on windows");
+    return;
+}
+
+TestData.skipEnforceFastCountOnValidate = true;
+
+const rst = new ReplSetTest({
+    nodes: [
+        {},
+        {
+            // Disallow elections on secondary.
+            rsConfig: {
+                priority: 0,
+                votes: 0,
+            },
+        }
+    ],
+    // Frequent checkpoints.
+    nodeOptions: {syncdelay: 5}
+});
+
+rst.startSet();
+rst.initiate();
+
+const primary = rst.getPrimary();
+
+if (!FeatureFlagUtil.isEnabled(primary.getDB("admin"), "SelectiveBackup")) {
+    jsTestLog("Skipping as featureFlagSelectiveBackup is not enabled");
+    rst.stopSet();
+    return;
+}
+
+// Run the FSM workload on the primary throughout the duration of this test.
+const fsmPid = startFSMClient(primary.host);
+
+const fullBackupPath = primary.dbpath + kSeparator + "fullBackup";
+const selectiveBackupPath = primary.dbpath + kSeparator + "selectiveBackup";
+
+function resetBackupPaths() {
+    resetDbpath(fullBackupPath);
+    mkdir(fullBackupPath + kSeparator + "journal");
+
+    resetDbpath(selectiveBackupPath);
+    mkdir(selectiveBackupPath + kSeparator + "journal");
+}
+
+const kNumIterations = 10;
+for (let iteration = 1; iteration <= kNumIterations; iteration++) {
+    jsTestLog("Running iteration " + iteration);
+
+    resetBackupPaths();
+
+    const backupCursor = openBackupCursor(primary);
+
+    // Print the metadata document.
+    assert(backupCursor.hasNext());
+    jsTestLog(backupCursor.next());
+
+    const optionalFilesCopied = {};
+    const optionalFilesSkipped = {};
+    const orphanedFiles = [];
+
+    while (backupCursor.hasNext()) {
+        const doc = backupCursor.next();
+
+        // Copy all the files for the full backup.
+        jsTestLog("Copying for full backup: " + tojson(doc));
+        _copyFileHelper(doc.filename, primary.dbpath, fullBackupPath);
+
+        if (doc.required) {
+            jsTestLog("Copying required file for selective backup: " + tojson(doc));
+            _copyFileHelper(doc.filename, primary.dbpath, selectiveBackupPath);
+            continue;
+        }
+
+        const filename = doc.filename.substring(doc.filename.lastIndexOf(kSeparator) + 1);
+        const identStem = filename.split(".")[0];
+
+        if (doc.ns.length == 0) {
+            // The file is known to the storage engine but not the durable catalog at the backups
+            // checkpoint timestamp. This file would get dropped during startup recovery.
+            jsTestLog("Skipping orphaned file for selective backup: " + tojson(doc));
+            assert.eq("", doc.uuid);
+            orphanedFiles.push(identStem);
+            continue;
+        }
+
+        assert.neq("", doc.ns);
+        assert.neq("", doc.uuid);
+
+        if (optionalFilesCopied.hasOwnProperty(doc.uuid)) {
+            // At least one file for this ident was already copied, so this needs to be copied too.
+            if (identStem.startsWith("collection-")) {
+                optionalFilesCopied[doc.uuid].collUri = identStem;
+            } else if (identStem.startsWith("index-")) {
+                optionalFilesCopied[doc.uuid].indexUris.push(identStem);
+            }
+
+            jsTestLog("Copying for selective backup: " + tojson(doc));
+            _copyFileHelper(doc.filename, primary.dbpath, selectiveBackupPath);
+            continue;
+        }
+
+        if (optionalFilesSkipped.hasOwnProperty(doc.uuid)) {
+            // At least one file for this ident was already skipped, so this needs to be skipped
+            // too.
+            if (identStem.startsWith("collection-")) {
+                optionalFilesSkipped[doc.uuid].collUri = identStem;
+            } else if (identStem.startsWith("index-")) {
+                optionalFilesSkipped[doc.uuid].indexUris.push(identStem);
+            }
+
+            jsTestLog("Skipping for selective backup: " + tojson(doc));
+            continue;
+        }
+
+        const entry = {collUri: undefined, indexUris: [], ns: doc.ns, uuid: doc.uuid};
+        if (identStem.startsWith("collection-")) {
+            entry.collUri = identStem;
+        } else if (identStem.startsWith("index-")) {
+            entry.indexUris.push(identStem);
+        }
+
+        if (Math.random() < 0.5) {
+            // 50% chance to either copy or skip this ident.
+            optionalFilesSkipped[doc.uuid] = entry;
+
+            jsTestLog("Skipping for selective backup: " + tojson(doc));
+            continue;
+        }
+
+        optionalFilesCopied[doc.uuid] = entry;
+
+        jsTestLog("Copying for selective backup: " + tojson(doc));
+        _copyFileHelper(doc.filename, primary.dbpath, selectiveBackupPath);
+    }
+
+    backupCursor.close();
+
+    jsTestLog("Optional files copied for selective backup: " + tojson(optionalFilesCopied));
+    jsTestLog("Optional files skipped for selective backup: " + tojson(optionalFilesSkipped));
+
+    const numCollectionsSkipped = Object.keys(optionalFilesSkipped).length;
+
+    // TODO SERVER-62605: remove this block after WT-8703 is complete.
+    for (const optionalFileSkipped of Object.keys(optionalFilesSkipped)) {
+        const entry = optionalFilesSkipped[optionalFileSkipped];
+        removeUriFromWiredTigerBackup(selectiveBackupPath, entry.collUri);
+        for (const indexUri of entry.indexUris) {
+            removeUriFromWiredTigerBackup(selectiveBackupPath, indexUri);
+        }
+    }
+
+    // Restore the full backup.
+    const fullBackupConn =
+        MongoRunner.runMongod({dbpath: fullBackupPath, noCleanData: true, restore: ""});
+    assert(fullBackupConn);
+
+    // Clear the RamLog to avoid reading the log from the restore above.
+    clearRawMongoProgramOutput();
+
+    // Restore the selective backup.
+    const selectiveBackupConn =
+        MongoRunner.runMongod({dbpath: selectiveBackupPath, noCleanData: true, restore: ""});
+    assert(selectiveBackupConn);
+
+    // Files known to the storage engine but not the durable catalog at the recovery timestamp will
+    // be dropped and need to be removed from optionalFilesCopied.
+    rawMongoProgramOutput().split('\n').forEach((line) => {
+        if (!line.includes("Dropping unknown ident") && !line.includes("Dropping internal ident")) {
+            return;
+        }
+
+        // JSONify the log line.
+        const firstBraceIdx = line.search("{");
+        const lineObj = JSON.parse(line.substring(firstBraceIdx));
+        const droppedIdent = lineObj.attr.ident;
+
+        if (orphanedFiles.includes(droppedIdent)) {
+            jsTestLog("Removing orphaned file during startup recovery: " + droppedIdent);
+            const index = orphanedFiles.indexOf(droppedIdent);
+            orphanedFiles.splice(index, 1);
+            return;
+        }
+
+        for (const optionalFileCopied of Object.keys(optionalFilesCopied)) {
+            const entry = optionalFilesCopied[optionalFileCopied];
+            if (entry.collUri == droppedIdent || entry.indexUris.includes(droppedIdent)) {
+                jsTestLog("Removing optional file copied during startup recovery: " +
+                          tojson(entry));
+                delete optionalFilesCopied[optionalFileCopied];
+                break;
+            }
+        }
+    });
+
+    assert.eq(0, orphanedFiles.length, orphanedFiles);
+
+    const selectiveAdminDb = selectiveBackupConn.getDB("admin");
+    const dbList = selectiveAdminDb.runCommand({listDatabases: 1, nameOnly: true}).databases;
+
+    dbList.forEach(function(dbInfo) {
+        const dbName = dbInfo.name;
+        const collList = selectiveBackupConn.getDB(dbName)
+                             .runCommand({listCollections: 1, filter: {type: "collection"}})
+                             .cursor.firstBatch;
+
+        collList.forEach(function(collInfo) {
+            const uuidString = extractUUIDFromObject(collInfo.info.uuid);
+
+            if (optionalFilesCopied.hasOwnProperty(uuidString)) {
+                let collName;
+                if (collInfo.options.hasOwnProperty("timeseries")) {
+                    collName = dbName + "." + collInfo.name.replace("system.buckets.", "");
+                } else {
+                    collName = dbName + "." + collInfo.name;
+                }
+
+                // If this collection was optional and copied, check that the namespace is the same
+                // as it was during the backup.
+                assert.eq(optionalFilesCopied[uuidString].ns, collName);
+            }
+
+            // Remove the entry in optionalFilesCopied, if it exists. optionalFilesCopied needs to
+            // be empty at the end of this procedure, otherwise we missed restoring a collection we
+            // copied, which would be a bug.
+            delete optionalFilesCopied[uuidString];
+
+            // Remove the entry in optionalFilesSkipped, if it exists. If this happens, then we
+            // restored a collection we didn't backup, which would be a bug.
+            delete optionalFilesSkipped[uuidString];
+        });
+
+        const fullDbHash =
+            assert.commandWorked(fullBackupConn.getDB(dbName).runCommand({dbHash: 1}));
+        const selectiveDbHash =
+            assert.commandWorked(selectiveBackupConn.getDB(dbName).runCommand({dbHash: 1}));
+
+        for (const collection of Object.keys(selectiveDbHash.collections)) {
+            if (collection == "system.views") {
+                // Skip comparing dbHash on 'system.views' collections as views for collections not
+                // restored are removed.
+                continue;
+            }
+
+            const selectiveCollHash = selectiveDbHash.collections[collection];
+            const fullCollHash = fullDbHash.collections[collection];
+
+            assert.eq(selectiveCollHash, fullCollHash);
+        }
+    });
+
+    MongoRunner.stopMongod(fullBackupConn);
+    MongoRunner.stopMongod(selectiveBackupConn);
+
+    // Verify that we restored all non-required backed up collections.
+    assert.eq(0, Object.keys(optionalFilesCopied).length, optionalFilesCopied);
+
+    // Verify that we didn't restore collections not backed up.
+    assert.eq(
+        numCollectionsSkipped, Object.keys(optionalFilesSkipped).length, optionalFilesSkipped);
+
+    // TODO SERVER-63787: test point-in-time restore functionality.
+}
+
+stopFSMClient(fsmPid);
+rst.stopSet();
+}());
