eba57578e83a1cbb1bb2f0528f61532531b0ca75
sergey.galtsev <sergey.galtsev@mongodb.com>
2022-03-31 15:22:40 +0000

    SERVER-63791 support unindexed fle2 encrypted fields
    GitOrigin-RevId: 211baa8da45861741390a1f4b452224a4a181115

--- src/mongo/db/modules/enterprise/jstests/fle2/basic_insert.js
+++ src/mongo/db/modules/enterprise/jstests/fle2/basic_insert.js
@@ -21,48 +21,64 @@ dbTest.dropDatabase();
 let client = new EncryptedClient(db.getMongo(), dbName);
 
 assert.commandWorked(client.createEncryptionCollection("basic", {
-    encryptedFields:
-        {"fields": [{"path": "first", "bsonType": "string", "queries": {"queryType": "equality"}}]}
+    encryptedFields: {
+        "fields": [
+            {"path": "first", "bsonType": "string", "queries": {"queryType": "equality"}},
+            {"path": "middle", "bsonType": "string"},
+            {"path": "aka", "bsonType": "string", "queries": {"queryType": "equality"}},
+        ]
+    }
 }));
 
 let edb = client.getDB();
 
 // Insert a document with a field that gets encrypted
-let res = assert.commandWorked(
-    edb.runCommand({"insert": "basic", documents: [{"_id": 1, "first": "mark"}]}));
+let res = assert.commandWorked(edb.runCommand({
+    "insert": "basic",
+    documents: [{
+        "_id": 1,
+        "first": "dwayne",
+        "middle": "elizondo mountain dew herbert",
+        "aka": "president camacho"
+    }]
+}));
 print(tojson(res));
 assert.eq(res.n, 1);
 client.assertWriteCommandReplyFields(res);
 
-client.assertEncryptedCollectionCounts("basic", 1, 1, 0, 1);
+client.assertEncryptedCollectionCounts("basic", 1, 2, 0, 2);
 
 // Verify it is encrypted with an unencrypted client
 let rawDoc = dbTest.basic.find().toArray()[0];
 print(tojson(rawDoc));
 assertIsIndexedEncryptedField(rawDoc["first"]);
+assertIsUnindexedEncryptedField(rawDoc["middle"]);
+assertIsIndexedEncryptedField(rawDoc["aka"]);
 assert(rawDoc["__safeContent__"] !== undefined);
 
 // Verify we decrypt it clean with an encrypted client.
 let doc = edb.basic.find().toArray()[0];
 print(tojson(doc));
-assert.eq(doc["first"], "mark");
+assert.eq(doc["first"], "dwayne");
+assert.eq(doc["middle"], "elizondo mountain dew herbert");
+assert.eq(doc["aka"], "president camacho");
 assert(doc["__safeContent__"] !== undefined);
 
-client.assertOneEncryptedDocumentFields("basic", {}, {"first": "mark"});
+client.assertOneEncryptedDocumentFields("basic", {}, {"first": "dwayne"});
 
 // Make an insert with no encrypted fields
-assert.commandWorked(edb.basic.insert({"last": "marco"}));
+assert.commandWorked(edb.basic.insert({"last": "camacho"}));
 
-rawDoc = dbTest.basic.find({"last": "marco"}).toArray()[0];
+rawDoc = dbTest.basic.find({"last": "camacho"}).toArray()[0];
 print(tojson(rawDoc));
-assert.eq(rawDoc["last"], "marco");
+assert.eq(rawDoc["last"], "camacho");
 assert(rawDoc["__safeContent__"] === undefined);
 
-client.assertEncryptedCollectionCounts("basic", 2, 1, 0, 1);
+client.assertEncryptedCollectionCounts("basic", 2, 2, 0, 2);
 
 // Trigger a duplicate key exception and validate the response
 res = assert.commandFailed(
-    edb.runCommand({"insert": "basic", documents: [{"_id": 1, "first": "marco"}]}));
+    edb.runCommand({"insert": "basic", documents: [{"_id": 1, "first": "camacho"}]}));
 print(tojson(res));
 
 assert.eq(res.n, 0);
