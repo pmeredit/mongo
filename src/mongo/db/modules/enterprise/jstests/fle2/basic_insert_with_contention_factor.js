f1ffcf52a99b88abd97b1c450129b9b4732d17cb
joshua <80741223+jlap199@users.noreply.github.com>
2022-04-04 23:16:20 +0000

    SERVER-64749 Test inserting with non-trivial contention factor
    GitOrigin-RevId: fbb07f336ed4c25a064e302cef38863660a871b0

---
+++ src/mongo/db/modules/enterprise/jstests/fle2/basic_insert_with_contention_factor.js
@@ -0,0 +1,79 @@
+/**
+ * Test encrypted insert contention factor works
+ *
+ * @tags: [
+ *  featureFlagFLE2,
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+if (!isFLE2Enabled()) {
+    return;
+}
+
+const dbName = 'basic_insert_with_contention_factor';
+const dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+const client = new EncryptedClient(db.getMongo(), dbName);
+
+assert.commandWorked(client.createEncryptionCollection("basic", {
+    encryptedFields: {
+        "fields": [
+            {
+                "path": "first",
+                "bsonType": "string",
+                "queries": {
+                    "queryType": "equality",
+                    "contention": NumberLong(2),
+                }
+            },
+            {
+                "path": "last",
+                "bsonType": "string",
+                "queries": {
+                    "queryType": "equality",
+                }
+            },
+        ]
+    }
+}));
+
+const edb = client.getDB();
+
+assert.commandWorked(
+    edb.runCommand({"insert": "basic", documents: [{"_id": 1, "first": "Bob", "last": "A"}]}));
+assert.commandWorked(
+    edb.runCommand({"insert": "basic", documents: [{"_id": 2, "first": "Bob", "last": "B"}]}));
+assert.commandWorked(
+    edb.runCommand({"insert": "basic", documents: [{"_id": 3, "first": "Tim", "last": "C"}]}));
+assert.commandWorked(
+    edb.runCommand({"insert": "basic", documents: [{"_id": 4, "first": "Tim", "last": "D"}]}));
+
+client.assertEncryptedCollectionDocuments("basic", [
+    {"_id": 1, "first": "Bob", "last": "A"},
+    {"_id": 2, "first": "Bob", "last": "B"},
+    {"_id": 3, "first": "Tim", "last": "C"},
+    {"_id": 4, "first": "Tim", "last": "D"},
+]);
+
+{
+    const docs = edb.basic.find({"first": "Bob"}).toArray().sort(function(a, b) {
+        return a.last < b.last ? -1 : a.last > b.last ? 1 : 0;
+    });
+    assert.eq(docs.length, 2);
+    assert.eq(docs[0]["last"], "A");
+    assert.eq(docs[1]["last"], "B");
+}
+{
+    const docs = edb.basic.find({"first": "Tim"}).toArray().sort(function(a, b) {
+        return a.last < b.last ? -1 : a.last > b.last ? 1 : 0;
+    });
+    assert.eq(docs.length, 2);
+    assert.eq(docs[0]["last"], "C");
+    assert.eq(docs[1]["last"], "D");
+}
+}());
