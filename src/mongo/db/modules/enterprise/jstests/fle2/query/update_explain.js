33a396e8c2bd8975704b6ed0a48d23fccf53baa5
Davis Haupt <davis.haupt@mongodb.com>
2022-04-07 18:35:08 +0000

    SERVER-65237 Server-side rewrite in explain for delete and update commands
    GitOrigin-RevId: f19983e0b05464623f3afb4de8b94bb0918e6420

---
+++ src/mongo/db/modules/enterprise/jstests/fle2/query/update_explain.js
@@ -0,0 +1,45 @@
+/**
+ * Test explain for delete with encrypted fields for FLE2.
+ * @tags: [
+ *  featureFlagFLE2,
+ * ]
+ */
+load('jstests/aggregation/extras/utils.js');  // For assertArrayEq.
+load("jstests/fle2/libs/encrypted_client_util.js");
+load("src/mongo/db/modules/enterprise/jstests/fle2/query/match_expression_data.js");
+
+(function() {
+const assertExplainResult = (edb, collName, query, assertions) => {
+    const result = assert.commandWorked(edb.runCommand({
+        explain: {
+            update: collName,
+            updates: [{q: query, u: {$set: {modified: true}}}],
+        },
+        verbosity: "queryPlanner"
+    }));
+    let inputQuery;
+    if (result.queryPlanner.winningPlan.shards) {
+        inputQuery = result.queryPlanner.winningPlan.shards[0].parsedQuery;
+    } else {
+        inputQuery = result.queryPlanner.parsedQuery;
+    }
+
+    assertions(inputQuery, result);
+};
+
+const {encryptedFields} = matchExpressionFLETestCases;
+const collName = jsTestName();
+
+runEncryptedTest(db, "update_explain", collName, encryptedFields, (edb) => {
+    assert.commandWorked(edb[collName].insert({_id: 0, ssn: "123"}));
+    assertExplainResult(edb, collName, {"ssn": "123"}, (query, result) => {
+        assert(query.hasOwnProperty(kSafeContentField), result);
+        assert(!query.hasOwnProperty("ssn"), result);
+    });
+    assertExplainResult(edb, collName, {_id: 0}, (query, result) => {
+        assert(!query.hasOwnProperty(kSafeContentField), result);
+        assert(!query.hasOwnProperty("ssn"), result);
+        assert(query.hasOwnProperty("_id"), result);
+    });
+});
+}());
