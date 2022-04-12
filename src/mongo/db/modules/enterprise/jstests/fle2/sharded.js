4161897e9b255f4515fbbcb616405580fd37c6d7
joshua <80741223+jlap199@users.noreply.github.com>
2022-04-12 19:14:35 +0000

    SERVER-64897 Test sharding an encrypted collection on an unencrypted field
    GitOrigin-RevId: 900dcb3df9efb90510e58000efbc176f46e58ee9

---
+++ src/mongo/db/modules/enterprise/jstests/fle2/sharded.js
@@ -0,0 +1,109 @@
+/**
+ * Test CRUD operations work on sharded collections.
+ *
+ * @tags: [
+ * requires_fcv_60
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+load("jstests/concurrency/fsm_workload_helpers/server_types.js");
+
+(function() {
+'use strict';
+
+if (!isFLE2ShardingEnabled()) {
+    return;
+}
+
+const dbName = 'sharded';
+const dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+const client = new EncryptedClient(db.getMongo(), dbName);
+
+assert.commandWorked(client.createEncryptionCollection("basic", {
+    encryptedFields:
+        {"fields": [{"path": "a", "bsonType": "string", "queries": {"queryType": "equality"}}]}
+}));
+
+const edb = client.getDB();
+
+assert(isMongos(edb));
+assert.commandWorked(db.adminCommand({shardCollection: 'sharded.basic', key: {b: 'hashed'}}));
+
+// Insert 10 documents.
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(edb.basic.insert({
+        _id: i,
+        a: i.toString(),
+        b: ((i + 1) % 10).toString(),
+    }));
+}
+
+client.assertEncryptedCollectionCounts("basic", 10, 10, 0, 10);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "0", b: "1"},
+    {_id: 1, a: "1", b: "2"},
+    {_id: 2, a: "2", b: "3"},
+    {_id: 3, a: "3", b: "4"},
+    {_id: 4, a: "4", b: "5"},
+    {_id: 5, a: "5", b: "6"},
+    {_id: 6, a: "6", b: "7"},
+    {_id: 7, a: "7", b: "8"},
+    {_id: 8, a: "8", b: "9"},
+    {_id: 9, a: "9", b: "0"},
+]);
+
+// Update all 10 documents.
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(edb.basic.updateOne({b: i.toString()}, {$set: {a: i.toString()}}));
+}
+client.assertEncryptedCollectionCounts("basic", 10, 20, 10, 30);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "1", b: "1"},
+    {_id: 1, a: "2", b: "2"},
+    {_id: 2, a: "3", b: "3"},
+    {_id: 3, a: "4", b: "4"},
+    {_id: 4, a: "5", b: "5"},
+    {_id: 5, a: "6", b: "6"},
+    {_id: 6, a: "7", b: "7"},
+    {_id: 7, a: "8", b: "8"},
+    {_id: 8, a: "9", b: "9"},
+    {_id: 9, a: "0", b: "0"},
+]);
+
+// Modify all 10 documents.
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(edb.runCommand({
+        findAndModify: "basic",
+        query: {b: i.toString()},
+        update: {$set: {a: ((i + 1) % 10).toString()}},
+    }));
+}
+client.assertEncryptedCollectionCounts("basic", 10, 30, 20, 50);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "2", b: "1"},
+    {_id: 1, a: "3", b: "2"},
+    {_id: 2, a: "4", b: "3"},
+    {_id: 3, a: "5", b: "4"},
+    {_id: 4, a: "6", b: "5"},
+    {_id: 5, a: "7", b: "6"},
+    {_id: 6, a: "8", b: "7"},
+    {_id: 7, a: "9", b: "8"},
+    {_id: 8, a: "0", b: "9"},
+    {_id: 9, a: "1", b: "0"},
+]);
+
+// Delete 5 documents.
+for (let i = 0; i < 10; i += 2) {
+    assert.commandWorked(edb.basic.deleteOne({b: i.toString()}));
+}
+client.assertEncryptedCollectionCounts("basic", 5, 30, 25, 55);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "2", b: "1"},
+    {_id: 2, a: "4", b: "3"},
+    {_id: 4, a: "6", b: "5"},
+    {_id: 6, a: "8", b: "7"},
+    {_id: 8, a: "0", b: "9"},
+]);
+}());
---
+++ src/mongo/db/modules/enterprise/jstests/fle2/txn_sharded.js
@@ -0,0 +1,155 @@
+/**
+ * Test CRUD operations work on sharded collections in a transaction.
+ *
+ * @tags: [
+ * requires_fcv_60
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+load("jstests/concurrency/fsm_workload_helpers/server_types.js");
+
+(function() {
+'use strict';
+
+if (!isFLE2ShardingEnabled()) {
+    return;
+}
+
+const dbName = 'txn_sharded';
+const dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+const client = new EncryptedClient(db.getMongo(), dbName);
+
+assert.commandWorked(client.createEncryptionCollection("basic", {
+    encryptedFields:
+        {"fields": [{"path": "a", "bsonType": "string", "queries": {"queryType": "equality"}}]}
+}));
+
+const edb = client.getDB();
+
+assert(isMongos(edb));
+assert.commandWorked(db.adminCommand({shardCollection: 'txn_sharded.basic', key: {b: 'hashed'}}));
+
+const session = edb.getMongo().startSession({causalConsistency: false});
+const sessionDB = session.getDatabase(dbName);
+const sessionColl = sessionDB.getCollection("basic");
+
+// Insert 10 documents in a transaction and commit.
+session.startTransaction();
+
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(sessionColl.insert({
+        _id: i,
+        a: i.toString(),
+        b: ((i + 1) % 10).toString(),
+    }));
+}
+
+session.commitTransaction();
+
+client.assertEncryptedCollectionCounts("basic", 10, 10, 0, 10);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "0", b: "1"},
+    {_id: 1, a: "1", b: "2"},
+    {_id: 2, a: "2", b: "3"},
+    {_id: 3, a: "3", b: "4"},
+    {_id: 4, a: "4", b: "5"},
+    {_id: 5, a: "5", b: "6"},
+    {_id: 6, a: "6", b: "7"},
+    {_id: 7, a: "7", b: "8"},
+    {_id: 8, a: "8", b: "9"},
+    {_id: 9, a: "9", b: "0"},
+]);
+
+// Update 10 documents in a transaction and abort.
+session.startTransaction();
+
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(sessionColl.updateOne({b: i.toString()}, {$set: {a: i.toString()}}));
+}
+
+assert.commandWorked(session.abortTransaction_forTesting());
+
+client.assertEncryptedCollectionCounts("basic", 10, 10, 0, 10);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "0", b: "1"},
+    {_id: 1, a: "1", b: "2"},
+    {_id: 2, a: "2", b: "3"},
+    {_id: 3, a: "3", b: "4"},
+    {_id: 4, a: "4", b: "5"},
+    {_id: 5, a: "5", b: "6"},
+    {_id: 6, a: "6", b: "7"},
+    {_id: 7, a: "7", b: "8"},
+    {_id: 8, a: "8", b: "9"},
+    {_id: 9, a: "9", b: "0"},
+]);
+
+// Modify all 10 documents in a transaction and commit.
+session.startTransaction();
+
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(sessionColl.runCommand({
+        findAndModify: "basic",
+        query: {b: i.toString()},
+        update: {$set: {a: i.toString()}},
+    }));
+}
+
+session.commitTransaction();
+
+client.assertEncryptedCollectionCounts("basic", 10, 20, 10, 30);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "1", b: "1"},
+    {_id: 1, a: "2", b: "2"},
+    {_id: 2, a: "3", b: "3"},
+    {_id: 3, a: "4", b: "4"},
+    {_id: 4, a: "5", b: "5"},
+    {_id: 5, a: "6", b: "6"},
+    {_id: 6, a: "7", b: "7"},
+    {_id: 7, a: "8", b: "8"},
+    {_id: 8, a: "9", b: "9"},
+    {_id: 9, a: "0", b: "0"},
+]);
+
+// Delete 5 documents in a transaction and abort.
+session.startTransaction();
+
+for (let i = 0; i < 10; i += 2) {
+    assert.commandWorked(sessionColl.deleteOne({b: i.toString()}));
+}
+
+assert.commandWorked(session.abortTransaction_forTesting());
+
+client.assertEncryptedCollectionCounts("basic", 10, 20, 10, 30);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "1", b: "1"},
+    {_id: 1, a: "2", b: "2"},
+    {_id: 2, a: "3", b: "3"},
+    {_id: 3, a: "4", b: "4"},
+    {_id: 4, a: "5", b: "5"},
+    {_id: 5, a: "6", b: "6"},
+    {_id: 6, a: "7", b: "7"},
+    {_id: 7, a: "8", b: "8"},
+    {_id: 8, a: "9", b: "9"},
+    {_id: 9, a: "0", b: "0"},
+]);
+
+// Delete 5 documents in a transaction and commit.
+session.startTransaction();
+
+for (let i = 0; i < 10; i += 2) {
+    assert.commandWorked(sessionColl.deleteOne({b: i.toString()}));
+}
+
+session.commitTransaction();
+
+client.assertEncryptedCollectionCounts("basic", 5, 20, 15, 35);
+client.assertEncryptedCollectionDocuments("basic", [
+    {_id: 0, a: "1", b: "1"},
+    {_id: 2, a: "3", b: "3"},
+    {_id: 4, a: "5", b: "5"},
+    {_id: 6, a: "7", b: "7"},
+    {_id: 8, a: "9", b: "9"},
+]);
+}());
