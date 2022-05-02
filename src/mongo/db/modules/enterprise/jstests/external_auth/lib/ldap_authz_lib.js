a9558c88645c07bd51dfb71e9b1ac526194a74d0
Gregory Noma <gregory.noma@gmail.com>
2022-05-02 14:22:51 -0400

    SERVER-65918 Remove references to `ephemeralForTest`
    GitOrigin-RevId: 34b460e8c54ed5072364074aea4642f8a60f5040

--- src/mongo/db/modules/enterprise/jstests/external_auth/lib/ldap_authz_lib.js
+++ src/mongo/db/modules/enterprise/jstests/external_auth/lib/ldap_authz_lib.js
@@ -322,8 +322,7 @@ function runTests(testCallback, configGenerator, callbackOptions) {
     // ReplSetTest cannot be done automatically without the shell performing such authentication, so
     // in this test we must make the choice explicitly, based on the global test options.
     var wcMajorityJournalDefault;
-    if (jsTestOptions().noJournal || jsTestOptions().storageEngine == "ephemeralForTest" ||
-        jsTestOptions().storageEngine == "inMemory") {
+    if (jsTestOptions().noJournal || jsTestOptions().storageEngine == "inMemory") {
         wcMajorityJournalDefault = false;
     } else {
         wcMajorityJournalDefault = true;
