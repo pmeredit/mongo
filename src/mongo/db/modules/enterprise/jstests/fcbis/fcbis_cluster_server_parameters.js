32970263c1dc160d9fc9af60bfbe6fba40bbcb41
Varun Ravichandran <varun.ravichandran@mongodb.com>
2022-05-08 23:02:41 +0000

    SERVER-66082: Propagate cluster server parameters to new nodes during file-copy based initial sync
    GitOrigin-RevId: 9f13785685a93c8ce4adfc563c34021b333c81ef

---
+++ src/mongo/db/modules/enterprise/jstests/fcbis/fcbis_cluster_server_parameters.js
@@ -0,0 +1,58 @@
+/**
+ * Tests that cluster server parameters are propagated onto new nodes that use file-copy based
+ * initial sync.
+ *
+ * @tags: [requires_fcv_60, requires_persistence, requires_wiredtiger]
+ */
+
+(function() {
+"use strict";
+
+load("jstests/replsets/rslib.js");
+load("jstests/libs/fail_point_util.js");
+
+const testName = TestData.testName;
+const rst = new ReplSetTest({
+    name: testName,
+    nodes: [{
+        setParameter: {
+            'initialSyncMethod': 'fileCopyBased',
+        }
+    }]
+});
+rst.startSet();
+rst.initiateWithHighElectionTimeout();
+const primary = rst.getPrimary();
+
+jsTestLog("Setting a cluster parameter on the replica set");
+assert.commandWorked(
+    primary.adminCommand({setClusterParameter: {testIntClusterParameter: {intData: 5}}}));
+rst.awaitReplication();
+
+jsTestLog("Ensuring there is an up-to-date stable checkpoint for FCBIS to copy from.");
+rst.awaitLastStableRecoveryTimestamp();
+assert.commandWorked(primary.adminCommand({fsync: 1}));
+
+jsTestLog("Adding the initial sync destination node to the replica set.");
+const initialSyncNode = rst.add({
+    rsConfig: {priority: 0, votes: 0},
+    setParameter: {
+        'initialSyncMethod': 'fileCopyBased',
+        'numInitialSyncAttempts': 1,
+    }
+});
+rst.reInitiate();
+
+// Wait for initial sync to complete.
+rst.awaitReplication();
+
+jsTestLog("Checking that the new node has the up-to-date cluster parameter value");
+const clusterServerParameters = assert
+                                    .commandWorked(initialSyncNode.adminCommand(
+                                        {getClusterParameter: "testIntClusterParameter"}))
+                                    .clusterParameters;
+assert.eq(clusterServerParameters[0].intData, 5);
+
+jsTestLog("Done with test.");
+rst.stopSet();
+})();
