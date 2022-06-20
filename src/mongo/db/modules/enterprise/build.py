9e55259997d994bbd572fe0af1531302955b1133
Adam Rayner <adam.rayner@gmail.com>
2022-06-20 20:06:36 +0000

    SERVER-65733: remove SNMP module
    GitOrigin-RevId: 40744ea466afddf242d6db42e7af2ee4ca4c3d7c

--- src/mongo/db/modules/enterprise/build.py
+++ src/mongo/db/modules/enterprise/build.py
@@ -34,7 +34,6 @@ def configure(conf, env):
         "sasl",
         "search",
         "serverless",
-        "snmp",
     ]
 
     selected_features = []
@@ -63,30 +62,6 @@ def configure(conf, env):
     if 'sasl' in env['MONGO_ENTERPRISE_FEATURES']:
         env['MONGO_BUILD_SASL_CLIENT'] = True
 
-    if 'snmp' in env['MONGO_ENTERPRISE_FEATURES']:
-
-        if not env.TargetOSIs('darwin'):
-            if not conf.CheckCXXHeader("net-snmp/net-snmp-config.h"):
-                env.ConfError("Could not find <net-snmp/net-snmp-config.h>, required for "
-                              "enterprise build.")
-
-        snmpFlags = None
-        if not env.TargetOSIs("windows", "darwin"):
-            try:
-                snmpFlags = env.ParseFlags("!net-snmp-config --agent-libs")
-            except OSError as ose:
-                # the net-snmp-config command was not found
-                env.ConfError("Could not find or execute 'net-snmp-config': {0}", ose)
-            else:
-                # Don't inject the whole environment held in snmpFlags, because the net-snmp-config
-                # utility throws some weird flags in that get picked up in LINKFLAGS that we don't want
-                # (see SERVER-23200). The only thing we need here are the LIBS and the LIBPATH, and any
-                # rpath that was set in LINKFLAGS.
-                #
-                # We need to do LINKFLAGS globally since it affects runtime of executables.
-                env.Append(LINKFLAGS=[flag for flag in snmpFlags['LINKFLAGS'] if '-Wl,-rpath' in flag])
-
-
     # Compute a path to use for including files that are generated in the build directory.
     # Computes: $BUILD_DIR/mongo/db/modules/<enterprise_module_name>/src
     src_include_path = os.path.join("$BUILD_DIR", str(env.Dir(root).Dir('src'))[4:] )
@@ -107,15 +82,6 @@ def configure(conf, env):
             if 'encryptdb' in env['MONGO_ENTERPRISE_FEATURES']:
                 env.Append(CPPDEFINES=[("MONGO_ENTERPRISE_ENCRYPTDB", 1)])
 
-        if builder:
-            if 'snmp' in env['MONGO_ENTERPRISE_FEATURES']:
-                if env.TargetOSIs("windows"):
-                    env['SNMP_SYSLIBDEPS'] = ['netsnmp','netsnmpagent','netsnmpmibs']
-                    env.Append(CPPDEFINES=["NETSNMP_NO_INLINE"])
-                elif not env.TargetOSIs("darwin"):
-                    env['SNMP_SYSLIBDEPS'] = [lib for lib in snmpFlags['LIBS'] if lib != "pci"]
-                    env.Append(LIBPATH=snmpFlags['LIBPATH'])
-                    env.Append(CPPDEFINES=["NETSNMP_NO_INLINE"])
         return env
 
     env['MODULE_INJECTORS']['enterprise'] = injectEnterpriseModule
