a1eb3d922c0751c81360c1596d1ed27c17a52e3a
Sara Golemon <sara.golemon@mongodb.com>
2022-07-15 12:14:04 -0500

    SERVER-67660 Add JWKManager
    GitOrigin-RevId: bcf5d61a7e6d743ab4e93baec5fa8809985c3196

--- src/mongo/db/modules/enterprise/src/sasl/oidc_params.cpp
+++ src/mongo/db/modules/enterprise/src/sasl/oidc_params.cpp
@@ -4,8 +4,44 @@
 
 #include "oidc_params.h"
 
+#include <fstream>
+
+#include "mongo/base/init.h"
+#include "mongo/bson/json.h"
+#include "mongo/crypto/jwk_manager.h"
+
 namespace mongo {
+namespace {
+crypto::JWKManager oidcKeyManager;
+
+MONGO_INITIALIZER(OIDCKeyManager)(InitializerContext*) {
+    if (oidcGlobalParams.keySetFile.empty()) {
+        return;
+    }
+
+    std::ifstream keyFile(oidcGlobalParams.keySetFile, std::ios_base::in);
+    uassert(ErrorCodes::OperationFailed,
+            str::stream() << "Failed opening OID key file: " << oidcGlobalParams.keySetFile,
+            keyFile.is_open());
+
+    BSONObj keyData;
+    try {
+        std::string data((std::istreambuf_iterator<char>(keyFile)),
+                         std::istreambuf_iterator<char>());
+        keyData = fromjson(data);
+    } catch (const DBException& ex) {
+        uassertStatusOK(ex.toStatus().withContext(
+            str::stream() << "Invalid JSON in '" << oidcGlobalParams.keySetFile << "'"));
+    }
+
+    oidcKeyManager = crypto::JWKManager(keyData);
+}
+}  // namespace
 
 OIDCGlobalParams oidcGlobalParams;
+
+crypto::JWKManager* getOIDCKeyManager() {
+    return &oidcKeyManager;
+}
 
 }  // namespace mongo
--- src/mongo/db/modules/enterprise/src/sasl/oidc_params.h
+++ src/mongo/db/modules/enterprise/src/sasl/oidc_params.h
@@ -6,14 +6,22 @@
 
 #include <string>
 
+#include "mongo/crypto/jwk_manager.h"
+
 namespace mongo {
 
 struct OIDCGlobalParams {
     std::string authURL;
     std::string clientId;
     std::string clientSecret;
+    std::string keySetFile;
 };
 
 extern OIDCGlobalParams oidcGlobalParams;
+
+/**
+ * Retrieve the global JWKManager instance for OIDC keys.
+ */
+crypto::JWKManager* getOIDCKeyManager();
 
 }  // namespace mongo
