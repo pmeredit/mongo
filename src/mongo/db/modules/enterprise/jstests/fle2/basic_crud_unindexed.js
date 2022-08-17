53da84f7887041ab6c88dcc6548a9a107bbb03ba
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2022-08-17 16:38:06 +0200

    SERVER-68065 Cannot update unindexed field in QE
    GitOrigin-RevId: ff437f9bb885ad196862e3f8a78a2f8ea5bbd351

---
+++ src/mongo/db/modules/enterprise/jstests/fle2/basic_crud_unindexed.js
@@ -0,0 +1,40 @@
+/**
+ * Test that crud functions on unindexed fields works correctly.
+ *
+ * Since query for sharded findAndModify must contain the shard key,
+ * we need to disable this test from mongos.
+ *
+ * @tags: [ assumes_against_mongod_not_mongos ]
+ */
+
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+const dbName = 'basic_crud_unindexed';
+const dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+const client = new EncryptedClient(db.getMongo(), dbName);
+
+assert.commandWorked(client.createEncryptionCollection(
+    "basic_unindexed", {encryptedFields: {"fields": [{"path": "field", "bsonType": "string"}]}}));
+
+const edb = client.getDB();
+
+assert.commandWorked(edb.basic_unindexed.insert({"_id": 1, "field": "foo"}));
+
+assert.commandWorked(edb.basic_unindexed.runCommand(
+    {findAndModify: edb.basic_unindexed.getName(), query: {}, update: {$set: {"field": "bar"}}}));
+
+client.assertEncryptedCollectionDocuments("basic_unindexed", [{"_id": 1, "field": "bar"}]);
+
+assert.commandWorked(edb.basic_unindexed.update({}, {$set: {field: 'cool'}}));
+
+client.assertEncryptedCollectionDocuments("basic_unindexed", [{"_id": 1, "field": "cool"}]);
+
+assert.commandWorked(edb.basic_unindexed.deleteOne({}));
+
+client.assertEncryptedCollectionDocuments("basic_unindexed", []);
+})();
