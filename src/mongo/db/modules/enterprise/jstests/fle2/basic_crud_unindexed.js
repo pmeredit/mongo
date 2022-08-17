f32268d1b1019360e536f4179b62354d30e80fbd
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2022-08-17 16:38:06 +0200

    SERVER-68065 Cannot update unindexed field in QE
    GitOrigin-RevId: b5779a06992293984822040e36fc11dfb68365bb

---
+++ src/mongo/db/modules/enterprise/jstests/fle2/basic_crud_unindexed.js
@@ -0,0 +1,35 @@
+/**
+ * Test that crud functions on unindexed fields works correctly.
+ */
+
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+const dbName = 'basic_crud_unindexed';
+const dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+const client = new EncryptedClient(db.getMongo(), dbName);
+
+assert.commandWorked(client.createEncryptionCollection(
+    "basic_unindexed", {encryptedFields: {"fields": [{"path": "field", "bsonType": "string"}]}}));
+
+const edb = client.getDB();
+
+assert.commandWorked(edb.basic_unindexed.insert({"_id": 1, "field": "foo"}));
+
+assert.commandWorked(edb.basic_unindexed.runCommand(
+    {findAndModify: edb.basic_unindexed.getName(), query: {}, update: {$set: {"field": "bar"}}}));
+
+client.assertEncryptedCollectionDocuments("basic_unindexed", [{"_id": 1, "field": "bar"}]);
+
+assert.commandWorked(edb.basic_unindexed.update({}, {$set: {field: 'cool'}}));
+
+client.assertEncryptedCollectionDocuments("basic_unindexed", [{"_id": 1, "field": "cool"}]);
+
+assert.commandWorked(edb.basic_unindexed.deleteOne({}));
+
+client.assertEncryptedCollectionDocuments("basic_unindexed", []);
+})();
