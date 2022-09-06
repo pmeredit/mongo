cf52a6aa7a807a2b03102740f1b0151a65ea0d11
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2022-09-06 16:01:46 -0400

    SERVER-67757 FLE2 Range Query: implement delete
    GitOrigin-RevId: f6c3a2c4f30b00917b60964e04fe0d2fca27a88b

---
+++ src/mongo/db/modules/enterprise/jstests/fle2/basic_delete_range.js
@@ -0,0 +1,57 @@
+/**
+ * Test encrypted delete works
+ *
+ * @tags: [
+ *   featureFlagFLE2Range,
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+// TODO SERVER-67760 remove once feature flag is gone
+if (!isFLE2RangeEnabled()) {
+    jsTest.log("Test skipped because featureFlagFLE2Range is not enabled");
+    return;
+}
+
+let dbName = 'basic_insert_range';
+let dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+let client = new EncryptedClient(db.getMongo(), dbName);
+
+const edb = client.getDB();
+
+jsTest.log("createEncryptionCollection");
+assert.commandWorked(client.createEncryptionCollection("basic", {
+    encryptedFields: {
+        "fields": [
+            {
+                "path": "age",  // first name
+                "bsonType": "int",
+                "queries":
+                    {"queryType": "range", "min": NumberInt(1), "max": NumberInt(16), "sparsity": 1}
+            },
+            {
+                path: "name",
+                bsonType: "string",
+            }
+        ]
+    }
+}));
+
+assert.commandWorked(edb.basic.insert({"name": "Bob", "age": NumberInt(12)}));
+
+const kHypergraphHeight = 5;
+
+client.assertEncryptedCollectionCounts("basic", 1, kHypergraphHeight, 0, kHypergraphHeight);
+
+let doc = edb.basic.find().toArray()[0];
+
+assert.commandWorked(edb.basic.deleteOne({"_id": doc._id}));
+
+client.assertEncryptedCollectionCounts(
+    "basic", 0, kHypergraphHeight, kHypergraphHeight, 2 * kHypergraphHeight);
+}());
---
+++ src/mongo/db/modules/enterprise/jstests/fle2/txn_delete_range.js
@@ -0,0 +1,95 @@
+/**
+ * Test encrypted delete works under a user txn.
+ *
+ * @tags: [
+ * assumes_read_concern_unchanged,
+ * assumes_read_preference_unchanged,
+ * featureFlagFLE2Range,
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+// TODO SERVER-67760 remove once feature flag is gone
+if (!isFLE2RangeEnabled()) {
+    jsTest.log("Test skipped because featureFlagFLE2Range is not enabled");
+    return;
+}
+
+let dbName = 'txn_insert_range';
+let dbTest = db.getSiblingDB(dbName);
+dbTest.dropDatabase();
+
+let client = new EncryptedClient(db.getMongo(), dbName);
+
+assert.commandWorked(client.createEncryptionCollection("basic", {
+    encryptedFields: {
+        "fields": [
+            {
+                "path": "height",
+                "bsonType": "long",
+                "queries": {
+                    "queryType": "range",
+                    "min": NumberLong(1),
+                    "max": NumberLong(16),
+                    "sparsity": 1,
+                }
+            },
+            {
+                "path": "num.num",
+                "bsonType": "int",
+                "queries":
+                    {"queryType": "range", "min": NumberInt(0), "max": NumberInt(3), "sparsity": 2}
+            },
+            {"path": "ssn", "bsonType": "string", "queries": {"queryType": "equality"}}
+        ]
+    }
+}));
+
+let edb = client.getDB();
+
+// Insert a document with a field that gets encrypted
+const session = edb.getMongo().startSession({causalConsistency: false});
+const sessionDB = session.getDatabase(dbName);
+const sessionColl = sessionDB.getCollection("basic");
+
+const kHypergraphHeight = 5;
+
+// Hypergraph for numnum has height of 2 because sparsity is 2
+const kHypergraphNumNum = 2;
+const kEqualityTags = 1;
+const kTagsPerEntry = kHypergraphHeight + kHypergraphNumNum + kEqualityTags;
+
+// Verify we can insert two documents in a txn
+session.startTransaction();
+
+assert.commandWorked(sessionColl.insert(
+    {name: "joe", "height": NumberLong(4), "num": {"num": NumberInt(2)}, "ssn": "abcd"}));
+assert.commandWorked(sessionColl.insert(
+    {name: "bob", "height": NumberLong(5), "num": {"num": NumberInt(1)}, "ssn": "efgh"}));
+
+session.commitTransaction();
+
+client.assertEncryptedCollectionCounts("basic", 2, 2 * kTagsPerEntry, 0, 2 * kTagsPerEntry);
+
+session.startTransaction();
+
+assert.commandWorked(sessionColl.deleteOne({name: "joe"}));
+assert.commandWorked(sessionColl.deleteOne({name: "bob"}));
+
+assert.commandWorked(session.abortTransaction_forTesting());
+
+client.assertEncryptedCollectionCounts("basic", 2, 2 * kTagsPerEntry, 0, 2 * kTagsPerEntry);
+
+session.startTransaction();
+
+assert.commandWorked(sessionColl.deleteOne({name: "joe"}));
+assert.commandWorked(sessionColl.deleteOne({name: "bob"}));
+
+session.commitTransaction();
+
+client.assertEncryptedCollectionCounts(
+    "basic", 0, 2 * kTagsPerEntry, 2 * kTagsPerEntry, 4 * kTagsPerEntry);
+}());
