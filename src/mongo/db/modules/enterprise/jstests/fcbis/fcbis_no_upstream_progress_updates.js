7621c01ac1e09e678e795d43ecd534768dbf7701
Vesselina Ratcheva <vesselina.ratcheva@10gen.com>
2022-09-09 23:26:18 +0000

    SERVER-69049 Add testing for initial sync semantics with FCBIS
    GitOrigin-RevId: ef0319becf01c1e7bb11f5f6f94202f7f32122dd

---
+++ src/mongo/db/modules/enterprise/jstests/fcbis/fcbis_no_upstream_progress_updates.js
@@ -0,0 +1,143 @@
+/**
+ * Test that a node in FCBIS does not report replication progress. There are two routes
+ * these kinds of updates take:
+ *  - via spanning tree:
+ *      initial-syncing nodes should send no replSetUpdatePosition commands upstream at all
+ *  - via heartbeats:
+ *      these nodes should include null lastApplied and lastDurable optimes in heartbeat responses
+ * Neither of these should happen during FCBIS.
+ *
+ * @tags: [requires_persistence, requires_wiredtiger]
+ */
+(function() {
+"use strict";
+
+load("jstests/libs/write_concern_util.js");
+load("jstests/libs/fail_point_util.js");
+load('jstests/replsets/rslib.js');
+
+const testName = jsTestName();
+const rst = new ReplSetTest({name: testName, nodes: [{}, {rsConfig: {priority: 0}}]});
+rst.startSet();
+rst.initiateWithHighElectionTimeout();
+
+const primary = rst.getPrimary();
+const primaryDb = primary.getDB("test");
+// The default WC is majority and this test cannot satisfy majority writes.
+assert.commandWorked(primary.adminCommand(
+    {setDefaultRWConcern: 1, defaultWriteConcern: {w: 1}, writeConcern: {w: "majority"}}));
+
+assert.commandWorked(primaryDb.test.insert({"starting": "doc"}, {writeConcern: {w: 2}}));
+
+jsTestLog("Ensuring there is an up-to-date stable checkpoint for FCBIS to copy from.");
+rst.awaitLastStableRecoveryTimestamp();
+assert.commandWorked(primary.adminCommand({fsync: 1}));
+
+jsTestLog("Adding a new node to the replica set");
+
+const secondary = rst.add({
+    rsConfig: {priority: 0, votes: 0},
+    setParameter: {
+        'failpoint.forceSyncSourceCandidate':
+            tojson({mode: 'alwaysOn', data: {"hostAndPort": primary.host}}),
+        'initialSyncMethod': 'fileCopyBased',
+        'failpoint.fCBISHangBeforeFinish': tojson({mode: 'alwaysOn'}),
+    }
+});
+rst.reInitiate();
+rst.waitForState(secondary, ReplSetTest.State.STARTUP_2);
+
+// Add the new node with votes:0 and then give it votes:1 to avoid 'newlyAdded' and mimic a resync,
+// where a node is in initial sync with 1 vote.
+let nextConfig = rst.getReplSetConfigFromNode(0);
+nextConfig.members[2].votes = 1;
+reconfig(rst, nextConfig, false /* force */, true /* wait */);
+
+// Shut down the steady-state secondary so that it cannot participate in the majority.
+rst.stop(1);
+
+jsTestLog("Waiting for FCBIS node to get to end of initial sync");
+
+assert.soonNoExcept(() => {
+    assert.commandWorked(secondary.adminCommand({
+        waitForFailPoint: "fCBISHangBeforeFinish",
+        timesEntered: 1,
+        maxTimeMS: kDefaultWaitForFailPointTimeout
+    }));
+    return true;
+});
+
+jsTestLog("Checking write acknowledgment");
+
+// Make sure the node does not participate in the acknowledgment of any writes.
+const writeResW2 = primaryDb.runCommand({
+    insert: "test",
+    documents: [{"writeConcernTwo": "shouldfail"}],
+    writeConcern: {w: 2, wtimeout: 4000}
+});
+assert.commandWorkedIgnoringWriteConcernErrors(writeResW2);
+checkWriteConcernTimedOut(writeResW2);
+
+const writeResWMaj = primaryDb.runCommand({
+    insert: "test",
+    documents: [{"writeConcernMajority": "shouldfail"}],
+    writeConcern: {w: "majority", wtimeout: 4000}
+});
+assert.commandWorkedIgnoringWriteConcernErrors(writeResWMaj);
+checkWriteConcernTimedOut(writeResWMaj);
+
+// The FCBIS node should have not sent replSetUpdatePosition at any point.
+sleep(4 * 1000);
+const numUpdatePosition = assert.commandWorked(secondary.adminCommand({serverStatus: 1}))
+                              .metrics.repl.network.replSetUpdatePosition.num;
+assert.eq(0, numUpdatePosition);
+
+const nullOpTime = {
+    "ts": Timestamp(0, 0),
+    "t": NumberLong(-1)
+};
+const nullWallTime = ISODate("1970-01-01T00:00:00Z");
+
+// Make sure that even though the FCBIS node adjusts its lastApplied and lastDurable at the end...
+const statusAfterWMaj = assert.commandWorked(secondary.adminCommand({replSetGetStatus: 1}));
+const secondaryOpTimes = statusAfterWMaj.optimes;
+assert.gte(
+    bsonWoCompare(secondaryOpTimes.appliedOpTime, nullOpTime), 0, () => tojson(secondaryOpTimes));
+assert.gte(
+    bsonWoCompare(secondaryOpTimes.durableOpTime, nullOpTime), 0, () => tojson(secondaryOpTimes));
+assert.neq(nullWallTime, secondaryOpTimes.optimeDate, () => tojson(secondaryOpTimes));
+assert.neq(nullWallTime, secondaryOpTimes.optimeDurableDate, () => tojson(secondaryOpTimes));
+
+// ...the primary thinks they're still null as they were null in the heartbeat responses.
+const primaryStatusRes = assert.commandWorked(primary.adminCommand({replSetGetStatus: 1}));
+const secondaryOpTimesAsSeenByPrimary = primaryStatusRes.members[2];
+assert.docEq(secondaryOpTimesAsSeenByPrimary.optime,
+             nullOpTime,
+             () => tojson(secondaryOpTimesAsSeenByPrimary));
+assert.docEq(secondaryOpTimesAsSeenByPrimary.optimeDurable,
+             nullOpTime,
+             () => tojson(secondaryOpTimesAsSeenByPrimary));
+assert.eq(nullWallTime,
+          secondaryOpTimesAsSeenByPrimary.optimeDate,
+          () => tojson(secondaryOpTimesAsSeenByPrimary));
+assert.eq(nullWallTime,
+          secondaryOpTimesAsSeenByPrimary.optimeDurableDate,
+          () => tojson(secondaryOpTimesAsSeenByPrimary));
+
+jsTestLog("Waiting for FCBIS node to reach steady state");
+
+// Turn off the failpoint and wait for the node to finish initial sync.
+assert.commandWorked(
+    secondary.adminCommand({configureFailPoint: "fCBISHangBeforeFinish", mode: "off"}));
+rst.awaitSecondaryNodes(null, [secondary]);
+
+jsTestLog("Doing new write that should be accepted");
+
+// The set should now be able to satisfy {w:2} writes.
+assert.commandWorked(
+    primaryDb.runCommand({insert: "test", documents: [{"will": "succeed"}], writeConcern: {w: 2}}));
+
+jsTestLog("Done with test");
+rst.restart(1);
+rst.stopSet();
+})();
---
+++ src/mongo/db/modules/enterprise/jstests/fcbis/fcbis_nodes_contribute_to_liveness_majorities.js
@@ -0,0 +1,125 @@
+/*
+ * Tests that nodes in FCBIS are considered a part of the liveness majority. This test starts with a
+ * two-node replica set. We then add a secondary and force it to hang in FCBIS. We proceed to remove
+ * all communication for the other secondary. We verify that the primary does not step down and no
+ * elections were called, since the node in FCBIS contributes to the liveness majority. Finally, we
+ * verify that nodes in FCBIS are able to vote in an election. This is done by stepping down the
+ * primary, waiting for it to become a secondary, and allowing it to run for primary again. Since
+ * the other secondary is disconnected, the primary must receive a vote from the FCBIS node to get
+ * elected again.
+ *
+ * @tags: [requires_persistence, requires_wiredtiger]
+ */
+
+(function() {
+"use strict";
+
+load("jstests/libs/fail_point_util.js");
+load("jstests/replsets/rslib.js");
+
+const name = jsTestName();
+const rst = new ReplSetTest({
+    name,
+    nodes: [{}, {rsConfig: {priority: 0}}],
+    settings: {electionTimeoutMillis: 3000, heartbeatIntervalMillis: 250},
+    useBridge: true
+});
+rst.startSet();
+rst.initiate();
+
+const primary = rst.getPrimary();
+const primaryDb = primary.getDB("test");
+
+// Add some data to be cloned.
+assert.commandWorked(primaryDb.test.insert([{a: 1}, {b: 2}, {c: 3}]));
+
+jsTestLog("Ensuring there is an up-to-date stable checkpoint for FCBIS to copy from.");
+rst.awaitLastStableRecoveryTimestamp();
+assert.commandWorked(primary.adminCommand({fsync: 1}));
+
+const secondary = rst.getSecondaries()[0];
+
+const initialSyncSecondary = rst.add({
+    rsConfig: {priority: 0, votes: 0},
+    setParameter: {
+        'initialSyncMethod': 'fileCopyBased',
+        'failpoint.fCBISHangBeforeFinish': tojson({mode: 'alwaysOn'}),
+    },
+});
+
+rst.reInitiate();
+
+// Add the new node with votes:0 and then give it votes:1 to avoid 'newlyAdded' and mimic a resync,
+// where a node is in initial sync with 1 vote.
+let nextConfig = rst.getReplSetConfigFromNode(0);
+nextConfig.members[2].votes = 1;
+reconfig(rst, nextConfig, false /* force */, true /* wait */);
+
+jsTestLog("Waiting for FCBIS node to get to end of initial sync");
+
+assert.soonNoExcept(() => {
+    assert.commandWorked(initialSyncSecondary.adminCommand({
+        waitForFailPoint: "fCBISHangBeforeFinish",
+        timesEntered: 1,
+        maxTimeMS: kDefaultWaitForFailPointTimeout
+    }));
+    return true;
+});
+
+secondary.disconnect(primary);
+
+// Verify that the primary should not step down due to not seeing a quorum. This is because the
+// primary should be receiving heartbeats from the initial sync node. We specifically look for the
+// 'Can't see a majority of the set, relinquishing primary' log message.
+assert.throws(() => checkLog.containsJson(primary, 21809, {} /* attrs */, 3000));
+
+assert.eq(primary, rst.getPrimary());
+
+// Verify that the term is still 1, so the primary did not call any elections.
+let primaryReplSetStatus = assert.commandWorked(primary.adminCommand({replSetGetStatus: 1}));
+assert.eq(1, primaryReplSetStatus.term);
+
+jsTestLog("Stepping down primary");
+
+// Step down the primary and prevent it from running for elections with a high freeze timeout.
+assert.commandWorked(
+    primary.adminCommand({replSetStepDown: ReplSetTest.kForeverSecs, force: true}));
+waitForState(primary, ReplSetTest.State.SECONDARY);
+
+// Unfreeze the primary so that it can run for election. It should immediately run for election as
+// no other nodes are able to call for an election.
+assert.commandWorked(primary.adminCommand({replSetFreeze: 0}));
+
+jsTestLog("Re-electing same primary");
+
+// Verify that the primary was elected.
+assert.eq(primary, rst.getPrimary());
+
+// Verify that the term has incremented due to the last election.
+primaryReplSetStatus = assert.commandWorked(primary.adminCommand({replSetGetStatus: 1}));
+assert.gt(primaryReplSetStatus.term, 1);
+
+// Verify that initial sync node voted in the election and sets the correct term in its
+// electionParticipantMetrics field.
+const initialSyncSecondaryReplSetStatus =
+    assert.commandWorked(initialSyncSecondary.adminCommand({replSetGetStatus: 1}));
+assert.eq(2, initialSyncSecondaryReplSetStatus.electionParticipantMetrics.electionTerm);
+
+// The disconnected secondary did not vote in the last election, so its electionParticipantMetrics
+// field should not be set.
+const disconnectedSecondaryReplSetStatus =
+    assert.commandWorked(secondary.adminCommand({replSetGetStatus: 1}));
+assert.eq(undefined, disconnectedSecondaryReplSetStatus.electionParticipantMetrics);
+
+// Since the primary sends a shut down command to all secondaries in `rst.stopSet()`, we reconnect
+// the disconnected secondary to the primary to allow it to be shut down.
+secondary.reconnect(primary);
+
+jsTestLog("Waiting for the FCBIS node to become a secondary");
+
+assert.commandWorked(
+    initialSyncSecondary.adminCommand({configureFailPoint: "fCBISHangBeforeFinish", mode: "off"}));
+waitForState(initialSyncSecondary, ReplSetTest.State.SECONDARY);
+
+rst.stopSet();
+})();
