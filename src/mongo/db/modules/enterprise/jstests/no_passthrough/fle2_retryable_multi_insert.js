babacd5c08abce6227986b7377439f69b0cfe5cd
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2022-11-09 16:29:12 -0500

    SERVER-66315 Support batched inserts with FLE 2
    GitOrigin-RevId: 93542b0e64d840d3d73361b029e201d2d3b6cd72

---
+++ src/mongo/db/modules/enterprise/jstests/no_passthrough/fle2_retryable_multi_insert.js
@@ -0,0 +1,120 @@
+/**
+ * Test encrypted retryable works
+ *
+ * @tags: [
+ * assumes_unsharded_collection,
+ * requires_non_retryable_commands,
+ * assumes_read_preference_unchanged,
+ * requires_capped,
+ * assumes_unsharded_collection,
+ * exclude_from_large_txns,
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+function countOplogEntries(primaryConn) {
+    var oplog = primaryConn.getDB('local').oplog.rs;
+
+    return oplog.find(({"ns": "admin.$cmd", "o.applyOps.ns": "multi_retry.basic", "op": 'c'}))
+        .itcount();
+}
+
+// primaryConn = connection to primary of shard in mongos otherwise primaryConn = conn
+function runTest(conn, primaryConn) {
+    let dbName = 'multi_retry';
+    let collName = 'basic';
+
+    let client = new EncryptedClient(conn, dbName);
+
+    assert.commandWorked(client.createEncryptionCollection(collName, {
+        encryptedFields: {
+            "fields": [
+                {"path": "first", "bsonType": "string", "queries": {"queryType": "equality"}},
+                {"path": "middle", "bsonType": "string"},
+                {"path": "aka", "bsonType": "string", "queries": {"queryType": "equality"}},
+            ]
+        }
+    }));
+
+    let edb = client.getDB();
+
+    const lsid = UUID();
+
+    // Test retryable writes for insert
+    //
+    let result = assert.commandWorked(edb.runCommand({
+        "insert": collName,
+        documents: [
+            {
+                "_id": 1,
+                "first": "dwayne",
+                "middle": "elizondo mountain dew herbert",
+                "aka": "president camacho"
+            },
+            {"_id": 2, "first": "bob", "middle": "belcher", "aka": "bobs burgers"},
+            {"_id": 3, "first": "linda", "middle": "belcher", "aka": "linda"}
+        ],
+        ordered: false,
+        lsid: {id: lsid},
+        txnNumber: NumberLong(31)
+    }));
+
+    print("Here is the initial result");
+    print(tojson(result));
+
+    client.assertEncryptedCollectionCounts(collName, 3, 6, 0, 6);
+
+    let oplogCount = countOplogEntries(primaryConn);
+    assert.eq(oplogCount, 3);
+
+    let retryResult = assert.commandWorked(edb.runCommand({
+        "insert": collName,
+        documents: [
+            {
+                "_id": 1,
+                "first": "dwayne",
+                "middle": "elizondo mountain dew herbert",
+                "aka": "president camacho"
+            },
+            {"_id": 2, "first": "bob", "middle": "belcher", "aka": "bobs burgers"},
+            {"_id": 3, "first": "linda", "middle": "belcher", "aka": "linda"}
+        ],
+        ordered: false,
+        lsid: {id: lsid},
+        txnNumber: NumberLong(31)
+    }));
+
+    print(tojson(retryResult));
+    assert.eq(result.ok, retryResult.ok);
+    assert.eq(result.n, retryResult.n);
+    assert.eq(result.writeErrors, retryResult.writeErrors);
+    assert.eq(result.writeConcernErrors, retryResult.writeConcernErrors);
+    // Assert we did not write a second time to the oplog
+    assert.eq(oplogCount, countOplogEntries(primaryConn));
+
+    client.assertEncryptedCollectionCounts(collName, 3, 6, 0, 6);
+}
+
+jsTestLog("ReplicaSet: Testing fle2 retryable writes");
+{
+    const rst = new ReplSetTest({nodes: 1});
+    rst.startSet();
+
+    rst.initiate();
+    rst.awaitReplication();
+    runTest(rst.getPrimary(), rst.getPrimary());
+    rst.stopSet();
+}
+
+jsTestLog("Sharding: Testing fle2 retryable writes");
+{
+    const st = new ShardingTest({shards: 1, mongos: 1, config: 1});
+
+    runTest(st.s, st.shard0);
+
+    st.stop();
+}
+}());
