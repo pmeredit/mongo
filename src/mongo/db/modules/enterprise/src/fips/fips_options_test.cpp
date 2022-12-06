21ce8587c9d9c429e21d82be544b9b858ce1b78d
Daniel Gottlieb <daniel.gottlieb@mongodb.com>
2022-12-06 13:33:39 -0500

    SERVER-71808: Refactor options_parser to expose a `readRawFile` method.
    GitOrigin-RevId: 1548311ce19c33bc47e4564ab77b630e3549c8ff

--- src/mongo/db/modules/enterprise/src/fips/fips_options_test.cpp
+++ src/mongo/db/modules/enterprise/src/fips/fips_options_test.cpp
@@ -65,7 +65,7 @@ Status storeFIPSServerOptions() {
 
 class OptionsParserTester : public moe::OptionsParser {
 public:
-    Status readConfigFile(const std::string& filename, std::string* config, ConfigExpand) {
+    Status readConfigFile(const std::string& filename, std::string* config, moe::ConfigExpand) {
         if (filename != _filename) {
             ::mongo::StringBuilder sb;
             sb << "Parser using filename: " << filename
