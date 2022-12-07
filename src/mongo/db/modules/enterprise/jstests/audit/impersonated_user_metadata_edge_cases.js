89d9a8e5798c50f3103d8ac9b7edcdd8c5706cab
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2022-12-07 14:56:27 -0500

    SERVER-64029 Prohibit impersonating multiple users
    GitOrigin-RevId: 292bb1cae09c86ec0d15fae50683cb2eada3d37f

--- src/mongo/db/modules/enterprise/jstests/audit/impersonated_user_metadata_edge_cases.js
+++ src/mongo/db/modules/enterprise/jstests/audit/impersonated_user_metadata_edge_cases.js
@@ -6,6 +6,22 @@
 const kInvalidValueExpectedObject = 10065;
 
 const kUserTestCases = [
+    {test: 'string', pass: false},
+    {test: {}, pass: false},
+    {test: {user: 'foo'}, pass: false},
+    {test: {db: 'bar'}, pass: false},
+    {
+        test: [{user: 'foo', db: 'bar'}],
+        pass: false,
+        code: ErrorCodes.Unauthorized,
+        passOnMongos: false
+    },
+    {test: [{user: 'foo', db: 'bar'}], pass: false, passOnMongos: false},
+    {test: {user: 'foo', db: 'bar'}, pass: false, passOnMongos: true},
+];
+
+// TODO SERVER-72448: Remove
+const kUsersTestCases = [
     {test: [], pass: true},
     {test: ['string'], pass: false},
     {test: [{}], pass: false},
@@ -51,6 +67,34 @@ function runTests(conn, authenticated, isMongos = false) {
 
     kUserTestCases.forEach(function(user) {
         kRoleTestCases.forEach(function(role) {
+            const iumd = {"$impersonatedUser": user.test, "$impersonatedRoles": role.test};
+            const cmd = {hello: 1, "$audit": iumd};
+
+            const pass = (user.pass || (isMongos && user.passOnMongos)) &&
+                (role.pass || (isMongos && role.passOnMongos));
+
+            const expect = pass ? 'pass' : 'fail';
+            jsTest.log("Command should " + expect + ": " + tojson(cmd));
+            if (pass) {
+                assert.commandWorked(admin.runCommand(cmd));
+            } else if (user.code || role.code) {
+                const expectedCodes = [ErrorCodes.BadValue];
+                if (user.code !== undefined) {
+                    expectedCodes.push(user.code);
+                }
+                if (role.code !== undefined) {
+                    expectedCodes.push(role.code);
+                }
+                assert.commandFailedWithCode(admin.runCommand(cmd), expectedCodes);
+            } else {
+                assert.commandFailed(admin.runCommand(cmd));
+            }
+        });
+    });
+
+    // TODO SERVER-72448: Remove
+    kUsersTestCases.forEach(function(user) {
+        kRoleTestCases.forEach(function(role) {
             const iumd = {"$impersonatedUsers": user.test, "$impersonatedRoles": role.test};
             const cmd = {hello: 1, "$audit": iumd};
 
@@ -75,6 +119,23 @@ function runTests(conn, authenticated, isMongos = false) {
             }
         });
     });
+
+    if (authenticated) {
+        admin.logout();
+    }
+
+    // TODO SERVER-72448: Remove
+    if (!isMongos) {
+        const iumd = {
+            "$impersonatedUser": {user: 'foo', db: 'bar'},
+            "$impersonatedUsers": [{user: 'foo', db: 'bar'}],
+            "$impersonatedRoles": [{role: 'foo', db: 'bar'}]
+        };
+        const cmd = {hello: 1, "$audit": iumd};
+        jsTest.log("Command should fail: " + tojson(cmd));
+
+        assert.commandFailedWithCode(admin.runCommand(cmd), ErrorCodes.Unauthorized);
+    }
 
     if (authenticated) {
         admin.logout();
