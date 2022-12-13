e610a9fdd3591220137b5f3f9a65d74d6a372802
Benety Goh <benety@mongodb.com>
2022-12-13 09:34:40 -0500

    SERVER-72006 add OpObserver::onTransactionStart()
    closes #800
    
    GitOrigin-RevId: 56a2cf267be6e1fa65a82b302e3c02b60f2559ba

--- src/mongo/db/modules/enterprise/src/audit/audit_op_observer.h
+++ src/mongo/db/modules/enterprise/src/audit/audit_op_observer.h
@@ -179,6 +179,8 @@ public:
                        const NamespaceString& collectionName,
                        const UUID& uuid) final {}
 
+    void onTransactionStart(OperationContext* opCtx) final {}
+
     void onUnpreparedTransactionCommit(OperationContext* opCtx,
                                        const TransactionOperations& transactionOperations) final {}
 
