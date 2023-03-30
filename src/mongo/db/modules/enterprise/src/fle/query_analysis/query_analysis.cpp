fe308fc333970df06301c3d43dd4ecdfaf99aff1
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2023-03-30 15:48:13 -0400

    SERVER-73303 Remove Queryable Encryption v1 Code
    GitOrigin-RevId: 059a02602b1718a78b16cc4778a3db4712f34488

--- src/mongo/db/modules/enterprise/src/fle/query_analysis/query_analysis.cpp
+++ src/mongo/db/modules/enterprise/src/fle/query_analysis/query_analysis.cpp
@@ -722,13 +722,6 @@ PlaceHolderResult addPlaceHoldersForDelete(OperationContext* opCtx,
         write_ops::DeleteCommandRequest::parse(IDLParserContext("delete"), request);
     std::vector<write_ops::DeleteOpEntry> markedDeletes;
     for (auto&& op : deleteRequest.getDeletes()) {
-        // TODO: SERVER-73303 remove this assertion when v2 is enabled by default
-        uassert(6382800,
-                "Multi-document deletes are not allowed with Queryable Encryption",
-                !(op.getMulti() && schemaTree->parsedFrom == FleVersion::kFle2) ||
-                    gFeatureFlagFLE2ProtocolVersion2.isEnabled(
-                        serverGlobalParams.featureCompatibility));
-
         markedDeletes.push_back(op);
         auto& opToMark = markedDeletes.back();
         auto collator = parseCollator(opCtx, op.getCollation());
