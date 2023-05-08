af3da059e46abf8b51fa0b293bc50e2839fa84ca
Militsa Sotirova <militsa.sotirova@mongodb.com>
2023-05-08 13:30:24 +0000

    SERVER-74454 Add tests for limit pushdown of search within lookup pipeline
    GitOrigin-RevId: 8d35d5ad6235c56677d915dc99dfc1865551caa9

--- src/mongo/db/modules/enterprise/jstests/search/search_batchsize.js
+++ src/mongo/db/modules/enterprise/jstests/search/search_batchsize.js
@@ -9,7 +9,7 @@ load('jstests/libs/uuid_util.js');  // For getUUIDFromListCollections.
 load("src/mongo/db/modules/enterprise/jstests/search/lib/shardingtest_with_mongotmock.js");
 
 const dbName = "test";
-const collName = "sharded_batchsize";
+const collName = "search_batchsize";
 const chunkBoundary = 8;
 
 const docs = [
@@ -25,11 +25,21 @@ const docs = [
     {"_id": 10, "title": "cakes and strawberries"},
     {"_id": 11, "title": "cakes and raspberries"},
     {"_id": 12, "title": "cakes and cakes"},
-    {"_id": 13, "title": "cakes and spinach"},
+    {"_id": 13, "title": "cakes and elderberries"},
     {"_id": 14, "title": "cakes and carrots"},
     {"_id": 15, "title": "cakes and more cakes"},
     {"_id": 16, "title": "cakes and even more cakes"},
 ];
+
+const foreignCollectionDocs = [
+    {"_id": 1, "fruit": "raspberries"},
+    {"_id": 2, "fruit": "blueberries"},
+    {"_id": 3, "fruit": "strawberries"},
+    {"_id": 4, "fruit": "gooseberries"},
+    {"_id": 5, "fruit": "mangos"},
+];
+const foreignCollName = "fruits";
+const foreignChunkBoundary = 3;
 
 const searchQuery = {
     query: "cakes",
@@ -361,7 +371,188 @@ function searchMetaAfterLimit(coll, collUUID, stWithMock) {
     assert.eq(expected, cursor.toArray());
 }
 
-function runTest(coll, collUUID, standaloneConn, stConn) {
+function buildHistorySearchWithinLookupStandalone(db, mongotConn, searchLookupQuery, numBerries) {
+    let foreignColl = db.getCollection(foreignCollName);
+    assert.commandWorked(foreignColl.insertMany(foreignCollectionDocs));
+    let foreignCollUUID = getUUIDFromListCollections(db, foreignCollName);
+
+    {
+        const history = [
+            {
+                expectedCommand: {
+                    search: foreignCollName,
+                    collectionUUID: foreignCollUUID,
+                    query: searchLookupQuery,
+                    $db: dbName,
+                    cursorOptions: {docsRequested: numBerries},
+                },
+                response: {
+                    cursor: {
+                        id: NumberLong(0),
+                        ns: foreignColl.getFullName(),
+                        nextBatch: [
+                            {"_id": 1, "$searchScore": 0.300},
+                            {"_id": 2, "$searchScore": 0.299},
+                            {"_id": 3, "$searchScore": 0.298},
+                            {"_id": 4, "$searchScore": 0.297},
+                            // We set mongotmock to return 4 documents here because of the
+                            // oversubscription that mongot would do (mongot would want to return 10
+                            // documents in this case because 3 * 1.064 < 10, but there are only 4
+                            // that satisfy the query so we return those 4).
+                        ],
+                    },
+                    ok: 1
+                }
+            },
+        ];
+        // We set this history once for each document in the local collection, as that is how many
+        // times the $lookup subpipeline that contains a $search stage will be executed. We need a
+        // new cursorId each time because calling setMockResponses with the same cursorId twice
+        // overwrites the first mock response.
+        for (let cursorId = 123; cursorId < 123 + docs.length; cursorId++) {
+            assert.commandWorked(mongotConn.adminCommand(
+                {setMockResponses: 1, cursorId: NumberLong(cursorId), history: history}));
+        }
+    }
+}
+
+function buildHistorySearchWithinLookupShardedEnv(db, stWithMock, searchLookupQuery, numBerries) {
+    let st = stWithMock.st;
+
+    let foreignColl = db.getCollection(foreignCollName);
+    assert.commandWorked(foreignColl.insertMany(foreignCollectionDocs));
+    let foreignCollUUID =
+        getUUIDFromListCollections(st.rs0.getPrimary().getDB(dbName), foreignCollName);
+
+    // Shard the foreign collection for the $lookup test and move the higher chunk to shard1.
+    st.shardColl(
+        foreignColl, {_id: 1}, {_id: foreignChunkBoundary}, {_id: foreignChunkBoundary + 1});
+
+    const planShardedSearchHistory = [{
+        expectedCommand: {
+            planShardedSearch: foreignCollName,
+            query: searchLookupQuery,
+            $db: dbName,
+            searchFeatures: {shardedSort: 1}
+        },
+        response: {ok: 1, protocolVersion: NumberInt(1), metaPipeline: []}
+    }];
+
+    function history(batch) {
+        return [
+            {
+                expectedCommand: {
+                    search: foreignCollName,
+                    collectionUUID: foreignCollUUID,
+                    query: searchLookupQuery,
+                    $db: dbName,
+                    cursorOptions: {docsRequested: numBerries},
+                },
+                response: {
+                    cursor: {
+                        id: NumberLong(0),
+                        ns: foreignColl.getFullName(),
+                        nextBatch: batch,
+                    },
+                    ok: 1
+                }
+            },
+        ];
+    }
+
+    const s0Mongot = stWithMock.getMockConnectedToHost(stWithMock.st.rs0.getPrimary());
+    const s1Mongot = stWithMock.getMockConnectedToHost(stWithMock.st.rs1.getPrimary());
+
+    // We need a new cursorId for each setMockResponses below because calling setMockResponses with
+    // the same cursorId twice overwrites the first mock response.
+    let cursorId = 123;
+
+    // These responses are necessary to reflect that each shard will invoke PSS during pipeline
+    // parsing.
+    s0Mongot.setMockResponses(planShardedSearchHistory, NumberLong(cursorId++));
+    s1Mongot.setMockResponses(planShardedSearchHistory, NumberLong(cursorId++));
+
+    // We set this history once for each document in the local collection as that is how many times
+    // the $lookup subpipeline that contains a $search stage will be executed.
+    for (let i = 0; i < docs.length; i++) {
+        // These responses are necessary to reflect that each shard will invoke PSS during the
+        // execution of the $lookup stage.
+        s0Mongot.setMockResponses(planShardedSearchHistory, NumberLong(cursorId++));
+        s1Mongot.setMockResponses(planShardedSearchHistory, NumberLong(cursorId++));
+
+        // Each search response is mocked twice because each shard will execute the subpipeline
+        // which requires it to get search results for itself and the other shard.
+        s0Mongot.setMockResponses(
+            history([{_id: 1, $searchScore: 0.3}, {_id: 2, $searchScore: 0.299}]),
+            NumberLong(cursorId++));
+        s0Mongot.setMockResponses(
+            history([{_id: 1, $searchScore: 0.3}, {_id: 2, $searchScore: 0.299}]),
+            NumberLong(cursorId++));
+        s1Mongot.setMockResponses(
+            history([{_id: 3, $searchScore: 0.298}, {_id: 4, $searchScore: 0.297}]),
+            NumberLong(cursorId++));
+        s1Mongot.setMockResponses(
+            history([{_id: 3, $searchScore: 0.298}, {_id: 4, $searchScore: 0.297}]),
+            NumberLong(cursorId++));
+    }
+
+    mockPlanShardedSearchResponse(
+        foreignCollName, searchLookupQuery, dbName, undefined /*sortSpec*/, stWithMock);
+
+    stWithMock.getMockConnectedToHost(stWithMock.st.rs0.getPrimary()).disableOrderCheck();
+    stWithMock.getMockConnectedToHost(stWithMock.st.rs1.getPrimary()).disableOrderCheck();
+}
+
+function testSearchWithinLookup(db, coll, mongotConn, stWithMock) {
+    // The $lookup subpipeline produces one document of the form {three_berries: {berries: [...]}}
+    // where the contents of the array are the first 3 fruits (3 from the limit stage) in the
+    // foreign collection documents that contain the substring "berries".
+    // Each document in the local collection has this document produced from the subpipeline after
+    // the $lookup stage. The $unwind stages unwind the array in that document. The $match stage
+    // filters out documents where the title field does not contain any of the first 3 berries as a
+    // substring. Thus, we are left with documents in the local collection where the titles contain
+    // one of the first 3 berries from the foreign collection documents.
+    const searchLookupQuery = {query: "berries", path: "fruit"};
+    const numBerries = 3;
+    const expected = [
+        {"title": "cakes and blueberries"},
+        {"title": "cakes and strawberries"},
+        {"title": "cakes and raspberries"},
+    ];
+    const pipeline = [
+        {
+            $lookup: {
+                from: foreignCollName,
+                pipeline: [
+                    {$search: searchLookupQuery},
+                    {$limit: numBerries},
+                    {$group: {_id: null, berries: {$addToSet: "$fruit"}}},
+                    {$project: {_id: 0}}
+                ],
+                as: "three_berries"
+            }
+        },
+        {$unwind: {path: "$three_berries"}},
+        {$unwind: {path: "$three_berries.berries"}},
+        {$match: {$expr: {$ne: [{$indexOfCP: ["$title", "$three_berries.berries"]}, -1]}}},
+        {$project: {_id: 0, title: 1}}
+    ];
+
+    // Exactly one of mongotConn and stWithMock is expected to be null.
+    if (mongotConn != null) {
+        assert(stWithMock == null);
+        buildHistorySearchWithinLookupStandalone(db, mongotConn, searchLookupQuery, numBerries);
+    } else {
+        assert(mongotConn == null);
+        buildHistorySearchWithinLookupShardedEnv(db, stWithMock, searchLookupQuery, numBerries);
+    }
+
+    let cursor = coll.aggregate(pipeline);
+    assert.eq(expected, cursor.toArray());
+}
+
+function runTest(db, collUUID, standaloneConn, stConn) {
+    let coll = db.getCollection(collName);
     function runSearchQueries(limitVal, otherLimitVal, skipVal) {
         // Perform a $search query with a limit stage.
         let pipeline = [{$search: searchQuery}, {$limit: limitVal}];
@@ -439,6 +630,8 @@ function runTest(coll, collUUID, standaloneConn, stConn) {
     if (stConn != null) {
         searchMetaAfterLimit(coll, collUUID, stConn);
     }
+
+    testSearchWithinLookup(db, coll, standaloneConn, stConn);
 }
 
 function setupAndRunTestStandalone() {
@@ -453,7 +646,7 @@ function setupAndRunTestStandalone() {
     assert.commandWorked(coll.insertMany(docs));
 
     let collUUID = getUUIDFromListCollections(db, collName);
-    runTest(coll, collUUID, mongotConn, null /* stConn */);
+    runTest(db, collUUID, mongotConn, null /* stConn */);
 
     MongoRunner.stopMongod(conn);
     mongotmock.stop();
@@ -486,7 +679,7 @@ function setupAndRunTestShardedEnv() {
     st.shardColl(coll, {_id: 1}, {_id: chunkBoundary}, {_id: chunkBoundary + 1});
 
     let collUUID = getUUIDFromListCollections(st.rs0.getPrimary().getDB(dbName), collName);
-    runTest(coll, collUUID, null /* standaloneConn */, stWithMock);
+    runTest(db, collUUID, null /* standaloneConn */, stWithMock);
 
     stWithMock.stop();
 }
