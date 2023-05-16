a04cb242e44d60fbafe1ae46e5e990940c02137c
Sandeep Dhoot <sandeep.dhoot@mongodb.com>
2023-05-16 21:40:24 +0000

    SERVER-77114: Some small fixes in pipeline_runner tool.
    GitOrigin-RevId: 1f795058d72aed4c303f806c0ec104ee5a450dec

--- src/mongo/db/modules/enterprise/src/streams/tools/pipeline_runner_main.cpp
+++ src/mongo/db/modules/enterprise/src/streams/tools/pipeline_runner_main.cpp
@@ -22,6 +22,7 @@
 #include "streams/exec/context.h"
 #include "streams/exec/event_deserializer.h"
 #include "streams/exec/executor.h"
+#include "streams/exec/in_memory_dead_letter_queue.h"
 #include "streams/exec/in_memory_sink_operator.h"
 #include "streams/exec/json_event_deserializer.h"
 #include "streams/exec/kafka_consumer_operator.h"
@@ -43,8 +44,8 @@ std::unique_ptr<KafkaConsumerOperator> createKafkaConsumerOperator(
     const BSONObj& sourceObj, EventDeserializer* deserializer) {
     KafkaConsumerOperator::Options options;
     options.bootstrapServers = sourceObj["bootstrapServers"].String();
-    options.topicName = sourceObj["topicName"].String();
-    int32_t numPartitions = sourceObj["numPartitions"].Int();
+    options.topicName = sourceObj["topic"].String();
+    int32_t numPartitions = sourceObj["partitionCount"].Int();
     for (int32_t partition = 0; partition < numPartitions; ++partition) {
         KafkaConsumerOperator::PartitionOptions partitionOptions;
         partitionOptions.partition = partition;
@@ -56,45 +57,38 @@ std::unique_ptr<KafkaConsumerOperator> createKafkaConsumerOperator(
 
 class PipelineRunner {
 public:
-    PipelineRunner() : _context(getTestContext()) {}
-
     void runPipelineUsingKafkaConsumerOperator(BSONObj pipelineObj);
-
-private:
-    std::unique_ptr<Context> _context;
 };
 
 void PipelineRunner::runPipelineUsingKafkaConsumerOperator(BSONObj pipelineObj) {
     BSONObj sourceObj = pipelineObj[0].Obj()["$source"].Obj();
     BSONObj restObj = pipelineObj.removeField(std::to_string(0));
     std::vector<BSONObj> pipeline;
+    pipeline.push_back(getTestSourceSpec());
     for (auto elem : restObj) {
         dassert(elem.type() == BSONType::Object);
         pipeline.push_back(elem.Obj());
     }
+    pipeline.push_back(getTestMemorySinkSpec());
 
     const NamespaceString kNss{"pipeline_runner"};
-    QueryTestServiceContext serviceContext;
-    auto opCtx = serviceContext.makeOperationContext();
-    auto expCtx = make_intrusive<ExpressionContextForTest>(opCtx.get(), kNss);
+    QueryTestServiceContext qtServiceContext;
+    auto svcCtx = qtServiceContext.getServiceContext();
+    auto context = getTestContext(svcCtx);
 
     auto deserializer = std::make_unique<JsonEventDeserializer>();
     auto source = createKafkaConsumerOperator(sourceObj, deserializer.get());
-    auto sink = std::make_unique<InMemorySinkOperator>(/*numInputs*/ 1);
-    auto sinkPtr = sink.get();
 
-    Parser parser(_context.get(), {});
+    Parser parser(context.get(), {});
     std::unique_ptr<OperatorDag> dag(parser.fromBson(pipeline));
 
     auto& operators = const_cast<OperatorDag::OperatorContainer&>(dag->operators());
 
-    // Add the source to the operator dag.
-    source->addOutput(operators.front().get(), 0);
-    operators.insert(operators.begin(), std::move(source));
-
-    // Add an in-memory sink to the operator dag.
-    operators.back()->addOutput(sink.get(), 0);
-    operators.push_back(std::move(sink));
+    // Replace InMemorySourceOperator in the operator dag with KafkaConsumerOperator.
+    operators[0] = std::move(source);
+    operators[0]->addOutput(operators[1].get(), 0);
+    auto sink = dynamic_cast<InMemorySinkOperator*>(operators.back().get());
+    invariant(sink);
 
     // Create an Executor and start it.
     Executor::Options executorOptions;
@@ -108,7 +102,7 @@ void PipelineRunner::runPipelineUsingKafkaConsumerOperator(BSONObj pipelineObj) 
     int32_t numConsecutiveEmptyRuns{0};
     while (numConsecutiveEmptyRuns < 100) {  // total 5s of extra runtime
         int32_t numDocsInCurRun{0};
-        std::queue<StreamMsgUnion> msgs = sinkPtr->getMessages();
+        std::queue<StreamMsgUnion> msgs = sink->getMessages();
         while (!msgs.empty()) {
             auto msg = std::move(msgs.front());
             msgs.pop();
@@ -133,6 +127,16 @@ void PipelineRunner::runPipelineUsingKafkaConsumerOperator(BSONObj pipelineObj) 
     std::cout << "\n\n\n(pipeline_runner_main) Received " << numDocs
               << " documents from the pipeline" << std::endl;
     executor->stop();
+
+    auto dlq = dynamic_cast<InMemoryDeadLetterQueue*>(context->dlq.get());
+    auto dlqMsgs = dlq->getMessages();
+    std::cout << "(pipeline_runner_main) Found " << dlqMsgs.size() << " documents in the DLQ"
+              << std::endl;
+    while (!dlqMsgs.empty()) {
+        auto msg = std::move(dlqMsgs.front());
+        dlqMsgs.pop();
+        std::cout << "DLQ: " << msg << std::endl;
+    }
 }
 
 /**
