72e134f246e3d0abf53e8a8e3f1892ab91aad0a7
Will Buerger <will.buerger@mongodb.com>
2023-05-22 19:28:15 +0000

    SERVER-73152: Aggregation shapification for query stats
    GitOrigin-RevId: facabb82f719adeae55f48251d9f27a456bb713c

--- src/mongo/db/modules/enterprise/jstests/no_passthrough/query_stats_disabled_with_fle2.js
+++ src/mongo/db/modules/enterprise/jstests/no_passthrough/query_stats_disabled_with_fle2.js
@@ -1,45 +1,12 @@
 /**
- * Test aggregations on encrypted collections.
+ * Test that query stats are not collected on encrypted collections.
  */
 
 load('jstests/aggregation/extras/utils.js');  // For assertArrayEq.
 load("jstests/fle2/libs/encrypted_client_util.js");
-load("jstests/libs/feature_flag_util.js");
+load("jstests/libs/telemetry_utils.js");  // For getQueryStatsAggCmd and getQueryStatsFindCmd.
 
 (function() {
-
-// TODO SERVER-73152 use telemetry_utils instead of this custom function
-function checkTelemetryOnAggregation(conn, namespace, pipeline) {
-    let telStore = conn.adminCommand({aggregate: 1, pipeline: [{$queryStats: {}}], cursor: {}});
-    for (let i = 0; i < telStore.cursor.firstBatch.length; i++) {
-        const entry = telStore.cursor.firstBatch[i];
-        if (entry.key.namespace == namespace) {
-            if (entry.key.pipeline && documentEq(entry.key.pipeline, pipeline)) {
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function checkTelemetryOnFind(conn, filter, execCount = 1) {
-    let telStore = conn.adminCommand({
-        aggregate: 1,
-        pipeline: [
-            {
-                $queryStats: {},
-
-            },
-            {$match: {"key.queryShape.filter": filter}}
-        ],
-        cursor: {}
-    });
-    const results = telStore.cursor.firstBatch;
-    if (results.length != 1) {
-        return false;
-    }
-    return results[0].metrics.execCount == execCount;
-}
 
 function runTest(conn) {
     const docs = [
@@ -106,58 +73,114 @@ function runTest(conn) {
         assert.commandWorked(encryptedColl.insert(doc));
     }
 
-    const pipeline = [{$match: {_id: 0}}];
-    const redactedPipeline = [{$match: {_id: "###"}}];
+    // Test for aggregation requests that query stats are only collected when encryption is not
+    // enabled.
+    {
+        const pipeline = [{$match: {_id: 0}}];
+        const redactedPipeline = [{$match: {_id: {$eq: "?number"}}}];
 
-    // Assert that telemetry is not collected on an aggregation on an encryption-enabled collection.
-    assert.eq(encryptedColl.aggregate(pipeline).itcount(), 1);
-    assert(!checkTelemetryOnAggregation(testDB, encryptedColl, redactedPipeline));
+        // Assert that telemetry is not collected on an aggregation on an encryption-enabled
+        // collection.
+        assert.eq(encryptedColl.aggregate(pipeline).itcount(), 1);
+        let queryStats = getQueryStatsAggCmd(testDB);
+        assert.eq(0, queryStats.length, queryStats);
 
-    // Assert that telemetry is collected on an aggregation on a regular collection.
-    assert.eq(coll.aggregate(pipeline).itcount(), 1);
-    assert(checkTelemetryOnAggregation(testDB, coll, redactedPipeline));
+        // Assert that telemetry is collected on an aggregation on a regular collection.
+        assert.eq(coll.aggregate(pipeline).itcount(), 1);
+        queryStats = getQueryStatsAggCmd(testDB);
+        assert.eq(1, queryStats.length);
+        assert.eq({"db": `${dbName}`, "coll": `${jsTestName()}`},
+                  queryStats[0].key.queryShape.cmdNs);
+        assert.eq(redactedPipeline, queryStats[0].key.queryShape.pipeline);
 
-    const encryptedPipeline = [{$match: {ssn: "456"}}];
-    const redactedEncryptedPipeline = [{$match: {ssn: "###"}}];
+        const encryptedPipeline = [{$match: {ssn: "456"}}];
+        const redactedEncryptedPipeline = [{$match: {ssn: {$eq: "?string"}}}];
 
-    // Assert that telemetry is not collected on an aggregation that queries an encrypted field.
-    assert.eq(encryptedColl.aggregate(encryptedPipeline).itcount(), 1);
-    assert(!checkTelemetryOnAggregation(testDB, encryptedColl, redactedEncryptedPipeline));
+        // Assert that telemetry is not collected on an aggregation that queries an encrypted field.
+        assert.eq(encryptedColl.aggregate(encryptedPipeline).itcount(), 1);
+        queryStats = getQueryStatsAggCmd(testDB);
+        assert.eq(1,
+                  queryStats.length);  // We still have the 1 previous query with stats collected.
 
-    // Assert that telemetry is collected when querying the same field in a regular collection.
-    assert.eq(coll.aggregate(encryptedPipeline).itcount(), 1);
-    assert(checkTelemetryOnAggregation(testDB, coll, redactedEncryptedPipeline));
+        // Assert that telemetry is collected when querying the same field in a regular collection.
+        assert.eq(coll.aggregate(encryptedPipeline).itcount(), 1);
+        queryStats = getQueryStatsAggCmd(testDB);
+        assert.eq(2, queryStats.length);
+        assert.eq({"db": `${dbName}`, "coll": `${jsTestName()}`},
+                  queryStats[1].key.queryShape.cmdNs);
+        assert.eq(redactedEncryptedPipeline, queryStats[1].key.queryShape.pipeline);
 
-    // Assert that telemetry is collected on a collection-less aggregation. $queryStats is an
-    // aggregation without a targeted collection, so the results of the $queryStats command should
-    // be visible in the telemetry store.
-    testDB.adminCommand({aggregate: 1, pipeline: [{$queryStats: {}}], cursor: {}});
-    assert(checkTelemetryOnAggregation(testDB, "admin.$cmd.aggregate", [{$queryStats: {}}]));
+        // Assert that telemetry was collected on the previous calls to $queryStats, representative
+        // of a collection-less aggregation. $queryStats is an aggregation without a targeted
+        // collection, so the results of the $queryStats command should be visible in the telemetry
+        // store.
+        const results = testDB.adminCommand({
+            aggregate: 1,
+            pipeline:
+                [{$queryStats: {}}, {$match: {"key.queryShape.cmdNs.coll": "$cmd.aggregate"}}],
+            cursor: {}
+        });
+        assert.eq(1, results.cursor.firstBatch.length);
+        queryStats = results.cursor.firstBatch;
+        assert.eq({"db": "admin", "coll": "$cmd.aggregate"}, queryStats[0].key.queryShape.cmdNs);
+        // This is the pipeline used by getQueryStatsAggCmd.
+        assert.eq(
+            [
+                {$queryStats: {}},
+                {
+                    $match: {
+                        $and: [
+                            {"key.queryShape.command": {"$eq": "?string"}},
+                            {"key.queryShape.pipeline.0.$queryStats": {$not: {$exists: "?bool"}}},
+                            {"key.applicationName": {"$eq": "?string"}}
+                        ]
+                    }
+                },
+                {$sort: {key: 1}}
+            ],
+            queryStats[0].key.queryShape.pipeline);
+    }
 
-    // TODO SERVER-73152 use telemetry_utils to filter by applicationName instead
-    // Since many internal commands use "find", we have to use a unique field name to ensure that
-    // name is not found in the telemetry store.
-    const findCmd = {"uniqueFieldName": "A"};
-    const redactedFindCmd = {"uniqueFieldName": {$eq: "?string"}};
+    // Test for find requests that query stats are only collected when encryption is not
+    // enabled.
+    {
+        const findCmd = {"manager": "D"};
+        const redactedFindCmd = {"manager": {$eq: "?string"}};
 
-    // Assert that telemetry is not collected on a find command on an encryption-enabled collection.
-    assert.eq(encryptedColl.find(findCmd).itcount(), 1);
-    assert(!checkTelemetryOnFind(testDB, redactedFindCmd));
+        // Assert that telemetry is not collected on a find command on an encryption-enabled
+        // collection.
+        assert.eq(encryptedColl.find(findCmd).itcount(), 1);
+        queryStats = getQueryStatsFindCmd(testDB, false, jsTestName());
+        assert.eq(0, queryStats.length, queryStats);
+        // assert.eq({"db": "admin", "coll": "system.keys"}, queryStats[0].key.queryShape.cmdNs);
+        // assert.eq({"db": "test", "coll": "keystore"}, queryStats[1].key.queryShape.cmdNs);
 
-    // Assert that telemetry is collected on a find command on a regular collection.
-    assert.eq(coll.find(findCmd).itcount(), 1);
-    assert(checkTelemetryOnFind(testDB, redactedFindCmd));
+        // Assert that telemetry is collected on a find command on a regular collection.
+        assert.eq(coll.find(findCmd).itcount(), 1);
+        queryStats = getQueryStatsFindCmd(testDB, false, jsTestName());
+        assert.eq(1, queryStats.length, queryStats);
+        assert.eq({"db": `${dbName}`, "coll": `${jsTestName()}`},
+                  queryStats[0].key.queryShape.cmdNs);
+        assert.eq(redactedFindCmd, queryStats[0].key.queryShape.filter);
 
-    const encryptedFindCmd = {"ssn": "456"};
-    const redactedEncryptedFindCmd = {"ssn": {$eq: "?string"}};
+        const encryptedFindCmd = {"ssn": "456"};
+        const redactedEncryptedFindCmd = {"ssn": {$eq: "?string"}};
 
-    // Assert that telemetry is not collected on a find command on an encryption-enabled collection.
-    assert.eq(encryptedColl.find(encryptedFindCmd).itcount(), 1);
-    assert(!checkTelemetryOnFind(testDB, redactedEncryptedFindCmd));
+        // Assert that telemetry is not collected on a find command on an encryption-enabled
+        // collection.
+        assert.eq(encryptedColl.find(encryptedFindCmd).itcount(), 1);
+        queryStats = getQueryStatsFindCmd(testDB, false, jsTestName());
+        // We still have the 1 previous query with stats collected.
+        assert.eq(1, queryStats.length, queryStats);
 
-    // Assert that telemetry is collected on a find command on a regular collection.
-    assert.eq(coll.find(encryptedFindCmd).itcount(), 1);
-    assert(checkTelemetryOnFind(testDB, redactedEncryptedFindCmd));
+        // Assert that telemetry is collected on a find command on a regular collection.
+        assert.eq(coll.find(encryptedFindCmd).itcount(), 1);
+        queryStats = getQueryStatsFindCmd(testDB, false, jsTestName());
+        assert.eq(2, queryStats.length, queryStats);
+        assert.eq({"db": `${dbName}`, "coll": `${jsTestName()}`},
+                  queryStats[1].key.queryShape.cmdNs);
+        assert.eq(redactedEncryptedFindCmd, queryStats[1].key.queryShape.filter);
+    }
 }
 
 const rst = new ReplSetTest({
@@ -173,19 +196,20 @@ const rstConn = rst.getPrimary();
 runTest(rstConn);
 rst.stopSet();
 
-const st = new ShardingTest({
-    mongos: 1,
-    shards: 1,
-    config: 1,
-    rs: {nodes: 1},
-    mongosOptions: {
-        setParameter: {
-            internalQueryStatsSamplingRate: -1,
-            featureFlagQueryStats: true,
-            'failpoint.skipClusterParameterRefresh': "{'mode':'alwaysOn'}"
-        }
-    }
-});
-runTest(st.s);
-st.stop();
+// TODO SERVER-77325 reenable these tests
+// const st = new ShardingTest({
+//     mongos: 1,
+//     shards: 1,
+//     config: 1,
+//     rs: {nodes: 1},
+//     mongosOptions: {
+//         setParameter: {
+//             internalQueryStatsSamplingRate: -1,
+//             featureFlagQueryStats: true,
+//             'failpoint.skipClusterParameterRefresh': "{'mode':'alwaysOn'}"
+//         }
+//     }
+// });
+// runTest(st.s);
+// st.stop();
 }());
