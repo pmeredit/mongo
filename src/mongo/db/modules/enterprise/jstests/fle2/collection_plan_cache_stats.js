7bf2678f50112c4872991bfaecabd084296d9cf7
Adrian Gonzalez <adriangonzalezmontemayor@gmail.com>
2023-06-26 18:35:44 +0000

    SERVER-78069 Fix race condition in collection_plan_cache_stats.js
    GitOrigin-RevId: 16a06bdb1237cca06fe7029897b287a53e8f505c

--- src/mongo/db/modules/enterprise/jstests/fle2/collection_plan_cache_stats.js
+++ src/mongo/db/modules/enterprise/jstests/fle2/collection_plan_cache_stats.js
@@ -32,6 +32,10 @@ assert.commandWorked(client.createEncryptionCollection("basic", {
         {"fields": [{"path": "first", "bsonType": "string", "queries": {"queryType": "equality"}}]}
 }));
 
+// Clear plan cache and wait to prevent race conditions.
+assert.commandWorked(dbTest.runCommand({planCacheClear: "basic"}));
+sleep(1500);
+
 assert.commandWorked(edb.basic.createIndex({a: 1}));
 assert.commandWorked(edb.basic.createIndex({b: 1}));
 
@@ -41,13 +45,20 @@ assert.commandWorked(edb.basic.insert({_id: 0, "first": "mark", a: 1, b: 1}));
 assert.commandWorked(edb.basic.insert({_id: 1, "first": "mark", a: 1, b: 1, c: 1}));
 assert.commandWorked(edb.basic.insert({_id: 2, "first": "mark", a: 1, b: 1, c: 1, d: 1}));
 
+// Clear plan cache and wait to prevent race conditions.
+assert.commandWorked(dbTest.runCommand({planCacheClear: "basic"}));
+sleep(1500);
+
 // Run three distinct query shapes and check that there are 3 cache entries since this is not run in
 // an encrypted client.
 assert.eq(3, dbTest.basic.find({a: 1, b: 1}).itcount());
 assert.eq(2, dbTest.basic.find({a: 1, b: 1, c: 1}).itcount());
 assert.eq(1, dbTest.basic.find({a: 1, b: 1, d: 1}).itcount());
 assert.eq(3, dbTest.basic.aggregate([{$planCacheStats: {}}]).itcount());
+
+// Clear plan cache and wait to prevent race conditions.
 assert.commandWorked(dbTest.runCommand({planCacheClear: "basic"}));
+sleep(1500);
 
 // Run three distinct query shapes on the encrypted client and verify planCacheStats returns 0. This
 // is because even though the query does not use encryption, the shell appends
@@ -56,7 +67,10 @@ assert.eq(3, edb.basic.find({a: 1, b: 1}).itcount());
 assert.eq(2, edb.basic.find({a: 1, b: 1, c: 1}).itcount());
 assert.eq(1, edb.basic.find({a: 1, b: 1, d: 1}).itcount());
 assert.eq(0, dbTest.basic.aggregate([{$planCacheStats: {}}]).itcount());
+
+// Clear plan cache and wait to prevent race conditions.
 assert.commandWorked(dbTest.runCommand({planCacheClear: "basic"}));
+sleep(1500);
 
 // Run three distinct query shapes using also an encrypted field on the encrypted client and verify
 // planCacheStats returns 0.
