468752ac893444269ca264b35cbf274ac067e2ae
Matthew Normyle <matthew.normyle@mongodb.com>
2023-09-20 00:03:30 +0000

    SERVER-81224: Streams: don't take mutex while visiting metrics
    GitOrigin-RevId: b9d5b2eb9dd745c52938f6a736eb14818e3cef26

--- src/mongo/db/modules/enterprise/src/streams/util/metric_manager.cpp
+++ src/mongo/db/modules/enterprise/src/streams/util/metric_manager.cpp
@@ -13,11 +13,11 @@ std::shared_ptr<Counter> MetricManager::registerCounter(std::string name,
                                                         LabelsVec labels) {
     stdx::lock_guard<Latch> lock(_mutex);
     auto counter = std::make_shared<Counter>();
-    MetricInfo metricInfo;
-    metricInfo.name = std::move(name);
-    metricInfo.description = std::move(description);
-    metricInfo.labels = std::move(labels);
-    metricInfo.metric = counter;
+    auto metricInfo = std::make_shared<MetricInfo>();
+    metricInfo->name = std::move(name);
+    metricInfo->description = std::move(description);
+    metricInfo->labels = std::move(labels);
+    metricInfo->metric = counter;
     _metrics.push_back(std::move(metricInfo));
     return counter;
 }
@@ -27,11 +27,11 @@ std::shared_ptr<Gauge> MetricManager::registerGauge(std::string name,
                                                     LabelsVec labels) {
     stdx::lock_guard<Latch> lock(_mutex);
     auto gauge = std::make_shared<Gauge>();
-    MetricInfo metricInfo;
-    metricInfo.name = std::move(name);
-    metricInfo.description = std::move(description);
-    metricInfo.labels = std::move(labels);
-    metricInfo.metric = gauge;
+    auto metricInfo = std::make_shared<MetricInfo>();
+    metricInfo->name = std::move(name);
+    metricInfo->description = std::move(description);
+    metricInfo->labels = std::move(labels);
+    metricInfo->metric = gauge;
     _metrics.push_back(std::move(metricInfo));
     return gauge;
 }
@@ -42,11 +42,11 @@ std::shared_ptr<CallbackGauge> MetricManager::registerCallbackGauge(std::string 
                                                                     CallbackGauge::CallbackFn fn) {
     stdx::lock_guard<Latch> lock(_mutex);
     auto gauge = std::make_shared<CallbackGauge>(std::move(fn));
-    MetricInfo metricInfo;
-    metricInfo.name = std::move(name);
-    metricInfo.description = std::move(description);
-    metricInfo.labels = std::move(labels);
-    metricInfo.metric = gauge;
+    auto metricInfo = std::make_shared<MetricInfo>();
+    metricInfo->name = std::move(name);
+    metricInfo->description = std::move(description);
+    metricInfo->labels = std::move(labels);
+    metricInfo->metric = gauge;
     _metrics.push_back(std::move(metricInfo));
     return gauge;
 }
--- src/mongo/db/modules/enterprise/src/streams/util/metric_manager.h
+++ src/mongo/db/modules/enterprise/src/streams/util/metric_manager.h
@@ -50,32 +50,45 @@ private:
 
     mutable mongo::Mutex _mutex = MONGO_MAKE_LATCH("MetricManager::mutex");
     // Tracks all registered metrics.
-    std::list<MetricInfo> _metrics;
+    std::list<std::shared_ptr<MetricInfo>> _metrics;
 };
 
 template <typename Visitor>
 void MetricManager::visitAllMetrics(Visitor* visitor) {
-    mongo::stdx::lock_guard<mongo::Latch> lock(_mutex);
-    auto it = _metrics.begin();
-    while (it != _metrics.end()) {
-        auto& metricInfo = *it;
-        auto metric = metricInfo.metric.lock();
+    // Populate a vector of the metrics we want to visit.
+    std::vector<std::shared_ptr<MetricInfo>> metricsToVisit;
+    {
+        mongo::stdx::lock_guard<mongo::Latch> lock(_mutex);
+        metricsToVisit.reserve(_metrics.size());
+        auto it = _metrics.begin();
+        while (it != _metrics.end()) {
+            auto& metricInfo = *it;
+            auto metric = metricInfo->metric.lock();
+            if (metric) {
+                metricsToVisit.push_back(*it);
+                ++it;
+            } else {
+                it = _metrics.erase(it);
+            }
+        }
+    }
+
+    // Note: we release the _mutex before visiting each metric.
+    for (auto& metricInfo : metricsToVisit) {
+        auto metric = metricInfo->metric.lock();
         if (!metric) {
-            it = _metrics.erase(it);
             continue;
         }
-
         if (auto counter = dynamic_cast<Counter*>(metric.get())) {
-            visitor->visit(counter, metricInfo.name, metricInfo.description, metricInfo.labels);
+            visitor->visit(counter, metricInfo->name, metricInfo->description, metricInfo->labels);
         } else if (auto gauge = dynamic_cast<Gauge*>(metric.get())) {
-            visitor->visit(gauge, metricInfo.name, metricInfo.description, metricInfo.labels);
+            visitor->visit(gauge, metricInfo->name, metricInfo->description, metricInfo->labels);
         } else {
             auto callbackGauge = dynamic_cast<CallbackGauge*>(metric.get());
             invariant(callbackGauge);
             visitor->visit(
-                callbackGauge, metricInfo.name, metricInfo.description, metricInfo.labels);
+                callbackGauge, metricInfo->name, metricInfo->description, metricInfo->labels);
         }
-        ++it;
     }
 }
 
