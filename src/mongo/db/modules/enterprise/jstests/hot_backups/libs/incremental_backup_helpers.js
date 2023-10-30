14219d1647ae5acd08f08fab1383076be7e0c2f4
Yu Jin Kang Park <yujin.kang@mongodb.com>
2023-10-30 09:47:33 +0000

    SERVER-81143 Fix jstest child fsm process exit code assert
    GitOrigin-RevId: 3b338ebbe5c325004aedf467d0376b93a4d7b870

--- src/mongo/db/modules/enterprise/jstests/hot_backups/libs/incremental_backup_helpers.js
+++ src/mongo/db/modules/enterprise/jstests/hot_backups/libs/incremental_backup_helpers.js
@@ -244,6 +244,12 @@ export const stopFSMClient = function(fsmPid) {
     if (!_isWindows()) {
         // The mongo shell calls TerminateProcess() on Windows rather than more gracefully
         // interrupting resmoke.py test execution.
-        assert.eq(130, exitCode, 'expected resmoke.py to exit due to being interrupted');
+
+        // resmoke.py may exit cleanly on SIGINT, returning 130 if the suite tests were running and
+        // returning SIGINT otherwise. It may also exit uncleanly, in which case
+        // stopMongoProgramByPid returns -SIGINT. See SERVER-67390 and SERVER-72449.
+        assert(exitCode == 130 || exitCode == -kSIGINT || exitCode == kSIGINT,
+               'expected resmoke.py to exit due to being interrupted, but exited with code: ' +
+                   exitCode);
     }
 };
--- src/mongo/db/modules/enterprise/jstests/live_import/export_import_concurrency.js
+++ src/mongo/db/modules/enterprise/jstests/live_import/export_import_concurrency.js
@@ -149,7 +149,13 @@ const exitCode = stopMongoProgramByPid(fsmPid, kSIGINT);
 if (!_isWindows()) {
     // The mongo shell calls TerminateProcess() on Windows rather than more gracefully
     // interrupting resmoke.py test execution.
-    assert.eq(130, exitCode, 'expected resmoke.py to exit due to being interrupted');
+
+    // resmoke.py may exit cleanly on SIGINT, returning 130 if the suite tests were running and
+    // returning SIGINT otherwise. It may also exit uncleanly, in which case stopMongoProgramByPid
+    // returns -SIGINT. See SERVER-67390 and SERVER-72449.
+    assert(
+        exitCode == 130 || exitCode == -kSIGINT || exitCode == kSIGINT,
+        'expected resmoke.py to exit due to being interrupted, but exited with code: ' + exitCode);
 }
 
 rst.stopSet();
