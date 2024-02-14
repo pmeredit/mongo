b2a05cd44824d9147213df1a83f6b5777d19b3b8
Vishnu Kaushik <vishnu.kaushik@mongodb.com>
2024-02-14 19:15:33 +0000

    SERVER-86139 test that FCBIS preserves recordIds for collections with recordIdsReplicated:true
    GitOrigin-RevId: ad497d35bff2376c6c9332a83c38e02a7f236fdb

---
+++ src/mongo/db/modules/enterprise/jstests/fcbis/preserve_record_ids_fcbis.js
@@ -0,0 +1,12 @@
+/**
+ * Tests that when initial syncing using FCBIS for a collection with recordIdsReplicated:true, the
+ * recordIds are preserved across the initial sync.
+ * @tags: [requires_persistence, requires_wiredtiger, featureFlagRecordIdsReplicated]
+ */
+import {
+    testPreservingRecordIdsDuringInitialSync,
+} from "jstests/libs/replicated_record_ids_utils.js";
+
+testPreservingRecordIdsDuringInitialSync(/*initSyncMethod*/ "fileCopyBased",
+                                         /*beforeCloningFP*/ "fCBISHangAfterOpeningBackupCursor",
+                                         /*afterCloningFP*/ "fCBISHangAfterFileCloning");
