da670631ddf46cead9bd0f82e93a93845227b763
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2024-04-02 15:07:53 -0400

    SERVER-86715 Re-enable config shard last-continuous tests in enterprise jstests/hot_backups/multiVersion (#20628)

--- src/mongo/db/modules/enterprise/jstests/hot_backups/multiVersion/sharded_last_cont_backup_restore_simple_config_shard.js
+++ src/mongo/db/modules/enterprise/jstests/hot_backups/multiVersion/sharded_last_cont_backup_restore_simple_config_shard.js
@@ -11,10 +11,9 @@ import {
     ShardedBackupRestoreTest
 } from "src/mongo/db/modules/enterprise/jstests/hot_backups/libs/sharded_backup_restore.js";
 
-// TODO(SERVER-86715): Re-enable after SERVER-84548 in 7.3.
-// let msg = new ShardedBackupRestoreTest(new NoopWorker(), {configShard: true}).run({
-//     isPitRestore: false,
-//     isSelectiveRestore: false,
-//     backupBinaryVersion: "last-continuous"
-// });
-// assert.eq(msg, "Test succeeded.");
+let msg = new ShardedBackupRestoreTest(new NoopWorker(), {configShard: true}).run({
+    isPitRestore: false,
+    isSelectiveRestore: false,
+    backupBinaryVersion: "last-continuous"
+});
+assert.eq(msg, "Test succeeded.");
