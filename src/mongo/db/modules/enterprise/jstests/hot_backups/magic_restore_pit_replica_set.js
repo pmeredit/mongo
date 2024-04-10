1e603e524deb3e6df5498f865518fbbee48bdd6f
Ali Mir <ali.mir@mongodb.com>
2024-04-10 15:39:56 -0400

    SERVER-88527 Create MagicRestoreUtils utility class for testing magic restore (#20796)

--- src/mongo/db/modules/enterprise/jstests/hot_backups/magic_restore_pit_replica_set.js
+++ src/mongo/db/modules/enterprise/jstests/hot_backups/magic_restore_pit_replica_set.js
@@ -18,12 +18,7 @@
  * ]
  */
 
-import {
-    _copyFileHelper,
-    _runMagicRestoreNode,
-    _writeObjsToMagicRestorePipe,
-    openBackupCursor
-} from "jstests/libs/backup_utils.js";
+import {_copyFileHelper, MagicRestoreUtils} from "jstests/libs/backup_utils.js";
 
 // TODO SERVER-86034: Run on Windows machines once named pipe related failures are resolved.
 if (_isWindows()) {
@@ -36,6 +31,8 @@ if (_isWindows()) {
 TestData.skipEnforceFastCountOnValidate = true;
 
 function runTest(insertHigherTermOplogEntry) {
+    jsTestLog("Running PIT magic restore with insertHigherTermOplogEntry: " +
+              insertHigherTermOplogEntry);
     const sourceCluster = new ReplSetTest({nodes: 1});
     sourceCluster.startSet();
     sourceCluster.initiateWithHighElectionTimeout();
@@ -53,97 +50,59 @@ function runTest(insertHigherTermOplogEntry) {
     const snapshotTs = assert.commandWorked(sourcePrimary.adminCommand({replSetGetStatus: 1}))
                            .optimes.lastCommittedOpTime.ts;
 
-    // Take the initial checkpoint.
-    assert.commandWorked(sourcePrimary.adminCommand({fsync: 1}));
-
-    const backupDbPath = MongoRunner.dataPath + "backup";
-    resetDbpath(backupDbPath);
-    mkdir(backupDbPath + "/journal");
-
-    // Open a backup cursor on the checkpoint.
-    const backupCursor = openBackupCursor(sourcePrimary.getDB("admin"));
-    // Print the backup metadata document.
-    assert(backupCursor.hasNext());
-    const {metadata} = backupCursor.next();
-    jsTestLog("Backup cursor metadata document: " + tojson(metadata));
+    const magicRestoreUtils = new MagicRestoreUtils({
+        backupSource: sourcePrimary,
+        pipeDir: MongoRunner.dataDir,
+        isPit: true,
+        insertHigherTermOplogEntry: insertHigherTermOplogEntry
+    });
+    magicRestoreUtils.takeCheckpointAndOpenBackup(sourcePrimary);
 
     // These documents will be truncated by magic restore, since they were written after the backup
     // cursor was opened. We will pass these oplog entries to magic restore to perform a PIT
     // restore, so they will be reinserted and reflected in the final state of the data.
     ['e', 'f', 'g', 'h'].forEach(
         key => { assert.commandWorked(sourceDb.getCollection(coll).insert({[key]: 1})); });
-
     assert.eq(sourceDb.getCollection(coll).find().toArray().length, 7);
 
-    let oplog = sourcePrimary.getDB("local").getCollection('oplog.rs');
-    const entriesAfterBackup =
-        oplog.find({ts: {$gt: metadata.checkpointTimestamp}}).sort({ts: 1}).toArray();
+    const checkpointTimestamp = magicRestoreUtils.getCheckpointTimestamp();
+    let {lastOplogEntryTs, entriesAfterBackup} =
+        magicRestoreUtils.getEntriesAfterBackup(sourcePrimary);
     assert.eq(entriesAfterBackup.length, 4);
 
-    while (backupCursor.hasNext()) {
-        const doc = backupCursor.next();
-        jsTestLog("Copying for backup: " + tojson(doc));
-        _copyFileHelper(doc.filename, sourcePrimary.dbpath, backupDbPath);
-    }
-    backupCursor.close();
+    magicRestoreUtils.copyFilesAndCloseBackup();
 
     let expectedConfig =
         assert.commandWorked(sourcePrimary.adminCommand({replSetGetConfig: 1})).config;
     // The new node will be allocated a new port by the test fixture.
     expectedConfig.members[0].host = getHostName() + ":" + (Number(sourcePrimary.port) + 2);
-    const restoreToHigherTermThan = 100;
-    let lastOplogEntryTs = entriesAfterBackup[entriesAfterBackup.length - 1].ts;
-    const objs = [{
+    let restoreConfiguration = {
         "nodeType": "replicaSet",
         "replicaSetConfig": expectedConfig,
-        "maxCheckpointTs": metadata.checkpointTimestamp,
+        "maxCheckpointTs": checkpointTimestamp,
         // Restore to the timestamp of the last oplog entry on the source cluster.
         "pointInTimeTimestamp": lastOplogEntryTs
-    }];
-    if (insertHigherTermOplogEntry) {
-        objs[0].restoreToHigherTermThan = NumberLong(restoreToHigherTermThan);
-    }
+    };
+    restoreConfiguration =
+        magicRestoreUtils.appendRestoreToHigherTermThanIfNeeded(restoreConfiguration);
 
-    _writeObjsToMagicRestorePipe([...objs, ...entriesAfterBackup], MongoRunner.dataDir);
-    _runMagicRestoreNode(backupDbPath, MongoRunner.dataDir);
+    magicRestoreUtils.writeObjsAndRunMagicRestore(restoreConfiguration, entriesAfterBackup);
 
     // Start a new replica set fixture on the dbpath.
     const destinationCluster = new ReplSetTest({nodes: 1});
-    destinationCluster.startSet({dbpath: backupDbPath, noCleanData: true});
+    destinationCluster.startSet({dbpath: magicRestoreUtils.getBackupDbPath(), noCleanData: true});
 
     const destPrimary = destinationCluster.getPrimary();
     const restoredConfig =
         assert.commandWorked(destPrimary.adminCommand({replSetGetConfig: 1})).config;
-    // If we passed in a value for the 'restoreToHigherTermThan' field in the restore config, a
-    // no-op oplog entry was inserted in the oplog with that term value + 100. On startup, the
-    // replica set node sets its term to this value. A new election occurred when the replica set
-    // restarted, so we must also increment the term by 1 regardless of if we passed in a higher
-    // term value.
-    const expectedTerm =
-        insertHigherTermOplogEntry ? restoreToHigherTermThan + 101 : expectedConfig.term + 1;
-    expectedConfig.term = expectedTerm;
-    assert.eq(expectedConfig, restoredConfig);
 
-    oplog = destPrimary.getDB("local").getCollection('oplog.rs');
-    if (insertHigherTermOplogEntry) {
-        const incrementTermEntry = oplog.findOne({op: "n", "o.msg": "restore incrementing term"});
-        assert(incrementTermEntry);
-        assert.eq(incrementTermEntry.t, restoreToHigherTermThan + 100);
-        lastOplogEntryTs = incrementTermEntry.ts;
-    }
+    magicRestoreUtils.assertConfigIsCorrect(expectedConfig, restoredConfig);
+    magicRestoreUtils.assertStableCheckpointIsCorrectAfterRestore(destPrimary);
 
-    // Ensure that the last stable checkpoint taken used the timestamp from the top of the oplog. As
-    // the timestamp is greater than 0, this means the magic restore took a stable checkpoint on
-    // shutdown.
-    const {lastStableRecoveryTimestamp} =
-        assert.commandWorked(destPrimary.adminCommand({replSetGetStatus: 1}));
-    assert(timestampCmp(lastStableRecoveryTimestamp, lastOplogEntryTs) == 0);
+    magicRestoreUtils.assertOplogCountForNamespace(
+        sourcePrimary, dbName + "." + coll, 7, "i" /* op filter*/);
 
-    const entries = oplog.find({op: "i", ns: dbName + "." + coll}).sort({ts: -1}).toArray();
-    assert.eq(entries.length, 7);
-
-    const minValid = destPrimary.getCollection('local.replset.minvalid').findOne();
-    assert.eq(minValid, {_id: ObjectId("000000000000000000000000"), t: -1, ts: Timestamp(0, 1)});
+    magicRestoreUtils.assertMinValidIsCorrect(destPrimary);
 
     // The original node still maintains the history store, so point-in-time reads will succeed.
     let res = sourcePrimary.getDB("db").runCommand(
@@ -151,18 +110,7 @@ function runTest(insertHigherTermOplogEntry) {
     assert.commandWorked(res);
     assert.eq(res.cursor.firstBatch.length, 3);
 
-    // A restored node will not preserve any history. The oldest timestamp should be set to the top
-    // of the oplog during recovery oplog application.
-    res = destPrimary.getDB("db").runCommand(
-        {find: "coll", readConcern: {level: "snapshot", atClusterTime: snapshotTs}});
-    assert.commandFailedWithCode(res, ErrorCodes.SnapshotTooOld);
-
-    // A snapshot read at the top of the oplog should succeed, since both the stable and oldest
-    // timestamps are set to this value on magic restore shutdown.
-    res = destPrimary.getDB("db").runCommand(
-        {find: "coll", readConcern: {level: "snapshot", atClusterTime: lastOplogEntryTs}});
-    assert.commandWorked(res);
-    assert.eq(res.cursor.firstBatch.length, 7);
+    magicRestoreUtils.assertCannotDoSnapshotRead(destPrimary, 7 /* expectedNumDocs */);
 
     let diff = DataConsistencyChecker.getDiff(
         sourcePrimary.getDB("db").getCollection("coll").find().sort({_id: 1}),
