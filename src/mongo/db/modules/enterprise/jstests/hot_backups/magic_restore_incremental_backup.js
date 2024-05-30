854a9f2588995ee9eee56d609a4ebd3ba339292a
seanzimm <102551488+seanzimm@users.noreply.github.com>
2024-05-30 14:56:09 -0400

    SERVER-90343: Add incremental backup test for magic restore (#22645)
    # Issue
    
    SERVER-90343
    
    # Description
    
    This patch adds a targeted test for doing a magic restore with a backup
    cursor with `incrementalBackup:true` option. This patch only adds a
    non-PIT test since the incremental restore only impacts the initial data
    snapshot and will show no difference to applying PIT oplog entries
    
    # Testing
    
    [evg
    patch](https://spruce.mongodb.com/version/66561bd6b1730000077575d8/tasks?page=0&sorts=STATUS%3AASC%3BBASE_STATUS%3ADESC)

---
+++ src/mongo/db/modules/enterprise/jstests/hot_backups/magic_restore_incremental_backup.js
@@ -0,0 +1,124 @@
+/*
+ * Tests a non-PIT replica set restore with magic restore using incremental backup cursors. The test
+ * does the following:
+ *
+ * - Starts a replica set, inserts some initial data, and creates the incremental backup data files.
+ * - Copies data files to the restore dbpath and closes the backup cursor.
+ * - Writes additional entries to the replica set and takes an incremental backup cursor.
+ * - Writes a restore configuration object to a named pipe via the mongo shell.
+ * - Starts a mongod with --magicRestore that parses the restore configuration and exits cleanly.
+ * - Restarts the initial replica set and asserts the replica set config and data are what we
+ *   expect.
+ *
+ * @tags: [
+ *     requires_persistence,
+ *     requires_wiredtiger
+ * ]
+ */
+
+import {MagicRestoreUtils} from "jstests/libs/backup_utils.js";
+
+// TODO SERVER-86034: Run on Windows machines once named pipe related failures are resolved.
+if (_isWindows()) {
+    jsTestLog("Temporarily skipping test for Windows variants. See SERVER-86034.");
+    quit();
+}
+
+function runTest(insertHigherTermOplogEntry) {
+    jsTestLog("Running non-PIT magic restore with insertHigherTermOplogEntry: " +
+              insertHigherTermOplogEntry);
+    let rst = new ReplSetTest({nodes: 1});
+    rst.startSet();
+    rst.initiateWithHighElectionTimeout();
+
+    let primary = rst.getPrimary();
+    const dbName = "db";
+    const coll = "coll";
+
+    const db = primary.getDB(dbName);
+    // Insert some data to restore. This data will be reflected in the restored node.
+    ['a', 'b', 'c'].forEach(
+        key => { assert.commandWorked(db.getCollection(coll).insert({[key]: 1})); });
+
+    const magicRestoreUtils = new MagicRestoreUtils({
+        backupSource: primary,
+        pipeDir: MongoRunner.dataDir,
+        insertHigherTermOplogEntry: insertHigherTermOplogEntry
+    });
+    let metadata = magicRestoreUtils.takeCheckpointAndOpenBackup(
+        {incrementalBackup: true, thisBackupName: "backup"});
+    assert.eq(metadata.incrementalBackup, true);
+    const firstCheckpointTs = metadata.checkpointTimestamp;
+
+    // These documents will not be captured by the first backup cursor since they were added after
+    // the cursor was opened but will be captured by the second incremental cursor.
+    ['e', 'f', 'g'].forEach(
+        key => { assert.commandWorked(db.getCollection(coll).insert({[key]: 1})); });
+    assert.eq(db.getCollection(coll).find().toArray().length, 6);
+
+    magicRestoreUtils.assertOplogCountForNamespace(primary, dbName + "." + coll, 6, "i");
+    const expectedDocs = db.getCollection(coll).find().toArray();
+
+    magicRestoreUtils.copyFilesAndCloseBackup();
+
+    // Take another incremental backup to capture 'e', 'f', 'g' entries.
+    metadata = magicRestoreUtils.takeCheckpointAndOpenBackup(
+        {incrementalBackup: true, thisBackupName: "newbackup", srcBackupName: "backup"});
+    assert.eq(metadata.incrementalBackup, true);
+    // Make sure the incremental backup has a greater timestamp than the initial backup.
+    assert.gt(metadata.checkpointTimestamp, firstCheckpointTs);
+
+    // This insert will be truncated since it was done after the final incremental backup cursor was
+    // opened.
+    assert.commandWorked(db.getCollection(coll).insert({'h': 1}));
+
+    let {entriesAfterBackup} = magicRestoreUtils.getEntriesAfterBackup(primary);
+    // Only the entry for 'h'.
+    assert.eq(entriesAfterBackup.length, 1);
+    magicRestoreUtils.copyFilesAndCloseBackup();
+
+    let expectedConfig = assert.commandWorked(primary.adminCommand({replSetGetConfig: 1})).config;
+    // The new node will be allocated a new port by the test fixture.
+    expectedConfig.members[0].host = getHostName() + ":" + (Number(primary.port) + 2);
+    rst.stopSet(null /* signal */, false /* forRestart */, {noCleanData: true});
+
+    let restoreConfiguration = {
+        "nodeType": "replicaSet",
+        "replicaSetConfig": expectedConfig,
+        "maxCheckpointTs": magicRestoreUtils.getCheckpointTimestamp(),
+    };
+    restoreConfiguration =
+        magicRestoreUtils.appendRestoreToHigherTermThanIfNeeded(restoreConfiguration);
+
+    magicRestoreUtils.writeObjsAndRunMagicRestore(
+        restoreConfiguration, [], {"replSet": jsTestName()});
+
+    // Restart the destination replica set.
+    rst = new ReplSetTest({nodes: 1});
+    rst.startSet({dbpath: magicRestoreUtils.getBackupDbPath(), noCleanData: true});
+
+    primary = rst.getPrimary();
+    const restoredConfig = assert.commandWorked(primary.adminCommand({replSetGetConfig: 1})).config;
+
+    magicRestoreUtils.assertConfigIsCorrect(expectedConfig, restoredConfig);
+
+    const restoredDocs = primary.getDB(dbName).getCollection(coll).find().toArray();
+    // 'a', 'b', 'c' were captured by the initial backup cursor.
+    // 'e', 'f', 'g' were captured by the second incremental backup cursor.
+    // 'h' was truncated during magic restore.
+    assert.eq(restoredDocs.length, 6);
+    assert.eq(restoredDocs, expectedDocs);
+
+    magicRestoreUtils.assertOplogCountForNamespace(primary, dbName + "." + coll, 6, "i");
+    magicRestoreUtils.assertMinValidIsCorrect(primary);
+    magicRestoreUtils.assertStableCheckpointIsCorrectAfterRestore(primary);
+    magicRestoreUtils.assertCannotDoSnapshotRead(primary, 6 /* expectedNumDocs */);
+
+    rst.stopSet();
+}
+
+// insertHigherTermOplogEntry causes a no-op oplog entry insert with a higher term. This affects the
+// stable timestamp on magic restore node shutdown.
+for (const insertHigherTermOplogEntry of [false, true]) {
+    runTest(insertHigherTermOplogEntry);
+}
