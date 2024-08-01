8782c2bf781b78819d79bf8b555725d4411e9ac3
Enrico Golfieri <enrico.golfieri@mongodb.com>
2024-08-01 14:29:55 +0200

    SERVER-92847 The filtering metadata can be initiated as corrupted during initial sync (#25472)
    A fresh new node connected to the replica set could have its metadata
    corrupted (i.e untracked when the collection is tracked) until a refresh
    occurs.
    
    This can happen because a file-copy based initial sync starts before the
    ShardingState is enabled ( which will happen [at the
    completion](https://github.com/10gen/mongo/blob/a695720350743644976494523035049cf1354ce7/src/mongo/db/s/sharding_initialization_mongod.cpp#L659)
    of the initial sync) causing the collection to be created for the first
    time and
    [access](https://github.com/10gen/mongo/blob/a695720350743644976494523035049cf1354ce7/src/mongo/db/catalog/create_collection.cpp#L681)
    its filtering metadata, causing to [initiate
    them](https://github.com/10gen/mongo/blob/a695720350743644976494523035049cf1354ce7/src/mongo/db/s/collection_sharding_state.cpp#L76-L86).
    The filtering metadata initiated when the sharding state is disabled
    [will by default store them as
    UNTRACKED](https://github.com/10gen/mongo/blob/80bfe0fc45720a12122962658c574b208580d346/src/mongo/db/s/collection_sharding_runtime.cpp#L161C1-L164C53).
    
    Once the node is elected to secondary, the filtering metadata will stay
    corrupted until the next refresh, which can cause data loss and wrong
    query result.
    
    The PR includes:
    - A simple workaround
    - A test to reproduce the case
    - A unit test to verify the behaviour of the CSR

---
+++ src/mongo/db/modules/enterprise/jstests/fcbis/fcbis_correct_filtering_metadata_after_initial_sync.js
@@ -0,0 +1,57 @@
+/**
+ * Verifies the correctness of filtering metadata after a file copy based initial sync. The test
+ * adds a new node to a shard replica set (which triggers an initial sync), then relies on a stale
+ * router to forward a query to the newly added node and verifies the correctness of the returned
+ * data.
+ *
+ * @tags: [requires_persistence, requires_wiredtiger]
+ */
+
+import {ShardingStateTest} from "jstests/sharding/libs/sharding_state_test.js";
+
+const st = new ShardingTest({config: 1, shards: {rs0: {nodes: 1}, rs1: {nodes: 2}}, mongos: 2});
+
+const dbName = "test";
+const collName = "coll";
+const ns = dbName + "." + collName;
+const staleMongos = st.s1;
+const mongos = st.s0;
+const nDocs = 100;
+
+// Create a collection as UNTRACKED and refresh the stale router so that it will install the version
+// as UNTRACKED.
+assert.commandWorked(
+    mongos.adminCommand({enableSharding: dbName, primaryShard: st.shard0.shardName}));
+assert.commandWorked(st.s0.getDB(dbName).createCollection(collName));
+staleMongos.getDB(dbName).getCollection(collName).find({}).toArray();
+
+// Shard the collection using the non-stale router.
+/*
+ * current shard version for test.coll:
+ * staleMongos: UNTRACKED
+ * mongos: TRACKED
+ */
+assert.commandWorked(mongos.adminCommand({shardCollection: ns, key: {x: 1}, unique: false}));
+
+// Insert some data and balance them equally among the 2 shards (half amount of docs per shard).
+for (var i = 0; i < nDocs; i++) {
+    mongos.getDB(dbName).getCollection(collName).insert({x: i});
+}
+const midDocs = nDocs / 2;
+assert.commandWorked(mongos.adminCommand(
+    {moveRange: ns, toShard: st.shard1.shardName, min: {x: midDocs}, max: {x: MaxKey}}));
+
+// Initial sync a new node as primary (primary here is needed just to make sure we can direct query
+// to it).
+ShardingStateTest.addReplSetNode({
+    replSet: st.rs0,
+    serverTypeFlag: "shardsvr",
+    newNodeParams: {initialSyncMethod: "fileCopyBased"}
+});
+
+// The find should return all the documents.
+staleMongos.setReadPref("secondary");
+var res = staleMongos.getDB(dbName).getCollection(collName).find({}).itcount();
+assert.eq(nDocs, res);
+
+st.stop();
