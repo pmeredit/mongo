0dfa75db7dca55d0bb9e43c327a6e12aee66ac96
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2024-09-30 20:46:35 +1000

    SERVER-94145 Add queryable backup/restore testing (#27551)
    This adds a new suite `queryable_wt` and integrates it into Evergreen.
    I've also added three tests to demonstrate usage and get some initial
    testing going.

---
+++ src/mongo/db/modules/enterprise/jstests/queryable_wt/simple_rs_restore.js
@@ -0,0 +1,91 @@
+/**
+ * Takes a full backup against a replica set and restores it using queryable.
+ *
+ * @tags: [requires_wiredtiger]
+ */
+import {_copyFileHelper, openBackupCursor} from "jstests/libs/backup_utils.js";
+import {ReplSetTest} from "jstests/libs/replsettest.js";
+import {
+    kSeparator
+} from "src/mongo/db/modules/enterprise/jstests/hot_backups/libs/incremental_backup_helpers.js";
+
+// Windows doesn't guarantee synchronous file operations.
+if (_isWindows()) {
+    print("Skipping test on windows");
+    quit();
+}
+
+// When opening a backup cursor, only checkpointed data is backed up. However, the most up-to-date
+// size storer information is used. Thus the fast count may be inaccurate.
+TestData.skipEnforceFastCountOnValidate = true;
+
+const rst = new ReplSetTest({
+    nodes: 1,
+    nodeOptions: {
+        // Disable periodic checkpoints.
+        syncdelay: 0,
+    }
+});
+rst.startSet();
+rst.initiate();
+
+resetDbpath(TestData.queryable_dbpath);
+mkdir(TestData.queryable_dbpath + kSeparator + "journal");
+
+const primary = rst.getPrimary();
+
+// Take the initial checkpoint.
+assert.commandWorked(primary.adminCommand({fsync: 1}));
+
+// Create a collection and insert some data.
+const dbName = "test";
+const collName = jsTestName();
+
+let db = primary.getDB(dbName);
+assert.commandWorked(db.createCollection(collName));
+let coll = db.getCollection(collName);
+
+const kNumDocs = 10;
+for (let i = 0; i < kNumDocs; i++) {
+    assert.commandWorked(coll.insert({_id: i}));
+}
+
+const backupCursor = openBackupCursor(primary.getDB("admin"));
+
+// Print the metadata document.
+assert(backupCursor.hasNext());
+jsTestLog(backupCursor.next());
+
+while (backupCursor.hasNext()) {
+    const doc = backupCursor.next();
+
+    // Copy all the files for the full backup.
+    jsTestLog("Copying file: " + tojson(doc));
+    _copyFileHelper({filename: doc.filename, fileSize: doc.fileSize},
+                    primary.dbpath,
+                    TestData.queryable_dbpath);
+}
+
+backupCursor.close();
+
+rst.stopSet();
+
+// We don't need to update the `storage.bson` file to modify the storage engine from wiredTiger to
+// queryable_wt as the file isn't copied by the backup cursor.
+let conn = MongoRunner.runMongod({
+    dbpath: TestData.queryable_dbpath,
+    storageEngine: "queryable_wt",
+    noCleanData: true,
+    queryableBackupApiUri: "localhost:8080",
+    queryableSnapshotId: "123456781234567812345678",
+    queryableBackupMode: ""
+});
+assert(conn);
+
+db = conn.getDB(dbName);
+coll = db.getCollection(collName);
+
+// All documents to the collection were written after the checkpoint the backup was taken against.
+// This test scenario does not replay the oplog.
+assert.eq(0, coll.find().toArray().length);
+MongoRunner.stopMongod(conn);
---
+++ src/mongo/db/modules/enterprise/jstests/queryable_wt/simple_standalone_restore.js
@@ -0,0 +1,81 @@
+/**
+ * Takes a full backup against a standalone and restores it using queryable.
+ *
+ * @tags: [requires_wiredtiger]
+ */
+import {_copyFileHelper, openBackupCursor} from "jstests/libs/backup_utils.js";
+import {
+    kSeparator
+} from "src/mongo/db/modules/enterprise/jstests/hot_backups/libs/incremental_backup_helpers.js";
+
+// Windows doesn't guarantee synchronous file operations.
+if (_isWindows()) {
+    print("Skipping test on windows");
+    quit();
+}
+
+// When opening a backup cursor, only checkpointed data is backed up. However, the most up-to-date
+// size storer information is used. Thus the fast count may be inaccurate.
+TestData.skipEnforceFastCountOnValidate = true;
+
+let conn = MongoRunner.runMongod();
+assert(conn);
+
+resetDbpath(TestData.queryable_dbpath);
+mkdir(TestData.queryable_dbpath + kSeparator + "journal");
+
+// Take the initial checkpoint.
+assert.commandWorked(conn.adminCommand({fsync: 1}));
+
+// Create a collection and insert some data.
+const dbName = "test";
+const collName = jsTestName();
+
+let db = conn.getDB(dbName);
+assert.commandWorked(db.createCollection(collName));
+let coll = db.getCollection(collName);
+
+const kNumDocs = 10;
+for (let i = 0; i < kNumDocs; i++) {
+    assert.commandWorked(coll.insert({_id: i}));
+}
+
+const backupCursor = openBackupCursor(conn.getDB("admin"));
+
+// Print the metadata document.
+assert(backupCursor.hasNext());
+jsTestLog(backupCursor.next());
+
+while (backupCursor.hasNext()) {
+    const doc = backupCursor.next();
+
+    // Copy all the files for the full backup.
+    jsTestLog("Copying file: " + tojson(doc));
+    _copyFileHelper(
+        {filename: doc.filename, fileSize: doc.fileSize}, conn.dbpath, TestData.queryable_dbpath);
+}
+
+backupCursor.close();
+
+MongoRunner.stopMongod(conn);
+
+// We don't need to update the `storage.bson` file to modify the storage engine from wiredTiger to
+// queryable_wt as the file isn't copied by the backup cursor.
+conn = MongoRunner.runMongod({
+    dbpath: TestData.queryable_dbpath,
+    storageEngine: "queryable_wt",
+    noCleanData: true,
+    queryableBackupApiUri: "localhost:8080",
+    queryableSnapshotId: "123456781234567812345678",
+    queryableBackupMode: ""
+});
+assert(conn);
+
+db = conn.getDB(dbName);
+coll = db.getCollection(collName);
+
+// All documents to the collection were written after the checkpoint the backup was taken against.
+// However, in standalone mode all tables have logging enabled and we expect the documents to be
+// recovered.
+assert.eq(kNumDocs, coll.find().toArray().length);
+MongoRunner.stopMongod(conn);
