62301aadcc4796b4854af32d703c9da4773c9368
Sara Golemon <sara.golemon@mongodb.com>
2024-10-18 14:52:57 -0500

    SERVER-95743 Convert buildInfo to TypedCommand (#28041)
    1. `BuildInfo` declared as IDL type `mongo/util/buildinfo.idl`
    2. Marshalling `VersionInfoInterface` data into `BuildInfo` handled
    separate from implementations `mongo/util/buildinfo.cpp`
    3. Existing calls to `VersionInfoInterface::appendBuildInfo()` converted
    to typed `getBuildInfo()`
    4. `appendStorageEngineList` translated to `getStorageEngineNames`.
    5. Base TypedCommand implementation `CmdBuildInfoCommon` implements
    basic `buildInfo` command.
    6. Shard role override in `dbcommands.cpp` to add `storageEngine` field.

--- src/mongo/db/modules/enterprise/src/fle/commands/cryptd_buildinfo.cpp
+++ src/mongo/db/modules/enterprise/src/fle/commands/cryptd_buildinfo.cpp
@@ -3,53 +3,10 @@
  */
 
 
-#include "mongo/platform/basic.h"
-
-#include "mongo/db/commands.h"
-#include "mongo/util/version.h"
-
-#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
-
+#include "mongo/db/commands/buildinfo_common.h"
 
 namespace mongo {
 namespace {
-
-/**
- * Implements { buildInfo : 1} for mongocryptd
- *
- * NOTE: The only method called is run(), the rest exist simply to ensure the code compiles.
- */
-class CryptDCmdBuildInfo : public BasicCommand {
-public:
-    CryptDCmdBuildInfo() : BasicCommand("buildInfo", "buildinfo") {}
-
-    AllowedOnSecondary secondaryAllowed(ServiceContext*) const final {
-        return AllowedOnSecondary::kAlways;
-    }
-
-    bool supportsWriteConcern(const BSONObj& cmd) const final {
-        return false;
-    }
-    std::string help() const final {
-        return "get version #, etc.\n"
-               "{ buildinfo:1 }";
-    }
-
-    Status checkAuthForOperation(OperationContext*,
-                                 const DatabaseName&,
-                                 const BSONObj&) const final {
-        return {ErrorCodes::Unauthorized, "unauthorized"};
-    }
-
-    bool run(OperationContext* opCtx,
-             const DatabaseName&,
-             const BSONObj& jsobj,
-             BSONObjBuilder& result) final {
-        VersionInfoInterface::instance().appendBuildInfo(&result);
-        return true;
-    }
-};
-MONGO_REGISTER_COMMAND(CryptDCmdBuildInfo).forShard();
-
+MONGO_REGISTER_COMMAND(CmdBuildInfoCommon).forShard();
 }  // namespace
 }  // namespace mongo
