99a430ca1d2714e457cbb7dfb9e22ce60d971a7b
Sara Golemon <sara.golemon@mongodb.com>
2024-10-21 09:12:20 -0500

    SERVER-95941 Do not log IPC events in audit log test (#28263)

--- src/mongo/db/modules/enterprise/jstests/audit/orphan-session.js
+++ src/mongo/db/modules/enterprise/jstests/audit/orphan-session.js
@@ -18,10 +18,13 @@ let runTest = function(conn, audit, admin) {
     admin.auth("admin", "pwd");
 };
 
+// Do not emit audit entries for IPC events.
+const auditFilter = JSON.stringify({"users": {"$ne": {user: "__system", db: "local"}}});
+
 {
     print("START orphan-session.js for standalone");
 
-    const m = MongoRunner.runMongodAuditLogger({auth: ''});
+    const m = MongoRunner.runMongodAuditLogger({auth: '', auditFilter: auditFilter});
     const audit = m.auditSpooler();
     const admin = m.getDB("admin");
 
@@ -34,7 +37,8 @@ let runTest = function(conn, audit, admin) {
 {
     print("START orphan-session.js for sharded cluster");
 
-    const st = MongoRunner.runShardedClusterAuditLogger({auth: null, keyFile: "jstests/libs/key1"});
+    const st = MongoRunner.runShardedClusterAuditLogger({keyFile: "jstests/libs/key1"},
+                                                        {auditFilter: auditFilter});
     const mongos = st.s0;
     const audit = mongos.auditSpooler();
     const admin = mongos.getDB("admin");
