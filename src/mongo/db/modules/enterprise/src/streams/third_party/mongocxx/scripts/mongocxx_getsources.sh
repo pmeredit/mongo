a30baf3f0059b777835c87792f65856a29f73c05
mdb-mayureshk <mayuresh.kulkarni@mongodb.com>
2024-11-20 10:04:10 -0800

    SERVER-97402 Upgrade mongocxx-driver version to 3.7.2 (#29386)

--- src/mongo/db/modules/enterprise/src/streams/third_party/mongocxx/scripts/mongocxx_getsources.sh
+++ src/mongo/db/modules/enterprise/src/streams/third_party/mongocxx/scripts/mongocxx_getsources.sh
@@ -19,20 +19,27 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR="$SCRIPT_DIR/.."
 mkdir -p $BASE_DIR
 DIST_DIR=$BASE_DIR/dist
-platformName=linux_$(uname -m)
-PLATFORM_DIR="$DIST_DIR/platform/$platformName"
+linuxPlatform=linux_x86_64
+aarchPlatform=linux_aarch64
+PLATFORM_DIR="$DIST_DIR/platform"
 
 # Copy existing platform files. For future version upgrades, we may have to run the cmake invocation
 # to configure the mongoc/mongocxx/bsoncxx repos instead of reusing existing platform files.
-TEMP_BSONCXX_PLATFORM_DIR=/tmp/existing/bsoncxx/$platformName
+TEMP_BSONCXX_PLATFORM_DIR=/tmp/existing/bsoncxx
 rm -rf $TEMP_BSONCXX_PLATFORM_DIR
-mkdir -p $TEMP_BSONCXX_PLATFORM_DIR
-cp -r $PLATFORM_DIR/bsoncxx $TEMP_BSONCXX_PLATFORM_DIR
+for platform in $linuxPlatform $aarchPlatform
+do
+  mkdir -p $TEMP_BSONCXX_PLATFORM_DIR/$platform
+  cp -r $PLATFORM_DIR/$platform/bsoncxx $TEMP_BSONCXX_PLATFORM_DIR/$platform
+done
 
-TEMP_MONGOCXX_PLATFORM_DIR=/tmp/existing/mongocxx/$platformName
+TEMP_MONGOCXX_PLATFORM_DIR=/tmp/existing/mongocxx/
 rm -rf $TEMP_MONGOCXX_PLATFORM_DIR
-mkdir -p $TEMP_MONGOCXX_PLATFORM_DIR
-cp -r $PLATFORM_DIR/mongocxx $TEMP_MONGOCXX_PLATFORM_DIR
+for platform in $linuxPlatform $aarchPlatform
+do
+  mkdir -p $TEMP_MONGOCXX_PLATFORM_DIR/$platform
+  cp -r $PLATFORM_DIR/$platform/mongocxx $TEMP_MONGOCXX_PLATFORM_DIR/$platform
+done
 
 # Clean the output directories
 rm -rf $DIST_DIR
@@ -64,18 +71,21 @@ mkdir -p $DIST_DIR/bson
 cp -r src/libbson/src/bson/*.h $DIST_DIR/bson
 
 # Copy the mongoc platform specified files
-mkdir -p $PLATFORM_DIR/mongoc
-cp cmake-build/src/libmongoc/src/mongoc/mongoc-config.h $PLATFORM_DIR/mongoc
+for platform in $linuxPlatform $aarchPlatform
+do
+  mkdir -p $PLATFORM_DIR/$platform/mongoc
+  cp cmake-build/src/libmongoc/src/mongoc/mongoc-config.h $PLATFORM_DIR/$platform/mongoc
+done
 
 # Copy the mongoc licences
 cp THIRD_PARTY_NOTICES $DIST_DIR/mongoc
 cp COPYING $DIST_DIR/mongoc
 
-# Download the 3.7.1 release of mongocxx
+# Download the 3.7.2 release of mongocxx
 cd $TEMP_DIR
-curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.7.1/mongo-cxx-driver-r3.7.1.tar.gz
-tar -xzf mongo-cxx-driver-r3.7.1.tar.gz
-cd mongo-cxx-driver-r3.7.1/build
+curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.7.2/mongo-cxx-driver-r3.7.2.tar.gz
+tar -xzf mongo-cxx-driver-r3.7.2.tar.gz
+cd mongo-cxx-driver-r3.7.2/build
 # TODO Get this cmake invocation to work.
 #cmake .. -DCMAKE_BUILD_TYPE=Release -DBSONCXX_POLY_USE_MNMLSTC=0 -DBSONCXX_POLY_USE_BOOST=1 -DENABLE_TESTS=OFF
 cd ..
@@ -87,8 +97,12 @@ cp -r src/bsoncxx/* $DIST_DIR/bsoncxx
 # in the main source files.
 rm -rf $DIST_DIR/bsoncxx/test
 # Copy the platform specific bsoncxx source files
-mkdir -p $PLATFORM_DIR/bsoncxx/config/private
-cp -r $TEMP_BSONCXX_PLATFORM_DIR/bsoncxx/config/* $PLATFORM_DIR/bsoncxx/config
+for platform in $linuxPlatform $aarchPlatform
+do 
+  mkdir -p $PLATFORM_DIR/$platform/bsoncxx/config/private
+  cp -r $TEMP_BSONCXX_PLATFORM_DIR/$platform/bsoncxx/config/* $PLATFORM_DIR/$platform/bsoncxx/config
+done 
+
 # TODO Once we can run the project cmake properly, copy the generated platform files
 #cp build/src/bsoncxx/config/config.hpp $PLATFORM_DIR/bsoncxx/config
 #cp build/src/bsoncxx/config/export.hpp $PLATFORM_DIR/bsoncxx/config
@@ -103,8 +117,12 @@ cp -r src/mongocxx/* $DIST_DIR/mongocxx
 # in the main source files.
 rm -rf $DIST_DIR/mongocxx/test
 # Copy the platform specific mongocxx source files
-mkdir -p $PLATFORM_DIR/mongocxx/config/private
-cp -r $TEMP_MONGOCXX_PLATFORM_DIR/mongocxx/config/* $PLATFORM_DIR/mongocxx/config
+for platform in $linuxPlatform $aarchPlatform
+do
+  mkdir -p $PLATFORM_DIR/$platform/mongocxx/config/private
+  cp -r $TEMP_MONGOCXX_PLATFORM_DIR/$platform/mongocxx/config/* $PLATFORM_DIR/$platform/mongocxx/config
+done
+
 #cp build/src/mongocxx/config/config.hpp $PLATFORM_DIR/mongocxx/config
 #cp build/src/mongocxx/config/export.hpp $PLATFORM_DIR/mongocxx/config
 #cp build/src/mongocxx/config/version.hpp $PLATFORM_DIR/mongocxx/config
