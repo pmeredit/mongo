d45a90f66ee4852cf52c1d00244434063d1fc0ee
HanaPearlman <hana.pearlman@mongodb.com>
2024-12-13 08:06:49 -0500

    SERVER-98129: Ensure QE commands are not logged by tassert logging mechanism (#29994)
    This PR introduces e2e tests ensuring that when queryable encryption
    commands fail with a tassert, they are not logged by the ScopedDebugInfo
    logging mechanism. In includes:
    - Introduce a new failpoint in the query planner that triggers a tassert
    when enabled
    - Change the diagnostic printer to check the whole curop stack for
    `shouldOmitDiagnosticInfo`

---
+++ src/mongo/db/modules/enterprise/jstests/no_passthrough/fle_tassert_log_omit.js
@@ -0,0 +1,201 @@
+/**
+ * Test log omission on tassert works correctly for various queryable encryption operations. Runs QE
+ * operations that hit tasserts and ensures that the resulting ScopedDebugInfo log line does not
+ * contain operation details.
+ *
+ * This test expects collections to persist across a restart.
+ * @tags: [requires_persistence]
+ */
+
+import {EncryptedClient} from "jstests/fle2/libs/encrypted_client_util.js";
+import {
+    failAllInserts,
+    getDiagnosticLogs,
+    planExecutorAlwaysFails,
+    queryPlannerAlwaysFails,
+    runWithFailpoint
+} from "jstests/libs/query/command_diagnostic_utils.js";
+import {ReplSetTest} from "jstests/libs/replsettest.js";
+import {ShardingTest} from "jstests/libs/shardingtest.js";
+
+// All commands below will reference the encrypted field "a" (see 'query'); when we tassert during
+// command processing, we should not see the command in the ScopeDebugInfo log line.
+const dbName = "test";
+const collName = jsTestName();
+function getEncryptedFields() {
+    return {
+        encryptedFields: {
+            "fields": [
+                {"path": "a", "bsonType": "int", "queries": {"queryType": "equality"}},
+            ]
+        }
+    };
+}
+const query = {
+    a: NumberInt(1),
+    b: 1
+};
+
+function runTest(conn, restartFn) {
+    const db = conn.getDB(dbName);
+    const client = new EncryptedClient(db.getMongo(), dbName);
+
+    // Runs 'command' with 'failpoint' enabled, asserts that a ScopedDebugInfo log line is
+    // logged, and that it does not contain the command description.
+    function runEncryptedCommandAndCheckLog({description, command, failpoint}) {
+        jsTestLog("Running test case: " + tojson(description));
+
+        let edb = client.getDB();
+        runWithFailpoint(edb, failpoint.failpointName, failpoint.failpointOpts, () => {
+            if (!command.bulkWrite) {
+                assert.commandFailedWithCode(edb.erunCommand(command), failpoint.errorCode);
+            } else {
+                assert.commandWorked(edb.eadminCommand(command));
+            }
+        });
+
+        // Ignore any diagnostic log lines for queries that may be failing in the background.
+        const diagnostics = getDiagnosticLogs({
+                                description: description,
+                                logFile: conn.fullOptions.logFile
+                            }).filter(line => line.includes(collName) || !line.includes("ns: "));
+        assert(diagnostics.length > 0, "Failed to find relevant ScopedDebugInfo log line");
+
+        // Log lines should not contain the command contents. There may be multiple ScopedDebugInfo
+        // log lines, and that each one can contain >1 commandDiagnostic entries.
+        assert(diagnostics.every(line => line.includes("commandDiagnostics: omitted") &&
+                                     !line.includes("opDescription")),
+               "Found a log line containing command diagnostics, when none were expected " +
+                   tojson(diagnostics));
+
+        // Each test case restarts the required node to clear the previous logs.
+        restartFn();
+    }
+
+    // GetMore
+    const {cursor} =
+        assert.commandWorked(client.getDB().erunCommand({find: collName, batchSize: 0}));
+    runEncryptedCommandAndCheckLog({
+        failpoint: planExecutorAlwaysFails,
+        description: "getMore",
+        command: {getMore: cursor.id, collection: collName}
+    });
+
+    // Find
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: "simple find",
+        command: {find: collName, filter: query, limit: 1},
+    });
+
+    // Aggregate
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: "agg with a simple $match",
+        command: {aggregate: collName, pipeline: [{$match: query}], cursor: {}},
+    });
+
+    // Count
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: "count",
+        command: {count: collName, query: query},
+    });
+
+    // Insert
+    runEncryptedCommandAndCheckLog({
+        failpoint: failAllInserts,
+        description: "insert",
+        command: {insert: collName, documents: [query], ordered: false},
+    });
+
+    // Delete
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: 'delete',
+        command: {
+            delete: collName,
+            deletes: [{q: query, limit: 1}],
+        },
+    });
+
+    // Update
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: 'update with simple filter',
+        command: {
+            update: collName,
+            updates: [{q: query, u: {b: 2}}],
+        },
+    });
+
+    // FindAndModify
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: 'findAndModify remove',
+        command: {
+            findAndModify: collName,
+            query: query,
+            remove: true,
+        },
+    });
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: 'findAndModify update',
+        command: {
+            findAndModify: collName,
+            query: query,
+            update: {b: 2},
+        },
+    });
+
+    // BulkWrite
+    runEncryptedCommandAndCheckLog({
+        failpoint: queryPlannerAlwaysFails,
+        description: 'bulkWrite update',
+        command: {
+            bulkWrite: 1,
+            ops: [
+                {update: 0, filter: query, updateMods: {b: 1}},
+            ],
+            nsInfo: [{ns: `${dbName}.${collName}`}],
+        },
+        // The top-level command doesn't fail when an update fails.
+        errorCode: 0,
+    });
+}
+
+jsTestLog("ReplicaSet: Testing fle2 tassert log omission");
+{
+    const testFixture = new ReplSetTest({nodes: 1, nodeOptions: {useLogFiles: true}});
+    testFixture.startSet();
+    testFixture.initiate();
+    testFixture.awaitReplication();
+
+    const db = testFixture.getPrimary().getDB(dbName);
+    db.dropDatabase();
+    const client = new EncryptedClient(db.getMongo(), dbName);
+    assert.commandWorked(client.createEncryptionCollection(collName, getEncryptedFields()));
+
+    runTest(testFixture.getPrimary(), () => {
+        testFixture.restart(testFixture.getPrimary(), {allowedExitCode: MongoRunner.EXIT_ABRUPT});
+        testFixture.waitForPrimary();
+    });
+    testFixture.stopSet();
+}
+
+jsTestLog("ShardingTest: Testing fle2 tassert log omission on shards");
+{
+    const fixture = new ShardingTest({shards: [{useLogFiles: true}], mongos: 1});
+
+    const db = fixture.s.getDB(dbName);
+    db.dropDatabase();
+    const client = new EncryptedClient(db.getMongo(), dbName);
+    assert.commandWorked(client.createEncryptionCollection(collName, getEncryptedFields()));
+
+    runTest(fixture.rs0.getPrimary(), () => {
+        fixture.rs0.restart(fixture.rs0.getPrimary(), {allowedExitCode: MongoRunner.EXIT_ABRUPT});
+        fixture.rs0.waitForPrimary();
+    });
+    fixture.stop();
+}
