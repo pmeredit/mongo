bc15a2a850f139939234b5e3534ddc2906785138
Mickey J Winters <314083+mjrb@users.noreply.github.com>
2024-12-16 15:14:10 -0500

    SERVER-95942 remove uneeded special handling for staleDbVersion in $out (#28772)
    Co-authored-by: Mihai Andrei <mihai.andrei@mongodb.com>

--- src/mongo/db/modules/enterprise/src/streams/exec/mongodb_process_interface.h
+++ src/mongo/db/modules/enterprise/src/streams/exec/mongodb_process_interface.h
@@ -317,12 +317,6 @@ public:
         const mongo::NamespaceString& nss,
         const std::set<mongo::FieldPath>& fieldPaths) const override;
 
-    boost::optional<mongo::DatabaseVersion> refreshAndGetDatabaseVersion(
-        const boost::intrusive_ptr<mongo::ExpressionContext>& expCtx,
-        const mongo::DatabaseName& dbName) const override {
-        return boost::none;
-    }
-
     void checkRoutingInfoEpochOrThrow(const boost::intrusive_ptr<mongo::ExpressionContext>& expCtx,
                                       const mongo::NamespaceString&,
                                       mongo::ChunkVersion) const override {
