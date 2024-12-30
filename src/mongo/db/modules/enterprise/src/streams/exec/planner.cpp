e5216d3cc2306ffb641bcc2c71c649879d1f617f
Jada Lilleboe <82007190+jadalilleboe@users.noreply.github.com>
2024-12-30 13:06:56 -0600

    SERVER-98429 Initial plumbing for processingTime windows (#30543)

--- src/mongo/db/modules/enterprise/src/streams/exec/planner.cpp
+++ src/mongo/db/modules/enterprise/src/streams/exec/planner.cpp
@@ -534,6 +534,25 @@ void configureContextStreamMetaFieldName(Context* context, StringData streamMeta
 Planner::Planner(Context* context, Options options)
     : _context(context), _options(std::move(options)), _nextOperatorId(_options.minOperatorId) {}
 
+mongo::WindowBoundaryEnum Planner::getValidBoundary(auto options) {
+    mongo::WindowBoundaryEnum boundary = options.getBoundary();
+    if (boundary == WindowBoundaryEnum::processingTime) {
+        auto enabled =
+            _context->featureFlags->getFeatureFlagValue(FeatureFlags::kProcessingTimeWindows)
+                .getBool();
+        uassert(ErrorCodes::StreamProcessorInvalidOptions,
+                "Processing time windows are not supported",
+                enabled && *enabled);
+        uassert(ErrorCodes::StreamProcessorInvalidOptions,
+                "Cannot specify allowed lateness value for a processing time window",
+                !options.getAllowedLateness());
+        uassert(ErrorCodes::StreamProcessorInvalidOptions,
+                "Cannot specify idle timeout value for a processing time window",
+                !options.getIdleTimeout());
+    }
+    return boundary;
+}
+
 void Planner::appendOperator(std::unique_ptr<Operator> oper) {
     if (!_operators.empty()) {
         _operators.back()->addOutput(oper.get(), 0);
@@ -1200,6 +1219,7 @@ BSONObj Planner::planTumblingWindow(DocumentSource* source) {
             interval.getSize() > 0);
 
     WindowAssigner::Options windowingOptions;
+    windowingOptions.boundary = getValidBoundary(options);
     windowingOptions.size = interval.getSize();
     windowingOptions.sizeUnit = interval.getUnit();
     windowingOptions.slide = interval.getSize();
@@ -1288,6 +1308,7 @@ BSONObj Planner::planHoppingWindow(DocumentSource* source) {
             hopInterval.getSize() > 0);
 
     WindowAssigner::Options windowingOptions;
+    windowingOptions.boundary = getValidBoundary(options);
     windowingOptions.size = windowInterval.getSize();
     windowingOptions.sizeUnit = windowInterval.getUnit();
     windowingOptions.slide = hopInterval.getSize();
--- src/mongo/db/modules/enterprise/src/streams/exec/planner.h
+++ src/mongo/db/modules/enterprise/src/streams/exec/planner.h
@@ -103,6 +103,7 @@ private:
         std::vector<std::pair<mongo::BSONObj, mongo::BSONObj>> rewrittenLookupStages;
     };
 
+    mongo::WindowBoundaryEnum getValidBoundary(auto options);
     // Adds the given Operator to '_operators'.
     void appendOperator(std::unique_ptr<Operator> oper);
 
