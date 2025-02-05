18620c41f896d9f577f85b232e6a95fffb9e71df
mongodb-matthew-normyle <120420718+mongodb-matthew-normyle@users.noreply.github.com>
2025-02-05 13:41:27 -0600

    SERVER-100480: Disable infinite loop test (#32020)
    This test is causing a lot of red in CI (see
    https://jira.mongodb.org/browse/BF-36414). Disabling this test for now,
    root causing and fixing the failure in
    https://jira.mongodb.org/browse/SERVER-100479.

--- src/mongo/db/modules/enterprise/jstests/streams/infinite_loop.js
+++ src/mongo/db/modules/enterprise/jstests/streams/infinite_loop.js
@@ -11,40 +11,45 @@ import {listStreamProcessors} from "src/mongo/db/modules/enterprise/jstests/stre
 // writing to. This infinite loop creates a document that gets larger and larger. This will
 // eventually lead to an error from the _changestream server_, complaining 16MB limit is exceeded.
 // In this case we fail the stream processor with a non-retryable error.
+function infiniteLoop() {
+    const sp = getDefaultSp();
+    const spName = "changeStreamDbSource";
+    const inputDb = db.getSiblingDB(test.dbName);
+    const outputColl = db.getSiblingDB(test.dbName)[test.outputCollName];
+    inputDb.dropDatabase();
+    outputColl.drop();
+    sp.createStreamProcessor(spName, [
+        {$source: {connectionName: test.atlasConnection, db: test.dbName}},
+        {
+            $merge: {
+                into: {
+                    connectionName: test.atlasConnection,
+                    db: test.dbName,
+                    coll: test.outputCollName
+                }
+            }
+        },
+    ]);
+    sp[spName].start();
 
-// Start a streamProcessor.
-const sp = getDefaultSp();
-const spName = "changeStreamDbSource";
-const inputDb = db.getSiblingDB(test.dbName);
-const outputColl = db.getSiblingDB(test.dbName)[test.outputCollName];
-inputDb.dropDatabase();
-outputColl.drop();
-sp.createStreamProcessor(spName, [
-    {$source: {connectionName: test.atlasConnection, db: test.dbName}},
-    {
-        $merge: {
-            into: {connectionName: test.atlasConnection, db: test.dbName, coll: test.outputCollName}
-        }
-    },
-]);
-sp[spName].start();
+    // Insert a doc that will start the infinite loop, verify the stream processor ends in an error
+    // state.
+    const doc = {a: 1};
+    const inputColl = inputDb[test.inputCollName];
+    inputColl.insertOne(doc);
+    assert.soon(() => {
+        const result = sp.listStreamProcessors();
+        let thisSp = result.streamProcessors.find((sp) => sp.name == spName);
+        assert.neq(thisSp, null);
+        let stats = sp[spName].stats();
+        return stats.dlqMessageCount > 0 ||
+            (thisSp.status == "error" &&
+             thisSp.error.code == ErrorCodes.StreamProcessorSourceDocTooLarge &&
+             thisSp.error.userError == true && thisSp.error.retryable == false);
+    }, "waiting for error to appear", 5 * 60 * 1000);
+    sp[spName].stop();
+    assert.eq(listStreamProcessors()["streamProcessors"].length, 0);
+}
 
-// Insert a doc that will start the infinite loop, verify the stream processor ends in an error
-// state.
-const doc = {
-    a: 1
-};
-const inputColl = inputDb[test.inputCollName];
-inputColl.insertOne(doc);
-assert.soon(() => {
-    const result = sp.listStreamProcessors();
-    let thisSp = result.streamProcessors.find((sp) => sp.name == spName);
-    assert.neq(thisSp, null);
-    let stats = sp[spName].stats();
-    return stats.dlqMessageCount > 0 ||
-        (thisSp.status == "error" &&
-         thisSp.error.code == ErrorCodes.StreamProcessorSourceDocTooLarge &&
-         thisSp.error.userError == true && thisSp.error.retryable == false);
-}, "waiting for error to appear", 5 * 60 * 1000);
-sp[spName].stop();
-assert.eq(listStreamProcessors()["streamProcessors"].length, 0);
+// TODO(SERVER-100479): RCA and re-enable infinite loop test.
+// infiniteLoop();
