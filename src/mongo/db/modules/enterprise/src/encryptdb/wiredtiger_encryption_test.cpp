e029f44e94a09a975067b05f26d9a4aab41fa327
Aaron B <aaron.balsara@mongodb.com>
2025-02-27 09:34:38 +1100

    SERVER-98110 Track duration between interrupt and WT Eviction interrupt (#31714)
    Measure the time taken from when the opCtx receives an interrupt to when
    WT calls the general callback and log it to slow logs. This assumes that
    WT will abort eviction "soon" after it calls the callback.

--- src/mongo/db/modules/enterprise/src/encryptdb/wiredtiger_encryption_test.cpp
+++ src/mongo/db/modules/enterprise/src/encryptdb/wiredtiger_encryption_test.cpp
@@ -71,7 +71,7 @@ public:
           _connection(_connectionTest.getConnection(), _connectionTest.getClockSource()) {}
 
     void writeData(OperationContext* opCtx) {
-        WiredTigerRecoveryUnit ru = WiredTigerRecoveryUnit(&_connection, nullptr);
+        WiredTigerRecoveryUnit ru = WiredTigerRecoveryUnit(&_connection, nullptr, nullptr);
         ru.setOperationContext(opCtx);
         WiredTigerSession* session = ru.getSession();
 
@@ -127,7 +127,7 @@ public:
     }
 
     void readData(OperationContext* opCtx) {
-        WiredTigerRecoveryUnit recoveryUnit(&_connection, nullptr);
+        WiredTigerRecoveryUnit recoveryUnit(&_connection, nullptr, nullptr);
         recoveryUnit.setOperationContext(opCtx);
         WiredTigerSession* session = recoveryUnit.getSession();
 
