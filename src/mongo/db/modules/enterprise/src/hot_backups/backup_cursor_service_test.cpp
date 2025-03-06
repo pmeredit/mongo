c9dec03c3d51fe4c8161678bbbc846c08b63df16
Haley Connelly <haley.connelly@mongodb.com>
2025-03-06 12:48:14 -0600

    SERVER-101357 Remove unused 'allocateDefaultSpace' parameter from DurableCatalog::createCollection (#32795)

--- src/mongo/db/modules/enterprise/src/hot_backups/backup_cursor_service_test.cpp
+++ src/mongo/db/modules/enterprise/src/hot_backups/backup_cursor_service_test.cpp
@@ -403,8 +403,7 @@ private:
         // Adds the collection to the durable catalog.
         auto storageEngine = opCtx->getServiceContext()->getStorageEngine();
         std::pair<RecordId, std::unique_ptr<RecordStore>> catalogIdRecordStorePair =
-            uassertStatusOK(storageEngine->getCatalog()->createCollection(
-                opCtx, nss, newOptions, /*allocateDefaultSpace=*/true));
+            uassertStatusOK(storageEngine->getCatalog()->createCollection(opCtx, nss, newOptions));
         auto& catalogId = catalogIdRecordStorePair.first;
         auto catalogEntry = DurableCatalog::get(opCtx)->getParsedCatalogEntry(opCtx, catalogId);
         auto metadata = catalogEntry->metadata;
