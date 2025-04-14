bf146602a87cdf50b6f66cdcb85e009bc171b7c5
Will Buerger <59492746+wbuerger46@users.noreply.github.com>
2025-04-14 14:45:55 -0400

    [PoC] Lint JSTests (#34988)
    I ran the JavaScript linter and didn't want these changes to bloat my upcoming PR so posting this separately.

--- jstests/aggregation/sources/addSomeCrabs.js
+++ jstests/aggregation/sources/addSomeCrabs.js
@@ -1,11 +1,7 @@
 const coll = db.docs;
 coll.drop();
 
-assert.commandWorked(coll.insertMany([
-    {_id: 0, a: 1},
-    {_id: 1, a: 2},
-    {_id: 2, a: 3}
-]));
+assert.commandWorked(coll.insertMany([{_id: 0, a: 1}, {_id: 1, a: 2}, {_id: 2, a: 3}]));
 
 assert.eq(coll.aggregate([{$addSomeCrabs: 3}]).toArray(), [
     {_id: 0, a: 1, someCrabs: "🦀🦀🦀"},
@@ -18,7 +14,5 @@ assert.eq(coll.aggregate([{$addSomeCrabs: 1}, {$limit: 2}]).toArray(), [
     {_id: 1, a: 2, someCrabs: "🦀"},
 ]);
 
-assert.eq(coll.aggregate([{$match: {a: {"$gte": 2}}}, {$addSomeCrabs: 5}]).toArray(), [
-    {_id: 1, a: 2, someCrabs: "🦀🦀🦀🦀🦀"},
-    {_id: 2, a: 3, someCrabs: "🦀🦀🦀🦀🦀"}
-]);
+assert.eq(coll.aggregate([{$match: {a: {"$gte": 2}}}, {$addSomeCrabs: 5}]).toArray(),
+          [{_id: 1, a: 2, someCrabs: "🦀🦀🦀🦀🦀"}, {_id: 2, a: 3, someCrabs: "🦀🦀🦀🦀🦀"}]);
--- jstests/aggregation/sources/echoC.js
+++ jstests/aggregation/sources/echoC.js
@@ -1,12 +1,15 @@
 const coll = db.add_fields_dotted_paths;
 coll.drop();
 
-const input = { "c": "C!" }
+const input = {
+    "c": "C!"
+}
 
-assert.eq(coll.aggregate([{$echoOxide: input}]).toArray(), [input]);
+              assert.eq(coll.aggregate([{$echoOxide: input}]).toArray(), [input]);
 
-assert.eq(coll.aggregate([{$echoWithSomeCrabs: {document: input, numCrabs: 0}}]).toArray(), [input]);
+assert.eq(coll.aggregate([{$echoWithSomeCrabs: {document: input, numCrabs: 0}}]).toArray(),
+          [input]);
 assert.eq(coll.aggregate([{$echoWithSomeCrabs: {document: input, numCrabs: 3}}]).toArray(), [{
-    "c": "C!",
-    "someCrabs": "🦀🦀🦀",
-}]);
+              "c": "C!",
+              "someCrabs": "🦀🦀🦀",
+          }]);
